38672d44d1da2bd9eb5d19183d69ea2137b960ba
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-02-17 13:17:26 -0200

    Add test for single color two pixel image

--- src/test/cpp/ImageTest.cpp
+++ src/test/cpp/ImageTest.cpp
@@ -6,6 +6,7 @@ class ImageTest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(ImageTest);
 	CPPUNIT_TEST(testSinglePixel);
 	CPPUNIT_TEST(testSingleRedPixel);
+	CPPUNIT_TEST(testTwoPixels);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -23,6 +24,17 @@ public:
         image.setPixel(0, 0, color);
 
         CPPUNIT_ASSERT(image.getPixel(0, 0) == color);
+    }
+
+    void testTwoPixels() {
+        Image image(2, 1);
+	int color = 10;
+
+        image.setPixel(0, 0, color);
+        image.setPixel(1, 0, color);
+
+        CPPUNIT_ASSERT(image.getPixel(0, 0) == color);
+        CPPUNIT_ASSERT(image.getPixel(1, 0) == color);
     }
 };
 
