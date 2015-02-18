13c4f8b012ebe04423fd216e162cbc41f082906d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-02-18 12:31:13 -0200

    Implement storing image pixels as integers

--- src/main/cpp/Image.h
+++ src/main/cpp/Image.h
@@ -1,16 +1,26 @@
 class Image {
 private:
-    int color;
+    int width;
+    int height;
+    int* pixels;
+
+    inline int getPixelIndex(int x, int y) {
+	return y * height + x;
+    }
 
 public:
     Image(int width, int height) {
+	this->width = width;
+	this->height = height;
+
+	pixels = new int[width * height];
     }
 
     void setPixel(int x, int y, int color) {
-	this->color = color;
+	pixels[getPixelIndex(x, y)] = color;
     }
 
     int getPixel(int x, int y) {
-	return color;
+	return pixels[getPixelIndex(x, y)];
     }
 };
