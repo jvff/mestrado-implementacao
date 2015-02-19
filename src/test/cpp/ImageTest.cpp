ec8c0431846f2109d8c5506b40eb7b364e6524b4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-02-19 15:03:37 -0200

    Replace CppUnit with Google Test
    - Remove main test file
    - Convert ImageTest to use Google Test
    - Update build script
      - Statically link to the gtest library and the provided main function
      - Link with pthreads

--- src/test/cpp/ImageTest.cpp
+++ src/test/cpp/ImageTest.cpp
@@ -1,64 +1,11 @@
-#include <cppunit/extensions/HelperMacros.h>
+#include <gtest/gtest.h>
 
 #include "Image.h"
 
-class ImageTest : public CppUnit::TestFixture {
-    CPPUNIT_TEST_SUITE(ImageTest);
-        CPPUNIT_TEST(testSinglePixel);
-        CPPUNIT_TEST(testSingleRedPixel);
-        CPPUNIT_TEST(testTwoPixels);
-        CPPUNIT_TEST(testTwoPixelsWithDifferentColors);
-        CPPUNIT_TEST(testRedGreenBlueRect);
-    CPPUNIT_TEST_SUITE_END();
-
+class ImageTest : public ::testing::Test {
 public:
-    void testSinglePixel() {
-        testSinglePixel(10);
-    }
-
-    void testSingleRedPixel() {
-        testSinglePixel(0xFF0000);
-    }
-
     void testSinglePixel(int color) {
         testPixelsWithSameColor(1, 1, color);
-    }
-
-    void testTwoPixels() {
-        testPixelsWithSameColor(2, 1, 10);
-    }
-
-    void testTwoPixelsWithDifferentColors() {
-        int colors[] = { 10, 20 };
-
-        testPixels(2, 1, colors);
-    }
-
-    void testRedGreenBlueRect() {
-        int width = 9;
-        int height = 5;
-        int colors[width * height];
-        int *colorIterator = &colors[0];
-
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
-                switch (y % 5) {
-                    case 0:
-                    case 1:
-                    case 2:
-                        *colorIterator++ = 0xFF << (2 * (x % 3));
-                        break;
-                    case 3:
-                        *colorIterator++ = 0x00000000;
-                        break;
-                    case 4:
-                        *colorIterator++ = 0xFFFFFFFF;
-                        break;
-                };
-            }
-        }
-
-        testPixels(width, height, colors);
     }
 
     void testPixelsWithSameColor(int width, int height, int color) {
@@ -71,7 +18,7 @@ public:
 
         for (int x = 0; x < width; ++x) {
             for (int y = 0; y < height; ++y)
-                CPPUNIT_ASSERT(image.getPixel(x, y) == color);
+                EXPECT_EQ(image.getPixel(x, y), color);
         }
     }
 
@@ -88,9 +35,52 @@ public:
 
         for (int x = 0; x < width; ++x) {
             for (int y = 0; y < height; ++y)
-                CPPUNIT_ASSERT_EQUAL(image.getPixel(x, y), *(colorIterator++));
+                EXPECT_EQ(image.getPixel(x, y), *(colorIterator++));
         }
     }
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(ImageTest);
+TEST_F(ImageTest, testSinglePixel) {
+    testSinglePixel(10);
+}
+
+TEST_F(ImageTest, testSingleRedPixel) {
+    testSinglePixel(0xFF0000);
+}
+
+TEST_F(ImageTest, testTwoPixelsWithTheSameColor) {
+    testPixelsWithSameColor(2, 1, 10);
+}
+
+TEST_F(ImageTest, testTwoPixelsWithDifferentColors) {
+    int colors[] = { 10, 20 };
+
+    testPixels(2, 1, colors);
+}
+
+TEST_F(ImageTest, testRedGreenBlueRect) {
+    int width = 9;
+    int height = 5;
+    int colors[width * height];
+    int *colorIterator = &colors[0];
+
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y) {
+            switch (y % 5) {
+                case 0:
+                case 1:
+                case 2:
+                    *colorIterator++ = 0xFF << (2 * (x % 3));
+                    break;
+                case 3:
+                    *colorIterator++ = 0x00000000;
+                    break;
+                case 4:
+                    *colorIterator++ = 0xFFFFFFFF;
+                    break;
+            };
+        }
+    }
+
+    testPixels(width, height, colors);
+}
