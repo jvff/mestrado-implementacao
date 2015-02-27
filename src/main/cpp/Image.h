e7fb5e7d148500407a633b347eb83bceff1e2b21
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-02-26 23:04:05 -0300

    Refactor to make Image an abstract class
    - Move the functionality into IntArrayImage sub class

--- src/main/cpp/Image.h
+++ src/main/cpp/Image.h
@@ -2,30 +2,18 @@
 #define IMAGE_H
 
 class Image {
-private:
+protected:
     int width;
     int height;
-    int* pixels;
-
-    inline int getPixelIndex(int x, int y) {
-        return y * width + x;
-    }
 
 public:
     Image(int width, int height) {
         this->width = width;
         this->height = height;
-
-        pixels = new int[width * height];
     }
 
-    void setPixel(int x, int y, int color) {
-        pixels[getPixelIndex(x, y)] = color;
-    }
-
-    int getPixel(int x, int y) {
-        return pixels[getPixelIndex(x, y)];
-    }
+    virtual void setPixel(int x, int y, int color) = 0;
+    virtual int getPixel(int x, int y) = 0;
 };
 
 #endif
--- src/main/cpp/IntArrayImage.h
+++ src/main/cpp/IntArrayImage.h
@@ -4,8 +4,24 @@
 #include "Image.h"
 
 class IntArrayImage : public Image {
+private:
+    int* pixels;
+
+    inline int getPixelIndex(int x, int y) {
+        return y * width + x;
+    }
+
 public:
     IntArrayImage(int width, int height) : Image(width, height) {
+        pixels = new int[width * height];
+    }
+
+    void setPixel(int x, int y, int color) {
+        pixels[getPixelIndex(x, y)] = color;
+    }
+
+    int getPixel(int x, int y) {
+        return pixels[getPixelIndex(x, y)];
     }
 };
 
