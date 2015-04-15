140e660a3b33dfeb3aeeecd822998bcef563d752
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-04-15 23:27:22 +0000

    Test Image::setPixel pixel type
    - Must compile because it will fail if it can't instantiate the object in the
      test if the setPixel method isn't implemented

--- src/test/cpp/DummyImageTest.cpp
+++ src/test/cpp/DummyImageTest.cpp
@@ -1,3 +1,5 @@
+#include <gtest/gtest.h>
+
 #include "Image.hpp"
 
 struct DummyType {
@@ -9,9 +11,20 @@ public:
     DummyImage(int width, int height) : Image(width, height) {
     }
 
+    virtual void setPixel(int x, int y, DummyType value) {
+    }
+
     virtual DummyType getPixel(int x, int y) {
         static DummyType dummyValue = { 0 };
 
         return dummyValue;
     }
 };
+
+TEST(DummyImageTest, classIsntAbstract) {
+    Image<DummyType>* image = new DummyImage(0, 0);
+
+    EXPECT_TRUE(image != NULL);
+
+    delete image;
+}
