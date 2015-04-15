e20d8c5fac9c7546cef154ec6c136d7413bfe977
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-04-15 21:41:44 +0000

    Move template functions to header file

--- src/test/cpp/ImageTest.hpp
+++ src/test/cpp/ImageTest.hpp
@@ -13,4 +13,44 @@ protected:
     void testPixels(int width, int height, const int colors[]);
 };
 
+template <typename ImageType>
+void ImageTest<ImageType>::testSinglePixel(int color) {
+    testPixelsWithSameColor(1, 1, color);
+}
+
+template <typename ImageType>
+void ImageTest<ImageType>::testPixelsWithSameColor(int width, int height,
+        int color) {
+    ImageType image(width, height);
+
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y)
+            image.setPixel(x, y, color);
+    }
+
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y)
+            EXPECT_EQ(image.getPixel(x, y), color);
+    }
+}
+
+template <typename ImageType>
+void ImageTest<ImageType>::testPixels(int width, int height,
+        const int colors[]) {
+    ImageType image(width, height);
+    const int* colorIterator = &colors[0];
+
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y)
+            image.setPixel(x, y, *(colorIterator++));
+    }
+
+    colorIterator = &colors[0];
+
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y)
+            EXPECT_EQ(image.getPixel(x, y), *(colorIterator++));
+    }
+}
+
 #endif
