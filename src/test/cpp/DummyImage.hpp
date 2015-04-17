c02f67e7a1cf97dd27323740c3c37ab98c26a10b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-04-17 14:25:15 -0300

    Move DummyImage into a separate header

---
+++ src/test/cpp/DummyImage.hpp
@@ -0,0 +1,37 @@
+#ifndef DUMMY_IMAGE_H
+#define DUMMY_IMAGE_H
+
+#include <cstddef>
+
+#include "Image.hpp"
+
+#include "DummyTypes.hpp"
+
+class DummyImage : public Image<DummyType> {
+private:
+    bool* destructorWasCalled;
+public:
+    DummyImage(int width, int height) : Image(width, height) {
+	destructorWasCalled = NULL;
+    }
+
+    ~DummyImage() {
+	if (destructorWasCalled != NULL)
+	    *destructorWasCalled = true;
+    }
+
+    virtual void setPixel(int x, int y, DummyType value) {
+    }
+
+    virtual DummyType getPixel(int x, int y) {
+        static DummyType dummyValue = { 0 };
+
+        return dummyValue;
+    }
+
+    void setDestructorListener(bool* destructorListener) {
+	destructorWasCalled = destructorListener;
+    }
+};
+
+#endif
--- src/test/cpp/DummyImageTest.cpp
+++ src/test/cpp/DummyImageTest.cpp
@@ -2,34 +2,7 @@
 
 #include "Image.hpp"
 
-#include "DummyTypes.hpp"
-
-class DummyImage : public Image<DummyType> {
-private:
-    bool* destructorWasCalled;
-public:
-    DummyImage(int width, int height) : Image(width, height) {
-	destructorWasCalled = NULL;
-    }
-
-    ~DummyImage() {
-	if (destructorWasCalled != NULL)
-	    *destructorWasCalled = true;
-    }
-
-    virtual void setPixel(int x, int y, DummyType value) {
-    }
-
-    virtual DummyType getPixel(int x, int y) {
-        static DummyType dummyValue = { 0 };
-
-        return dummyValue;
-    }
-
-    void setDestructorListener(bool* destructorListener) {
-	destructorWasCalled = destructorListener;
-    }
-};
+#include "DummyImage.hpp"
 
 TEST(DummyImageTest, classIsntAbstract) {
     Image<DummyType>* image = new DummyImage(0, 0);
