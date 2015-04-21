d933906e6e936cc3b22c0ac5cdb10de9ef5ba201
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-04-21 18:01:54 -0300

    Remove Dummy prefix from DummySimpleArrayImageTest

--- src/test/cpp/SimpleArrayImageTest.cpp
+++ src/test/cpp/SimpleArrayImageTest.cpp
@@ -2,7 +2,7 @@
 
 #include "DummySimpleArrayImage.hpp"
 
-TEST(DummySimpleArrayImageTest, classIsntAbstract) {
+TEST(SimpleArrayImageTest, classIsntAbstract) {
     SimpleArrayImage<DummyType>* image = new DummySimpleArrayImage(0, 0);
 
     EXPECT_TRUE(image != NULL);
