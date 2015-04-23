885929258fcb03aae9bc0578bee32e6e06555648
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-04-23 21:36:43 +0000

    Add ImageFactory instance to Filter

--- include/main/cpp/Filter.hpp
+++ include/main/cpp/Filter.hpp
@@ -1,10 +1,20 @@
 #ifndef FILTER_HPP
 #define FILTER_HPP
 
+#include "ImageFactory.hpp"
+
 template <typename SourceType, typename DestinationType>
 class Filter {
+protected:
+    ImageFactory<DestinationType>* imageFactory;
+
 public:
+    Filter() {
+        imageFactory = new ImageFactory<DestinationType>();
+    }
+
     virtual ~Filter() {
+        delete imageFactory;
     }
 };
 
