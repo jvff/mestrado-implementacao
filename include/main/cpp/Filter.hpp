dcfdf283775f9b8724712c896906807340480020
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-12 22:12:35 +0000

    Add pixel template parameters to Filter

--- include/main/cpp/Filter.hpp
+++ include/main/cpp/Filter.hpp
@@ -1,26 +1,29 @@
 #ifndef FILTER_HPP
 #define FILTER_HPP
 
+#include "Image.hpp"
 #include "ImageFactory.hpp"
 
-template <typename SourceType, typename DestinationType>
+template <typename SourcePixelType, typename DestinationPixelType,
+        class DestinationImageType,
+        class SourceImageType = Image<SourcePixelType> >
 class Filter {
 protected:
-    ImageFactory<DestinationType>* imageFactory;
+    ImageFactory<DestinationImageType>* imageFactory;
 
-    virtual int getDestinationImageWidth(SourceType* sourceImage) = 0;
-    virtual int getDestinationImageHeight(SourceType* sourceImage) = 0;
+    virtual int getDestinationImageWidth(SourceImageType* sourceImage) = 0;
+    virtual int getDestinationImageHeight(SourceImageType* sourceImage) = 0;
 
 public:
     Filter() {
-        imageFactory = new ImageFactory<DestinationType>();
+        imageFactory = new ImageFactory<DestinationImageType>();
     }
 
     virtual ~Filter() {
         delete imageFactory;
     }
 
-    virtual DestinationType* apply(SourceType* sourceImage) {
+    virtual DestinationImageType* apply(SourceImageType* sourceImage) {
         int width = getDestinationImageWidth(sourceImage);
         int height = getDestinationImageHeight(sourceImage);
 
