163cb0d75e9fd55faf3f6abd97f51262da42e3b1
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-21 18:31:25 +0000

    Change default source image type

--- include/main/cpp/BinarizationFilter.hpp
+++ include/main/cpp/BinarizationFilter.hpp
@@ -1,8 +1,10 @@
 #ifndef BINARIZATION_FILTER_HPP
 #define BINARIZATION_FILTER_HPP
 
+#include "Image.hpp"
+
 template <typename SourcePixelType, typename DestinationImageType,
-        typename SourceImageType = char>
+        typename SourceImageType = Image<SourcePixelType> >
 class BinarizationFilter {
 };
 
