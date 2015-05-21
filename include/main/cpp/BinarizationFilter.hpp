3b5209ce1e24c718d71a6f2b1098ba02e7cf57ea
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-21 18:24:15 +0000

    Add optional source image type template parameter

--- include/main/cpp/BinarizationFilter.hpp
+++ include/main/cpp/BinarizationFilter.hpp
@@ -1,7 +1,8 @@
 #ifndef BINARIZATION_FILTER_HPP
 #define BINARIZATION_FILTER_HPP
 
-template <typename SourcePixelType, typename DestinationImageType>
+template <typename SourcePixelType, typename DestinationImageType,
+        typename SourceImageType = char>
 class BinarizationFilter {
 };
 
