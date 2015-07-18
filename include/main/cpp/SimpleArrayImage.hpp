093e9d60b4d9d73a89a117de4baae6fb87687857
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-18 16:00:30 -0300

    Refactor SimpleArrayImage to use vector

--- include/main/cpp/SimpleArrayImage.hpp
+++ include/main/cpp/SimpleArrayImage.hpp
@@ -1,12 +1,14 @@
 #ifndef SIMPLE_ARRAY_IMAGE_HPP
 #define SIMPLE_ARRAY_IMAGE_HPP
 
+#include <vector>
+
 #include "Image.hpp"
 
 template <typename PixelType>
 class SimpleArrayImage : public Image<PixelType> {
 private:
-    PixelType* pixels;
+    std::vector<PixelType> pixels;
 
     inline unsigned int getPixelIndex(unsigned int x, unsigned int y) const {
         return y * Image<PixelType>::width + x;
@@ -14,12 +16,7 @@ private:
 
 public:
     SimpleArrayImage(unsigned int width, unsigned int height)
-            : Image<PixelType>(width, height) {
-        pixels = new PixelType[width * height];
-    }
-
-    virtual ~SimpleArrayImage() {
-        delete[] pixels;
+            : Image<PixelType>(width, height), pixels(width * height) {
     }
 
     void setPixel(unsigned int x, unsigned int y, PixelType value) {
@@ -28,59 +25,6 @@ public:
 
     PixelType getPixel(unsigned int x, unsigned int y) const {
         return pixels[getPixelIndex(x, y)];
-    }
-};
-
-template <>
-class SimpleArrayImage<bool> : public Image<bool> {
-private:
-    unsigned int pixelsPerBlock;
-    long* blocks;
-
-    inline unsigned int getPixelIndex(unsigned int x, unsigned int y) const {
-        return y * width + x;
-    }
-
-    inline unsigned int getBlockIndex(unsigned int pixelIndex) const {
-        return pixelIndex / pixelsPerBlock;
-    }
-
-    inline long getPixelMask(unsigned int pixelIndex) const {
-        unsigned int pixelIndexInBlock = pixelIndex % pixelsPerBlock;
-
-        return 1 << pixelIndexInBlock;
-    }
-public:
-    SimpleArrayImage(unsigned int width, unsigned int height)
-            : Image(width, height) {
-        unsigned int totalPixels = width * height;
-
-        pixelsPerBlock = sizeof(long) * 8;
-        blocks = new long[(totalPixels - 1) / pixelsPerBlock + 1];
-    }
-
-    ~SimpleArrayImage() {
-        delete[] blocks;
-    }
-
-    virtual void setPixel(unsigned int x, unsigned int y, bool value) {
-        unsigned int pixelIndex = getPixelIndex(x, y);
-        unsigned int blockIndex = getBlockIndex(pixelIndex);
-        long mask = getPixelMask(pixelIndex);
-        long invertedMask = ~mask;
-
-        if (value == true)
-            blocks[blockIndex] |= mask;
-        else
-            blocks[blockIndex] &= invertedMask;
-    }
-
-    virtual bool getPixel(unsigned int x, unsigned int y) const {
-        unsigned int pixelIndex = getPixelIndex(x, y);
-        unsigned int blockIndex = getBlockIndex(pixelIndex);
-        long mask = getPixelMask(pixelIndex);
-
-        return (blocks[blockIndex] & mask) != 0L;
     }
 };
 
