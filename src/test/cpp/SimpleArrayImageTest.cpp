4a4815e8a45d92f99f39ea0fe1376c277633d66c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-18 23:25:35 -0300

    Test if SimpleArrayImage is assignable to lambda
    - Can be set with a lambda expression

--- src/test/cpp/SimpleArrayImageTest.cpp
+++ src/test/cpp/SimpleArrayImageTest.cpp
@@ -14,3 +14,24 @@ TEST(SimpleArrayImageTest, classIsntAbstract) {
 TEST(SimpleArrayImageTest, destructorIsVirtual) {
     AssertThat<SimpleArrayImage<DummyType> >::hasVirtualDestructor();
 }
+
+TEST(SimpleArrayImageTest, isSettableWithLambdaExpression) {
+    unsigned int width = 5;
+    unsigned int height = 4;
+
+    SimpleArrayImage<DummyType> image(width, height);
+
+    image = [width] (unsigned int x, unsigned int y) -> DummyType {
+        return DummyType{ (int)(y * width + x) };
+    };
+
+    DummyType pixelValue = {0};
+
+    for (unsigned int y = 0; y < height; ++y) {
+        for (unsigned int x = 0; x < width; ++x) {
+            assertThat(image.getPixel(x, y)).isEqualTo(pixelValue);
+
+            ++pixelValue.value;
+        }
+    }
+}
