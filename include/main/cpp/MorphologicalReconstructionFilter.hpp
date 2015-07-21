e746faef104c648f44bd2fbcad0d98e539150b65
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-21 17:28:12 -0300

    Create empty MorphologicalReconstruction class

---
+++ include/main/cpp/MorphologicalReconstructionFilter.hpp
@@ -0,0 +1,8 @@
+#ifndef MORPHOLOGICAL_RECONSTRUCTION_FILTER_HPP
+#define MORPHOLOGICAL_RECONSTRUCTION_FILTER_HPP
+
+template <typename PixelType, class DestinationImageType>
+class MorphologicalReconstructionFilter {
+};
+
+#endif
