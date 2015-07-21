536029e907764f18f81eb74c8d1e69a1da242706
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-21 17:28:47 -0300

    Make morph recons filter extend Filter

--- include/main/cpp/MorphologicalReconstructionFilter.hpp
+++ include/main/cpp/MorphologicalReconstructionFilter.hpp
@@ -1,8 +1,16 @@
 #ifndef MORPHOLOGICAL_RECONSTRUCTION_FILTER_HPP
 #define MORPHOLOGICAL_RECONSTRUCTION_FILTER_HPP
 
+#include "Filter.hpp"
+
 template <typename PixelType, class DestinationImageType>
-class MorphologicalReconstructionFilter {
+class MorphologicalReconstructionFilter
+        : public Filter<PixelType, PixelType, DestinationImageType> {
+protected:
+    PixelType apply(unsigned int x, unsigned int y,
+            const Image<PixelType>& sourceImage) {
+        return sourceImage.getPixel(x, y);
+    }
 };
 
 #endif
