4df34c1ab21f69df964e85f4ccc2f8ea0e46373f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-21 17:25:35 -0300

    Test if morph recons filter is a Filter sub-class

--- src/test/cpp/MorphologicalReconstructionFilterTest.cpp
+++ src/test/cpp/MorphologicalReconstructionFilterTest.cpp
@@ -14,3 +14,12 @@ TEST(MorphologicalReconstructionFilterTest, classIsntAbstract) {
 
     AssertThat<FilterClass>::isConstructible(WithoutParameters());
 }
+
+TEST(MorphologicalReconstructionFilterTest, isFilter) {
+    using PixelType = DummyType;
+    using ImageType = FakeImage<PixelType>;
+    using FilterClass = MorphologicalReconstructionFilter<PixelType, ImageType>;
+    using ParentFilterClass = Filter<PixelType, PixelType, ImageType>;
+
+    AssertThat<FilterClass>::isSubClass(Of<ParentFilterClass>());
+}
