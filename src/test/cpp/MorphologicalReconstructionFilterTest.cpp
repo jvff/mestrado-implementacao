eff85eb1eab6e2da827f25b3c51283f388d8160b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-24 21:56:36 -0300

    Test morph recons filter with a simple input
    - A horizontal line as input image and a single marker
    - The output image should be the same as the input image, with a
      different value for the line depth

--- src/test/cpp/MorphologicalReconstructionFilterTest.cpp
+++ src/test/cpp/MorphologicalReconstructionFilterTest.cpp
@@ -3,6 +3,7 @@
 #include "asserts.hpp"
 
 #include "MorphologicalReconstructionFilter.hpp"
+#include "SimpleArrayImage.hpp"
 
 #include "DummyTypes.hpp"
 #include "FakeImage.hpp"
@@ -46,4 +47,49 @@ TEST(MorphologicalReconstructionFilterTest, sourceImageTypeIsPropagated) {
             SourceImageType>;
 
     AssertThat<FilterClass>::isSubClass(Of<ParentFilterClass>());
+}
+
+TEST(MorphologicalReconstructionFilterTest, markerSpreadsRight) {
+    using PixelType = unsigned char;
+    using ImageType = SimpleArrayImage<PixelType>;
+    using FilterType = MorphologicalReconstructionFilter<PixelType, ImageType>;
+
+    const unsigned int width = 5;
+    const unsigned int height = 3;
+    const unsigned char depth = 10;
+
+    FilterType filter;
+    ImageType sourceImage(width, height);
+    ImageType markerImage(width, height);
+    ImageType expectedImage(width, height);
+
+    sourceImage = [] (unsigned int x, unsigned int y) -> PixelType {
+        unsigned int maxX = width - 1;
+
+        if (x >= 1 && x < maxX && y == height / 2)
+            return 100;
+        else
+            return 0;
+    };
+
+    markerImage = [] (unsigned int x, unsigned int y) -> PixelType {
+        if (x == 1 && y == height / 2)
+            return depth;
+        else
+            return 0;
+    };
+
+    expectedImage = [] (unsigned int x, unsigned int y) -> PixelType {
+        unsigned int maxX = width - 1;
+
+        if (x >= 1 && x < maxX && y == height / 2)
+            return depth;
+        else
+            return 0;
+    };
+
+
+    filter.apply(sourceImage, markerImage);
+
+    assertThat(markerImage).isEqualTo(expectedImage);
 }
