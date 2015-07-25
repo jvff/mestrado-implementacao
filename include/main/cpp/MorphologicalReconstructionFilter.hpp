23cd64bc6716da74956a6b6d647b98cc2264f2ef
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-24 22:39:32 -0300

    Refactor filter code for readability
    - Separate code into different methods in order to make it clearer

--- include/main/cpp/MorphologicalReconstructionFilter.hpp
+++ include/main/cpp/MorphologicalReconstructionFilter.hpp
@@ -15,31 +15,58 @@ private:
 public:
     void apply(const SourceImageType& sourceImage,
             DestinationImageType& destinationImage) override {
-        unsigned int width = destinationImage.getWidth();
-        unsigned int height = destinationImage.getHeight();
-        unsigned int maxX = width - 1;
-        unsigned int maxY = height - 1;
-
-        for (unsigned int x = 0; x < maxX; ++x) {
-            for (unsigned int y = 0; y < maxY; ++y) {
-                const PixelType& markerPixel = destinationImage.getPixel(x, y);
-                const PixelType& sourcePixel = sourceImage.getPixel(x, y);
-                const PixelType& rightPixel = sourceImage.getPixel(x + 1, y);
-                const PixelType& oldPixel = destinationImage.getPixel(x + 1, y);
-
-                PixelType currentPixel = std::min(markerPixel, sourcePixel);
-                PixelType newRightPixel = std::min(currentPixel, rightPixel);
-                PixelType destinationPixel = std::max(newRightPixel, oldPixel);
-
-                destinationImage.setPixel(x + 1, y, destinationPixel);
-            }
-        }
+        avoidMarkersBiggerThanSourcePixels(sourceImage, destinationImage);
+        firstPass(sourceImage, destinationImage);
     }
 
 protected:
     PixelType apply(unsigned int x, unsigned int y,
             const SourceImageType& sourceImage) override {
         return sourceImage.getPixel(x, y);
+    }
+
+private:
+    void avoidMarkersBiggerThanSourcePixels(const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage) {
+        unsigned int width = destinationImage.getWidth();
+        unsigned int height = destinationImage.getHeight();
+
+        for (unsigned int x = 0; x < width; ++x) {
+            for (unsigned int y = 0; y < height; ++y) {
+                const PixelType sourcePixel = sourceImage.getPixel(x, y);
+                const PixelType markerPixel = destinationImage.getPixel(x, y);
+                const PixelType truncatedMarkerPixel =
+                        std::min(markerPixel, sourcePixel);
+
+                destinationImage.setPixel(x, y, truncatedMarkerPixel);
+            }
+        }
+    }
+
+    void firstPass(const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage) {
+        unsigned int width = destinationImage.getWidth();
+        unsigned int height = destinationImage.getHeight();
+        unsigned int maxX = width - 1;
+        unsigned int maxY = height - 1;
+
+        for (unsigned int x = 0; x < maxX; ++x) {
+            for (unsigned int y = 0; y < maxY; ++y)
+                propagateRight(sourceImage, destinationImage, x, y);
+        }
+    }
+
+    void propagateRight(const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage, unsigned int x,
+            unsigned int y) {
+        const PixelType markerCenter = destinationImage.getPixel(x, y);
+        const PixelType markerRight = destinationImage.getPixel(x + 1, y);
+        const PixelType sourceRight = sourceImage.getPixel(x + 1, y);
+
+        const PixelType newRightPixel = std::min(markerCenter, sourceRight);
+        const PixelType destinationPixel = std::max(newRightPixel, markerRight);
+
+        destinationImage.setPixel(x + 1, y, destinationPixel);
     }
 };
 
