72c7cf8fc2ea754927513a7cc60b08c9705bbea3
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-29 19:25:31 -0300

    Make WatershedFilter a sub-class of Filter

--- include/main/cpp/WatershedFilter.hpp
+++ include/main/cpp/WatershedFilter.hpp
@@ -1,12 +1,13 @@
 #ifndef WATERSHED_FILTER_HPP
 #define WATERSHED_FILTER_HPP
 
-#include "Image.hpp"
+#include "Filter.hpp"
 
 template <typename SourcePixelType, typename DestinationPixelType,
         class DestinationImageType,
         class SourceImageType = Image<SourcePixelType> >
-class WatershedFilter {
+class WatershedFilter : public Filter<SourcePixelType, DestinationPixelType,
+        DestinationImageType, SourceImageType> {
 };
 
 #endif
