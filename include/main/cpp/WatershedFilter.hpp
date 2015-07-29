33358139be0b07d96a531f852e7cdc6a506de034
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-29 18:33:09 -0300

    Create initial WatershedFilter class template
    - For now only has one parameter

---
+++ include/main/cpp/WatershedFilter.hpp
@@ -0,0 +1,9 @@
+#ifndef WATERSHED_FILTER_HPP
+#define WATERSHED_FILTER_HPP
+
+template <typename SourcePixelType, typename DestinationPixelType,
+        class DestinationImageType>
+class WatershedFilter {
+};
+
+#endif
