9c560bfe17b1359be5143a27632b51892b2a3ebd
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-29 19:19:47 -0300

    Add optional template paramater to WatershedFilter

--- include/main/cpp/WatershedFilter.hpp
+++ include/main/cpp/WatershedFilter.hpp
@@ -1,8 +1,11 @@
 #ifndef WATERSHED_FILTER_HPP
 #define WATERSHED_FILTER_HPP
 
+#include "Image.hpp"
+
 template <typename SourcePixelType, typename DestinationPixelType,
-        class DestinationImageType>
+        class DestinationImageType,
+        class SourceImageType = Image<SourcePixelType> >
 class WatershedFilter {
 };
 
