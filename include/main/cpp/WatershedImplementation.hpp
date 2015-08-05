c658bc657ce4683ac604f9b1246945cd66bf8ca5
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-05 19:04:03 -0300

    Code empty WatershedImplementation class template

---
+++ include/main/cpp/WatershedImplementation.hpp
@@ -0,0 +1,9 @@
+#ifndef WATERSHED_IMPLEMENTATION_HPP
+#define WATERSHED_IMPLEMENTATION_HPP
+
+template <typename SourcePixelType, typename DestinationPixelType,
+        typename DestinationImageType>
+class WatershedImplementation {
+};
+
+#endif
