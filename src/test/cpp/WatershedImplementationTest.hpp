294380991c05e86f7e5c8a7e4d1b71e646e69cae
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-06 15:36:53 -0300

    Copy watershed tests from filter test
    - Use the same tests from WatershedFilterTest to test the watershed
      implementation
    - Change only the TestData alias to point to a WatershedTestData
      instantiation based on an AbstractFilterImplementationTestData

--- src/test/cpp/WatershedImplementationTest.hpp
+++ src/test/cpp/WatershedImplementationTest.hpp
@@ -5,10 +5,24 @@
 
 #include "asserts.hpp"
 
+#include "SimpleArrayImage.hpp"
 #include "WatershedImplementation.hpp"
 
 #include "AbstractFilterImplementationTestData.hpp"
 #include "DummyTypes.hpp"
 #include "FakeImage.hpp"
+#include "WatershedTestData.hpp"
+
+#define IMAGE_PARAMS SourcePixelType, DestinationPixelType, \
+    DestinationImageType, SourceImageType
+
+template <typename SourcePixelType,
+        typename DestinationPixelType = SourcePixelType,
+        typename DestinationImageType = SimpleArrayImage<DestinationPixelType>,
+        typename SourceImageType = SimpleArrayImage<SourcePixelType> >
+using TestData = WatershedTestData<
+        AbstractFilterImplementationTestData<
+            WatershedImplementation<IMAGE_PARAMS>, IMAGE_PARAMS>,
+        IMAGE_PARAMS>;
 
 #endif
