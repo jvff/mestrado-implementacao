f50fad0f16c259b57be7236a796202f9dd54b4bb
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-06 11:16:34 -0300

    Refactor to split AbstractFilterTestData
    - Create AbstractImageTransformationTestData class template that doesn't
      need a filter, using instead a virtual method to transform the source
      image
    - Update AbstractFilterTestData to sub-class the new class template by
      only adding the filter and implementing the transformImage method

--- src/test/cpp/AbstractFilterTestData.hpp
+++ src/test/cpp/AbstractFilterTestData.hpp
@@ -7,57 +7,21 @@
 
 #include "SimpleArrayImage.hpp"
 
-#include "AbstractTestData.hpp"
+#include "AbstractImageTransformationTestData.hpp"
 
 template <typename FilterType, typename SourcePixelType,
         typename DestinationPixelType = SourcePixelType,
         typename DestinationImageType = SimpleArrayImage<DestinationPixelType>,
         typename SourceImageType = SimpleArrayImage<SourcePixelType> >
-class AbstractFilterTestData : public AbstractTestData {
-protected:
-    using SourceImagePointer = std::unique_ptr<SourceImageType>;
-    using DestinationImagePointer = std::unique_ptr<DestinationImageType>;
-
-    unsigned int width;
-    unsigned int height;
-
-    unsigned int expectedWidth;
-    unsigned int expectedHeight;
-
+class AbstractFilterTestData : public AbstractImageTransformationTestData<
+        SourcePixelType, DestinationPixelType, DestinationImageType,
+        SourceImageType> {
 public:
     FilterType filter;
-    SourceImagePointer sourceImage;
-    DestinationImagePointer expectedImage;
-
-public:
-    virtual ~AbstractFilterTestData() {
-    }
-
-    void setDimensions(unsigned int newWidth, unsigned int newHeight) {
-        if (stateIs(State::EMPTY)) {
-            width = newWidth;
-            height = newHeight;
-
-            updateExpectedDimensions();
-
-            sourceImage.reset(new SourceImageType(width, height));
-            expectedImage.reset(new DestinationImageType(expectedWidth,
-                        expectedHeight));
-
-            state = State::READY;
-        }
-    }
 
 protected:
-    virtual void updateExpectedDimensions() {
-        expectedWidth = width;
-        expectedHeight = height;
-    }
-
-    void runTest() override {
-        auto result = filter.apply(*sourceImage);
-
-        assertThat(result).isEqualTo(*expectedImage);
+    DestinationImageType transformImage() override {
+        return filter.apply(*this->sourceImage);
     }
 };
 
---
+++ src/test/cpp/AbstractImageTransformationTestData.hpp
@@ -0,0 +1,65 @@
+#ifndef ABSTRACT_IMAGE_TRANSFORMATION_TEST_DATA_HPP
+#define ABSTRACT_IMAGE_TRANSFORMATION_TEST_DATA_HPP
+
+#include <memory>
+
+#include "asserts.hpp"
+
+#include "SimpleArrayImage.hpp"
+
+#include "AbstractTestData.hpp"
+
+template <typename SourcePixelType,
+        typename DestinationPixelType = SourcePixelType,
+        typename DestinationImageType = SimpleArrayImage<DestinationPixelType>,
+        typename SourceImageType = SimpleArrayImage<SourcePixelType> >
+class AbstractImageTransformationTestData : public AbstractTestData {
+protected:
+    using SourceImagePointer = std::unique_ptr<SourceImageType>;
+    using DestinationImagePointer = std::unique_ptr<DestinationImageType>;
+
+    unsigned int width;
+    unsigned int height;
+
+    unsigned int expectedWidth;
+    unsigned int expectedHeight;
+
+public:
+    SourceImagePointer sourceImage;
+    DestinationImagePointer expectedImage;
+
+public:
+    virtual ~AbstractImageTransformationTestData() {
+    }
+
+    void setDimensions(unsigned int newWidth, unsigned int newHeight) {
+        if (stateIs(State::EMPTY)) {
+            width = newWidth;
+            height = newHeight;
+
+            updateExpectedDimensions();
+
+            sourceImage.reset(new SourceImageType(width, height));
+            expectedImage.reset(new DestinationImageType(expectedWidth,
+                        expectedHeight));
+
+            state = State::READY;
+        }
+    }
+
+protected:
+    virtual void updateExpectedDimensions() {
+        expectedWidth = width;
+        expectedHeight = height;
+    }
+
+    void runTest() override {
+        auto result = transformImage();
+
+        assertThat(result).isEqualTo(*expectedImage);
+    }
+
+    virtual DestinationImageType transformImage() = 0;
+};
+
+#endif
