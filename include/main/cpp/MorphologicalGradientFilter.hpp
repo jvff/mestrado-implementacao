f0c0adc2b35afe896269b49eff23bb99f588e4a6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-10 12:10:31 -0300

    Sub-class MorphologicalGradientFilter from Filter

--- include/main/cpp/MorphologicalGradientFilter.hpp
+++ include/main/cpp/MorphologicalGradientFilter.hpp
@@ -1,12 +1,13 @@
 #ifndef MORPHOLOGICAL_GRADIENT_FILTER_HPP
 #define MORPHOLOGICAL_GRADIENT_FILTER_HPP
 
-#include "Image.hpp"
+#include "Filter.hpp"
 
 template <typename SourcePixelType, typename DestinationPixelType,
         typename DestinationImageType,
         typename SourceImageType = Image<SourcePixelType> >
-class MorphologicalGradientFilter {
+class MorphologicalGradientFilter : public Filter<SourcePixelType,
+        DestinationPixelType, DestinationImageType, SourceImageType> {
 };
 
 #endif
