9d731b3d587ed21623c6d8a3f06dadd2859c91a1
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-10 11:54:10 -0300

    Create MorphologicalGradinteFilter class template

---
+++ include/main/cpp/MorphologicalGradientFilter.hpp
@@ -0,0 +1,9 @@
+#ifndef MORPHOLOGICAL_GRADIENT_FILTER_HPP
+#define MORPHOLOGICAL_GRADIENT_FILTER_HPP
+
+template <typename SourcePixelType, typename DestinationPixelType,
+        typename DestinationImageType>
+class MorphologicalGradientFilter {
+};
+
+#endif
