da97b10fdab077abc5936348db7a7240257a6f23
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-10 12:04:45 -0300

    Add optional template parameter to MG filter

--- include/main/cpp/MorphologicalGradientFilter.hpp
+++ include/main/cpp/MorphologicalGradientFilter.hpp
@@ -1,8 +1,11 @@
 #ifndef MORPHOLOGICAL_GRADIENT_FILTER_HPP
 #define MORPHOLOGICAL_GRADIENT_FILTER_HPP
 
+#include "Image.hpp"
+
 template <typename SourcePixelType, typename DestinationPixelType,
-        typename DestinationImageType>
+        typename DestinationImageType,
+        typename SourceImageType = Image<SourcePixelType> >
 class MorphologicalGradientFilter {
 };
 
