e60f86195cb8e91fc14109f6db2dbfe4844ed202
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-12 19:11:16 -0300

    Test AMG filter with a dark square
    - Test if adjustment works correctly

--- src/test/cpp/AdjustedMorphologicalGradientFilterTest.cpp
+++ src/test/cpp/AdjustedMorphologicalGradientFilterTest.cpp
@@ -106,3 +106,59 @@ TEST(AdjustedMorphologicalGradientFilterTest, lightSquare) {
 
     assertThat(result).isEqualTo(expectedImage);
 }
+
+TEST(AdjustedMorphologicalGradientFilterTest, darkSquare) {
+    using PixelType = unsigned char;
+    using ImageType = SimpleArrayImage<PixelType>;
+    using FilterType = AdjustedMorphologicalGradientFilter<PixelType, PixelType,
+            ImageType>;
+
+    const unsigned int structureSize = 1;
+    const PixelType activationThreshold = 200;
+    const PixelType maxPixelValue = 0xFF;
+    const float adjustmentExponent = 0.01;
+
+    const unsigned int imageSize = 10;
+    const unsigned int squareSize = 6;
+    const unsigned int squareStart = 2;
+    const unsigned int squareEnd = squareStart + squareSize - 1;
+    const unsigned int outerSquareStart = squareStart - structureSize;
+    const unsigned int outerSquareEnd = squareEnd + structureSize;
+    const unsigned int innerSquareStart = squareStart + structureSize;
+    const unsigned int innerSquareEnd = squareEnd - structureSize;
+
+    FilterType filter(structureSize, activationThreshold, maxPixelValue,
+            adjustmentExponent);
+    ImageType sourceImage(imageSize, imageSize);
+    ImageType expectedImage(imageSize, imageSize);
+
+    sourceImage = [] (unsigned int x, unsigned int y) -> PixelType {
+        if (x >= squareStart && x <= squareEnd && y >= squareStart
+                && y <= squareEnd) {
+            return 100;
+        } else
+            return 50;
+    };
+
+    expectedImage = [=] (unsigned int x, unsigned int y) -> PixelType {
+        if (x >= innerSquareStart && x <= innerSquareEnd
+                && y >= innerSquareStart && y <= innerSquareEnd) {
+            return 0;
+        } else if (x >= outerSquareStart && x <= outerSquareEnd
+                && y >= outerSquareStart && y <= outerSquareEnd) {
+            float factor = (float)maxPixelValue;
+            float unadjustedValue = 50.f;
+            float unadjustedNormalizedValue = unadjustedValue / factor;
+            float adjustedNormalizedValue = std::pow(unadjustedNormalizedValue,
+                    adjustmentExponent);
+            float adjustedValue = adjustedNormalizedValue * factor;
+
+            return (PixelType)adjustedValue;
+        } else
+            return 0;
+    };
+
+    auto result = filter.apply(sourceImage);
+
+    assertThat(result).isEqualTo(expectedImage);
+}
