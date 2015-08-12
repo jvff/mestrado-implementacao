559a6a5b8450b80dcb4904110ba34f03f28b9ee8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-12 14:25:35 -0300

    Create initial AMG filter class template

---
+++ include/main/cpp/AdjustedMorphologicalGradientFilter.hpp
@@ -0,0 +1,9 @@
+#ifndef ADJUSTED_MORPHOLOGICAL_GRADIENT_FILTER_HPP
+#define ADJUSTED_MORPHOLOGICAL_GRADIENT_FILTER_HPP
+
+template <typename SourcePixelType, typename DestinationPixelType,
+        typename DestinationImageType>
+class AdjustedMorphologicalGradientFilter {
+};
+
+#endif
