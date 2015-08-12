8a0eeac5ddd9a95b0d8d9b3ff3df5c4e68034c10
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-12 14:28:41 -0300

    Add optional template parameter to AMG filter

--- include/main/cpp/AdjustedMorphologicalGradientFilter.hpp
+++ include/main/cpp/AdjustedMorphologicalGradientFilter.hpp
@@ -1,8 +1,11 @@
 #ifndef ADJUSTED_MORPHOLOGICAL_GRADIENT_FILTER_HPP
 #define ADJUSTED_MORPHOLOGICAL_GRADIENT_FILTER_HPP
 
+#include "Image.hpp"
+
 template <typename SourcePixelType, typename DestinationPixelType,
-        typename DestinationImageType>
+        typename DestinationImageType,
+        typename SourceImageType = Image<SourcePixelType> >
 class AdjustedMorphologicalGradientFilter {
 };
 
