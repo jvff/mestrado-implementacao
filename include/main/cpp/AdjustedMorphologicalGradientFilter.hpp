8381eeadd6b031ceaec51806ed4c833c10d72b0a
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-12 16:51:45 -0300

    Implement AMG filter without adjustment

--- include/main/cpp/AdjustedMorphologicalGradientFilter.hpp
+++ include/main/cpp/AdjustedMorphologicalGradientFilter.hpp
@@ -2,16 +2,34 @@
 #define ADJUSTED_MORPHOLOGICAL_GRADIENT_FILTER_HPP
 
 #include "Filter.hpp"
+#include "MorphologicalGradientFilter.hpp"
 
 template <typename SourcePixelType, typename DestinationPixelType,
         typename DestinationImageType,
         typename SourceImageType = Image<SourcePixelType> >
 class AdjustedMorphologicalGradientFilter : public Filter<SourcePixelType,
         DestinationPixelType, DestinationImageType, SourceImageType> {
+private:
+    using MorphologicalGradientFilterType = MorphologicalGradientFilter<
+            SourcePixelType, DestinationPixelType, DestinationImageType,
+            SourceImageType>;
+    using SuperClass = Filter<SourcePixelType, DestinationPixelType,
+            DestinationImageType, SourceImageType>;
+
+    MorphologicalGradientFilterType unadjustedFilter;
+
 public:
-    AdjustedMorphologicalGradientFilter(unsigned int, const SourcePixelType&,
-            const DestinationPixelType&, float)
+    AdjustedMorphologicalGradientFilter(unsigned int structureSize,
+            const SourcePixelType&, const DestinationPixelType&, float)
+            : unadjustedFilter(structureSize) {
     }
+
+    void apply(const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage) override {
+        unadjustedFilter.apply(sourceImage, destinationImage);
+    }
+
+    using SuperClass::apply;
 
 private:
     DestinationPixelType apply(unsigned int x, unsigned int y,
