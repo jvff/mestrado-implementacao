9e70d5ef57ce499c96db26fcd46c64d2934f85c7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-12 14:35:31 -0300

    Make AMG filter a sub-class of Filter

--- include/main/cpp/AdjustedMorphologicalGradientFilter.hpp
+++ include/main/cpp/AdjustedMorphologicalGradientFilter.hpp
@@ -1,12 +1,13 @@
 #ifndef ADJUSTED_MORPHOLOGICAL_GRADIENT_FILTER_HPP
 #define ADJUSTED_MORPHOLOGICAL_GRADIENT_FILTER_HPP
 
-#include "Image.hpp"
+#include "Filter.hpp"
 
 template <typename SourcePixelType, typename DestinationPixelType,
         typename DestinationImageType,
         typename SourceImageType = Image<SourcePixelType> >
-class AdjustedMorphologicalGradientFilter {
+class AdjustedMorphologicalGradientFilter : public Filter<SourcePixelType,
+        DestinationPixelType, DestinationImageType, SourceImageType> {
 };
 
 #endif
