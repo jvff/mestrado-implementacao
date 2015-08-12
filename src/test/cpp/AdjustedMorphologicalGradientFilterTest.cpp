093ffd948e351772b2e861b1815bad82bdb05baa
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-12 14:32:31 -0300

    Test if AMG filter is a sub-class of Filter

--- src/test/cpp/AdjustedMorphologicalGradientFilterTest.cpp
+++ src/test/cpp/AdjustedMorphologicalGradientFilterTest.cpp
@@ -27,3 +27,16 @@ TEST(AdjustedMorphologicalGradientFilterTest, hasOptionalTemplateParameter) {
 
     AssertThat<ImplicitType>::isTheSame(As<ExplicitType>());
 }
+
+TEST(AdjustedMorphologicalGradientFilterTest, isFilter) {
+    using SourcePixelType = DummyTypes<1>;
+    using DestinationPixelType = DummyTypes<2>;
+    using SourceImageType = Image<SourcePixelType>;
+    using DestinationImageType = FakeImage<DestinationPixelType>;
+    using SubClass = AdjustedMorphologicalGradientFilter<SourcePixelType,
+            DestinationPixelType, DestinationImageType>;
+    using SuperClass = Filter<SourcePixelType, DestinationPixelType,
+            DestinationImageType, SourceImageType>;
+
+    AssertThat<SubClass>::isSubClass(Of<SuperClass>());
+}
