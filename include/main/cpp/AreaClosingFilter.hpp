6860391eaaf133c23b264994e04c5fd48552adcd
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-15 23:05:49 -0300

    Sub-class AreaClosingFilter from Filter

--- include/main/cpp/AreaClosingFilter.hpp
+++ include/main/cpp/AreaClosingFilter.hpp
@@ -1,12 +1,13 @@
 #ifndef AREA_CLOSING_FILTER_HPP
 #define AREA_CLOSING_FILTER_HPP
 
-#include "Image.hpp"
+#include "Filter.hpp"
 
 template <typename SourcePixelType, typename DestinationPixelType,
         typename DestinationImageType,
         typename SourceImageType = Image<SourcePixelType> >
-class AreaClosingFilter {
+class AreaClosingFilter : public Filter<SourcePixelType, DestinationPixelType,
+        DestinationImageType, SourceImageType> {
 };
 
 #endif
