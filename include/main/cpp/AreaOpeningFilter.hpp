de4f49e72677421e91521651a879a9aa2a0597af
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-16 12:50:49 -0300

    Sub-class AreaOpeningFilter from Filter

--- include/main/cpp/AreaOpeningFilter.hpp
+++ include/main/cpp/AreaOpeningFilter.hpp
@@ -1,12 +1,13 @@
 #ifndef AREA_OPENING_FILTER_HPP
 #define AREA_OPENING_FILTER_HPP
 
-#include "Image.hpp"
+#include "Filter.hpp"
 
 template <typename SourcePixelType, typename DestinationPixelType,
         typename DestinationImageType,
         typename SourceImageType = Image<SourcePixelType> >
-class AreaOpeningFilter {
+class AreaOpeningFilter : public Filter<SourcePixelType, DestinationPixelType,
+        DestinationImageType, SourceImageType> {
 };
 
 #endif
