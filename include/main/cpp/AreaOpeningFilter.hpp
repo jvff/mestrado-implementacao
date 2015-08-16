8a7410bbc0cc27b9476b78d10dd09a666cd8760e
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-16 12:50:49 -0300

    Add AreaOpeningFilter constructor with parameter
    - Parameter is the minimum area size to be included in the filtered
      image

--- include/main/cpp/AreaOpeningFilter.hpp
+++ include/main/cpp/AreaOpeningFilter.hpp
@@ -8,6 +8,15 @@ template <typename SourcePixelType, typename DestinationPixelType,
         typename SourceImageType = Image<SourcePixelType> >
 class AreaOpeningFilter : public Filter<SourcePixelType, DestinationPixelType,
         DestinationImageType, SourceImageType> {
+public:
+    AreaOpeningFilter(unsigned int) {
+    }
+
+private:
+    DestinationPixelType apply(unsigned int x, unsigned int y,
+            const SourceImageType& sourceImage) {
+        return sourceImage.getPixel(x, y);
+    }
 };
 
 #endif
