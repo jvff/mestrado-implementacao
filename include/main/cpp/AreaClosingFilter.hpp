69d8dcaa9f399140a4c9227bb37f66f9e1aa2b8d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-15 23:02:07 -0300

    Add optional template param. to AreaClosingFilter

--- include/main/cpp/AreaClosingFilter.hpp
+++ include/main/cpp/AreaClosingFilter.hpp
@@ -1,8 +1,11 @@
 #ifndef AREA_CLOSING_FILTER_HPP
 #define AREA_CLOSING_FILTER_HPP
 
+#include "Image.hpp"
+
 template <typename SourcePixelType, typename DestinationPixelType,
-        typename DestinationImageType>
+        typename DestinationImageType,
+        typename SourceImageType = Image<SourcePixelType> >
 class AreaClosingFilter {
 };
 
