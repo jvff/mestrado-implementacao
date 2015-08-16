b63ee31eaefe131ddcf3f97730b21972ffc7b1c4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-15 23:16:46 -0300

    Add AreaClosingFilter constructor with parameter
    - Parameter is the minimum area size to be included in the filtered
      image

--- include/main/cpp/AreaClosingFilter.hpp
+++ include/main/cpp/AreaClosingFilter.hpp
@@ -8,6 +8,15 @@ template <typename SourcePixelType, typename DestinationPixelType,
         typename SourceImageType = Image<SourcePixelType> >
 class AreaClosingFilter : public Filter<SourcePixelType, DestinationPixelType,
         DestinationImageType, SourceImageType> {
+public:
+    AreaClosingFilter(unsigned int) {
+    }
+
+private:
+    DestinationPixelType apply(unsigned int x, unsigned int y,
+            const SourceImageType& sourceImage) {
+        return sourceImage.getPixel(x, y);
+    }
 };
 
 #endif
