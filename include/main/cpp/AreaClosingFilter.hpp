8cc14cd0731c95611d1c594136ae58d45730c1a9
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-15 22:59:29 -0300

    Create initial AreaClosingFilter class template

---
+++ include/main/cpp/AreaClosingFilter.hpp
@@ -0,0 +1,9 @@
+#ifndef AREA_CLOSING_FILTER_HPP
+#define AREA_CLOSING_FILTER_HPP
+
+template <typename SourcePixelType, typename DestinationPixelType,
+        typename DestinationImageType>
+class AreaClosingFilter {
+};
+
+#endif
