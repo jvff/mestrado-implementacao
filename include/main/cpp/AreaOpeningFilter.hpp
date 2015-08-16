462c0ab20fb903503f2b300b4bb31586d1b53abd
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-16 12:26:34 -0300

    Create initial AreaOpeningFilter class template

---
+++ include/main/cpp/AreaOpeningFilter.hpp
@@ -0,0 +1,9 @@
+#ifndef AREA_OPENING_FILTER_HPP
+#define AREA_OPENING_FILTER_HPP
+
+template <typename SourcePixelType, typename DestinationPixelType,
+        typename DestinationImageType>
+class AreaOpeningFilter {
+};
+
+#endif
