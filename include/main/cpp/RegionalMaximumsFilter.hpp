a5d851f00a97e09549223f722e378c8a0697b42a
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-17 19:08:26 -0300

    Create initial RegionalMaximumsFilter class templ.

---
+++ include/main/cpp/RegionalMaximumsFilter.hpp
@@ -0,0 +1,9 @@
+#ifndef REGIONAL_MAXIMUMS_FILTER_HPP
+#define REGIONAL_MAXIMUMS_FILTER_HPP
+
+template <typename SourcePixelType, typename DestinationPixelType,
+        typename DestinationImageType>
+class RegionalMaximumsFilter {
+};
+
+#endif
