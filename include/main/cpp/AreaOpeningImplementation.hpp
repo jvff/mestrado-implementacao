c9a93c5564806a5bb7106d3e254700d2a7066afe
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-18 18:51:48 -0300

    Create empty AreaOpeningImplementation class

---
+++ include/main/cpp/AreaOpeningImplementation.hpp
@@ -0,0 +1,9 @@
+#ifndef AREA_OPENING_IMPLEMENTATION_HPP
+#define AREA_OPENING_IMPLEMENTATION_HPP
+
+template <typename SourcePixelType, typename DestinationPixelType,
+        typename DestinationImageType>
+class AreaOpeningImplementation {
+};
+
+#endif
