31576d506bbc37369738fb23db7b5537c2d50683
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-18 22:26:39 -0300

    Start implementation of AreaOpeningImplementation
    - For now just copies the source image into the destination image

--- include/main/cpp/AreaOpeningImplementation.hpp
+++ include/main/cpp/AreaOpeningImplementation.hpp
@@ -7,9 +7,26 @@ template <typename SourcePixelType, typename DestinationPixelType,
         typename DestinationImageType,
         typename SourceImageType = Image<SourcePixelType> >
 class AreaOpeningImplementation {
+private:
+    const SourceImageType& sourceImage;
+    DestinationImageType& destinationImage;
+
 public:
-    AreaOpeningImplementation(unsigned int, const SourceImageType&,
-            DestinationImageType&) {
+    AreaOpeningImplementation(unsigned int, const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage) : sourceImage(sourceImage),
+            destinationImage(destinationImage) {
+    }
+
+    void apply() {
+        copySourceImage();
+    }
+
+private:
+    void copySourceImage() {
+        destinationImage = [&] (unsigned int x, unsigned int y)
+                -> DestinationPixelType {
+            return sourceImage.getPixel(x, y);
+        };
     }
 };
 
