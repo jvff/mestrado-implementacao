3dfc45ae0e75411298f3a8b5e2cd2e0a85993364
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-20 11:10:25 -0300

    Test assignment with different images

--- src/test/cpp/ImageAssignmentWithImageTest.cpp
+++ src/test/cpp/ImageAssignmentWithImageTest.cpp
@@ -16,11 +16,23 @@ TEST_P(ImageAssignmentWithImageTest, isAssignable) {
 static ImageConfigurations imagesWithSameSize = { 3, 5, 3, 5 };
 static ImageConfigurations assignToLargerImage = { 3, 2, 5, 4 };
 static ImageConfigurations assignToSmallerImage = { 9, 7, 7, 6 };
+static ImageConfigurations assignToWiderImage = { 4, 3, 9, 3 };
+static ImageConfigurations assignToNarrowerImage = { 5, 4, 4, 4 };
+static ImageConfigurations assignToTallerImage = { 2, 3, 2, 4 };
+static ImageConfigurations assignToShorterImage = { 3, 5, 3, 2 };
+static ImageConfigurations assignToShorterWiderImage = { 4, 5, 3, 2 };
+static ImageConfigurations assignToTallerNarrowerImage = { 4, 2, 2, 3 };
 
 INSTANTIATE_TEST_CASE_P(ImageAssignmentWithImageTestInstantiation,
         ImageAssignmentWithImageTest,
         testing::Values(
             imagesWithSameSize,
             assignToLargerImage,
-            assignToSmallerImage
+            assignToSmallerImage,
+            assignToWiderImage,
+            assignToNarrowerImage,
+            assignToTallerImage,
+            assignToShorterImage,
+            assignToShorterWiderImage,
+            assignToTallerNarrowerImage
         ));
