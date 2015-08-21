6c7d9e9fc73cb831410d7e6c170b9713cdf4f0cb
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-21 17:41:19 -0300

    Refactor to create SimpleFilterTest fixture
    - Centralize common aliases
    - Create helper method to mock the source image

--- src/test/cpp/SimpleFilterTest.cpp
+++ src/test/cpp/SimpleFilterTest.cpp
@@ -1,31 +1,10 @@
-#include <gtest/gtest.h>
+#include "SimpleFilterTest.hpp"
 
-#include "asserts.hpp"
-#include "fakeit.hpp"
-
-#include "SimpleFilter.hpp"
-
-#include "DummyTypes.hpp"
-#include "FakeImage.hpp"
-#include "FakeSimpleFilter.hpp"
-
-using namespace fakeit;
-
-TEST(SimpleFilterTest, classTemplateExists) {
-    using SourcePixelType = DummyTypes<1>;
-    using DestinationPixelType = DummyTypes<2>;
-    using SourceImageType = Image<SourcePixelType>;
-    using DestinationImageType = FakeImage<DestinationPixelType>;
-    using DummyFilterType = SimpleFilter<SourceImageType, DestinationImageType>;
-
+TEST_F(SimpleFilterTest, classTemplateExists) {
     AssertThat<DummyFilterType>::isClassOrStruct();
 }
 
-TEST(SimpleFilterTest, isSubClassOfFilter) {
-    using SourcePixelType = DummyTypes<1>;
-    using DestinationPixelType = DummyTypes<2>;
-    using SourceImageType = Image<SourcePixelType>;
-    using DestinationImageType = FakeImage<DestinationPixelType>;
+TEST_F(SimpleFilterTest, isSubClassOfFilter) {
     using SubClass = SimpleFilter<SourceImageType, DestinationImageType>;
     using SuperClass = Filter<SourcePixelType, DestinationPixelType,
             DestinationImageType, SourceImageType>;
@@ -33,11 +12,7 @@ TEST(SimpleFilterTest, isSubClassOfFilter) {
     AssertThat<SubClass>::isSubClass(Of<SuperClass>());
 }
 
-TEST(SimpleFilterTest, destinationDimensionsAreSameAsSourceDimensions) {
-    using SourcePixelType = DummyTypes<1>;
-    using DestinationPixelType = DummyTypes<2>;
-    using SourceImageType = Image<SourcePixelType>;
-    using DestinationImageType = FakeImage<DestinationPixelType>;
+TEST_F(SimpleFilterTest, destinationDimensionsAreSameAsSourceDimensions) {
     using FilterType = FakeSimpleFilter<SourceImageType, DestinationImageType>;
 
     unsigned int width = 10;
@@ -45,11 +20,8 @@ TEST(SimpleFilterTest, destinationDimensionsAreSameAsSourceDimensions) {
 
     FilterType filter;
 
-    Mock<SourceImageType> sourceImageMock;
+    auto sourceImageMock = createSourceImageMock(width, height);
     const auto& sourceImage = sourceImageMock.get();
-
-    When(Method(sourceImageMock, getWidth)).Return(width);
-    When(Method(sourceImageMock, getHeight)).Return(height);
 
     auto result = filter.apply(sourceImage);
 
---
+++ src/test/cpp/SimpleFilterTest.hpp
@@ -0,0 +1,37 @@
+#ifndef SIMPLE_FILTER_TEST_HPP
+#define SIMPLE_FILTER_TEST_HPP
+
+#include <gtest/gtest.h>
+
+#include "asserts.hpp"
+#include "fakeit.hpp"
+
+#include "SimpleFilter.hpp"
+
+#include "DummyTypes.hpp"
+#include "FakeImage.hpp"
+#include "FakeSimpleFilter.hpp"
+
+using namespace fakeit;
+
+class SimpleFilterTest : public testing::Test {
+protected:
+    using SourcePixelType = DummyTypes<1>;
+    using DestinationPixelType = DummyTypes<2>;
+    using SourceImageType = Image<SourcePixelType>;
+    using DestinationImageType = FakeImage<DestinationPixelType>;
+    using DummyFilterType = SimpleFilter<SourceImageType, DestinationImageType>;
+
+protected:
+    Mock<SourceImageType> createSourceImageMock(unsigned int width,
+            unsigned int height) {
+        Mock<SourceImageType> mock;
+
+        When(Method(mock, getWidth)).Return(width);
+        When(Method(mock, getHeight)).Return(height);
+
+        return mock;
+    }
+};
+
+#endif
