b50d19dc38d56487e26c381b195327d732a1ae56
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-23 18:21:11 -0300

    Sub-class ComplexFilter from Filter

--- include/main/cpp/ComplexFilter.hpp
+++ include/main/cpp/ComplexFilter.hpp
@@ -1,9 +1,13 @@
 #ifndef COMPLEX_FILTER_HPP
 #define COMPLEX_FILTER_HPP
 
+#include "Filter.hpp"
+
 template <typename SourceImageType, typename DestinationImageType,
         typename ImplementationType>
-class ComplexFilter {
+class ComplexFilter : public Filter<typename SourceImageType::PixelType,
+        typename DestinationImageType::PixelType, DestinationImageType,
+        SourceImageType> {
 };
 
 #endif
