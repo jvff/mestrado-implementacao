a7f700446640dfb24ce9c60963184d0bf716fc93
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-27 12:57:54 -0300

    Create initial ComplexFilter class

---
+++ include/main/cpp/ComplexFilter.hpp
@@ -0,0 +1,9 @@
+#ifndef COMPLEX_FILTER_HPP
+#define COMPLEX_FILTER_HPP
+
+template <typename SourceImageType, typename DestinationImageType,
+        typename ImplementationType>
+class ComplexFilter {
+};
+
+#endif
