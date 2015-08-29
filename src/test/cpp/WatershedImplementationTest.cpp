4c42f4f971dc000eb2f650ac162296a32e13ca31
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-29 17:40:07 -0300

    Test if watershed implem. is a filter implem.

--- src/test/cpp/WatershedImplementationTest.cpp
+++ src/test/cpp/WatershedImplementationTest.cpp
@@ -13,5 +13,13 @@ TEST_F(WatershedImplementationTest, isConstructible) {
             With<SourceImageParameter, DestinationImageParameter>());
 }
 
+TEST_F(WatershedImplementationTest, isFilterImplementation) {
+    using SubClass = DummyImplementation;
+    using SuperClass = FilterImplementation<SourceImageType,
+            DestinationImageType>;
+
+    AssertThat<SubClass>::isSubClass(Of<SuperClass>());
+}
+
 INSTANTIATE_TYPED_TEST_CASE_P(WatershedImplementationTest, WatershedTests,
         TestData<unsigned char>);
