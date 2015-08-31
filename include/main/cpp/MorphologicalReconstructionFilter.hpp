f2bc4d8e105bac5e6b5a4c350922bad155d5a6eb
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-31 21:57:46 +0000

    Sub-class morph. recons. filter from ComplexFilter

--- include/main/cpp/MorphologicalReconstructionFilter.hpp
+++ include/main/cpp/MorphologicalReconstructionFilter.hpp
@@ -1,14 +1,14 @@
 #ifndef MORPHOLOGICAL_RECONSTRUCTION_FILTER_HPP
 #define MORPHOLOGICAL_RECONSTRUCTION_FILTER_HPP
 
-#include "Filter.hpp"
+#include "ComplexFilter.hpp"
 #include "MorphologicalReconstructionImplementation.hpp"
 
 template <typename PixelType, class DestinationImageType,
         class SourceImageType = Image<PixelType> >
-class MorphologicalReconstructionFilter
-        : public Filter<PixelType, PixelType, DestinationImageType,
-                SourceImageType> {
+class MorphologicalReconstructionFilter : public ComplexFilter<SourceImageType,
+        DestinationImageType, MorphologicalReconstructionImplementation<
+                SourceImageType, DestinationImageType> > {
 private:
     using ParentFilter = Filter<PixelType, PixelType, DestinationImageType,
             SourceImageType>;
