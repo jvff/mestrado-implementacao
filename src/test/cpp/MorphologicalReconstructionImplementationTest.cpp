4e37db25d410d5376cd3d0ead9ab55b31757ec3b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-31 18:16:21 +0000

    Test if MR implementation is FilterImplementation

--- src/test/cpp/MorphologicalReconstructionImplementationTest.cpp
+++ src/test/cpp/MorphologicalReconstructionImplementationTest.cpp
@@ -17,3 +17,16 @@ TEST(MorphologicalReconstructionImplementationTest, classTemplateExists) {
 
     AssertThat<DummyImplementationType>::isClassOrStruct();
 }
+
+TEST(MorphologicalReconstructionImplementationTest, isFilterImplementation) {
+    using SourcePixelType = DummyTypes<1>;
+    using DestinationPixelType = DummyTypes<2>;
+    using SourceImageType = Image<SourcePixelType>;
+    using DestinationImageType = FakeImage<DestinationPixelType>;
+    using SubClass = MorphologicalReconstructionImplementation<SourceImageType,
+            DestinationImageType>;
+    using SuperClass = FilterImplementation<SourceImageType,
+            DestinationImageType>;
+
+    AssertThat<SubClass>::isSubClass(Of<SuperClass>());
+}
