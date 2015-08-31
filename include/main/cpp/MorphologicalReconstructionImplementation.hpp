ded6f426924df253c54535c8c5944fe3ac367e4c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-31 18:11:17 +0000

    Create MorphologicalReconstructionImplementation

---
+++ include/main/cpp/MorphologicalReconstructionImplementation.hpp
@@ -0,0 +1,8 @@
+#ifndef MORPHOLOGICAL_RECONSTRUCTION_IMPLEMENTATION_HPP
+#define MORPHOLOGICAL_RECONSTRUCTION_IMPLEMENTATION_HPP
+
+template <typename SourceImageType, typename DestinationImageType>
+class MorphologicalReconstructionImplementation {
+};
+
+#endif
