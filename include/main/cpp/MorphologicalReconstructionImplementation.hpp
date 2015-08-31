e0b7ee1d441586083436dcc7dbfe449072bb9e31
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-31 18:27:54 +0000

    Make MR implem. constructible
    - Add constructor
    - Override abstract apply method

--- include/main/cpp/MorphologicalReconstructionImplementation.hpp
+++ include/main/cpp/MorphologicalReconstructionImplementation.hpp
@@ -6,6 +6,19 @@
 template <typename SourceImageType, typename DestinationImageType>
 class MorphologicalReconstructionImplementation
         : public FilterImplementation<SourceImageType, DestinationImageType> {
+private:
+    using SuperClass = FilterImplementation<SourceImageType,
+            DestinationImageType>;
+
+public:
+    MorphologicalReconstructionImplementation(
+            const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage)
+            : SuperClass(sourceImage, destinationImage) {
+    }
+
+    void apply() override {
+    }
 };
 
 #endif
