f32b71409ad7f4a293d4ca9bb1d1ddb33636a240
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-31 18:16:44 +0000

    Sub-class MR implem. from FilterImplementation

--- include/main/cpp/MorphologicalReconstructionImplementation.hpp
+++ include/main/cpp/MorphologicalReconstructionImplementation.hpp
@@ -1,8 +1,11 @@
 #ifndef MORPHOLOGICAL_RECONSTRUCTION_IMPLEMENTATION_HPP
 #define MORPHOLOGICAL_RECONSTRUCTION_IMPLEMENTATION_HPP
 
+#include "FilterImplementation.hpp"
+
 template <typename SourceImageType, typename DestinationImageType>
-class MorphologicalReconstructionImplementation {
+class MorphologicalReconstructionImplementation
+        : public FilterImplementation<SourceImageType, DestinationImageType> {
 };
 
 #endif
