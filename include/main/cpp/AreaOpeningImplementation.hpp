c0f6e673c4aa42ba796a553a29e32206d5645fa4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-01 10:53:39 -0300

    Sub-class AO implem. from FilterImplementation
    - Update constructor to pass parameters to super class

--- include/main/cpp/AreaOpeningImplementation.hpp
+++ include/main/cpp/AreaOpeningImplementation.hpp
@@ -1,19 +1,24 @@
 #ifndef AREA_OPENING_IMPLEMENTATION_HPP
 #define AREA_OPENING_IMPLEMENTATION_HPP
 
+#include "FilterImplementation.hpp"
 #include "Image.hpp"
 
 template <typename SourceImageType, typename DestinationImageType>
-class AreaOpeningImplementation {
+class AreaOpeningImplementation : public FilterImplementation<SourceImageType,
+        DestinationImageType> {
 private:
     using DestinationPixelType = typename DestinationImageType::PixelType;
+    using SuperClass = FilterImplementation<SourceImageType,
+            DestinationImageType>;
 
     const SourceImageType& sourceImage;
     DestinationImageType& destinationImage;
 
 public:
     AreaOpeningImplementation(unsigned int, const SourceImageType& sourceImage,
-            DestinationImageType& destinationImage) : sourceImage(sourceImage),
+            DestinationImageType& destinationImage) :
+            SuperClass(sourceImage, destinationImage), sourceImage(sourceImage),
             destinationImage(destinationImage) {
     }
 
