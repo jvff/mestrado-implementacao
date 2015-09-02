efb63198fa956056eeb28680fa2ab81cd9f97d91
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-02 18:04:27 -0300

    Create AreaOpeningImplementationTestData
    - Create helper methods to build the test cases
    - Use it in the test cases

--- src/test/cpp/AreaOpeningImplementationTest.cpp
+++ src/test/cpp/AreaOpeningImplementationTest.cpp
@@ -21,74 +21,17 @@ TEST_F(AreaOpeningImplementationTest, isConstructibleWithParameters) {
 }
 
 TEST_F(AreaOpeningImplementationTest, bigPlateauIsntCleared) {
-    using PixelType = unsigned char;
-    using ImageType = SimpleArrayImage<PixelType>;
-    using ImplementationType = AreaOpeningImplementation<Image<PixelType>,
-            ImageType>;
-
-    const unsigned int maximumPeakSize = 9;
-    const unsigned int imageSize = 6;
-    const unsigned int squareSize = 4;
-
-    const unsigned int squareStart = 1;
-    const unsigned int squareEnd = squareStart + squareSize - 1;
-
-    ImageType sourceImage(imageSize, imageSize);
-    ImageType resultingImage(imageSize, imageSize);
-    ImageType& expectedImage = sourceImage;
-
-    sourceImage = [] (unsigned int x, unsigned int y) {
-        if (x >= squareStart && x <= squareEnd && y >= squareStart
-                && y <= squareEnd) {
-            return 167;
-        } else
-            return 102;
-    };
-
-    ImplementationType implementation(maximumPeakSize, sourceImage,
-            resultingImage);
-
-    implementation.apply();
-
-    assertThat(resultingImage).isEqualTo(expectedImage);
+    TestData<unsigned char>()
+        .setDimensions(6, 6)
+        .setMaximumPeakSize(9)
+        .setBackground(102)
+        .drawSquare(1, 1, 4, 167);
 }
 
 TEST_F(AreaOpeningImplementationTest, smallPlateauIsCleared) {
-    using PixelType = unsigned char;
-    using ImageType = SimpleArrayImage<PixelType>;
-    using ImplementationType = AreaOpeningImplementation<Image<PixelType>,
-            ImageType>;
-
-    const unsigned int maximumPeakSize = 12;
-    const unsigned int imageSize = 6;
-    const unsigned int squareSize = 2;
-
-    const unsigned int squareStart = 1;
-    const unsigned int squareEnd = squareStart + squareSize - 1;
-
-    const PixelType plateauHeight = 201;
-    const PixelType floorHeight = 13;
-
-    ImageType sourceImage(imageSize, imageSize);
-    ImageType resultingImage(imageSize, imageSize);
-    ImageType expectedImage(imageSize, imageSize);
-
-    sourceImage = [] (unsigned int x, unsigned int y) -> PixelType {
-        if (x >= squareStart && x <= squareEnd && y >= squareStart
-                && y <= squareEnd) {
-            return plateauHeight;
-        } else
-            return floorHeight;
-    };
-
-    expectedImage = [] (unsigned int, unsigned int) -> PixelType {
-        return floorHeight;
-    };
-
-    ImplementationType implementation(maximumPeakSize, sourceImage,
-            resultingImage);
-
-    implementation.apply();
-
-    assertThat(resultingImage).isEqualTo(expectedImage);
+    TestData<unsigned char>()
+        .setDimensions(6, 6)
+        .setMaximumPeakSize(12)
+        .setBackground(13)
+        .drawSquare(1, 1, 2, 201);
 }
--- src/test/cpp/AreaOpeningImplementationTest.hpp
+++ src/test/cpp/AreaOpeningImplementationTest.hpp
@@ -8,8 +8,11 @@
 #include "AreaOpeningImplementation.hpp"
 #include "SimpleArrayImage.hpp"
 
+#include "AbstractFilterImplementationTestData.hpp"
+#include "ChainableMethodMacros.hpp"
 #include "DummyTypes.hpp"
 #include "FakeImage.hpp"
+#include "RectanglePainter.hpp"
 
 class AreaOpeningImplementationTest : public testing::Test {
 protected:
@@ -20,5 +23,83 @@ protected:
     using ImplementationClass = AreaOpeningImplementation<SourceImageType,
             DestinationImageType>;
 };
+
+template <typename PixelType, typename ImageType = SimpleArrayImage<PixelType> >
+class AreaOpeningImplementationTestData
+        : public AbstractFilterImplementationTestData<
+                AreaOpeningImplementation<ImageType, ImageType>, ImageType,
+                ImageType> {
+private:
+    using ImplementationType = AreaOpeningImplementation<ImageType, ImageType>;
+    using PainterType = RectanglePainter<ImageType>;
+    using State = AbstractTestData::State;
+    using SuperClass = AbstractFilterImplementationTestData<ImplementationType,
+            ImageType, ImageType>;
+    using ThisType = AreaOpeningImplementationTestData<PixelType, ImageType>;
+
+    std::unique_ptr<PainterType> sourcePainter;
+    std::unique_ptr<PainterType> expectedPainter;
+
+    unsigned int maximumPeakSize;
+
+public:
+    ~AreaOpeningImplementationTestData() {
+        tryToRunTest();
+    }
+
+    CHAIN(setDimensions, unsigned int width, unsigned int height) {
+        if (SuperClass::setDimensions(width, height)) {
+            sourcePainter.reset(new PainterType(*this->sourceImage));
+            expectedPainter.reset(new PainterType(*this->expectedImage));
+
+            this->state = State::SETTING_UP;
+        }
+    }
+
+    CHAIN(setMaximumPeakSize, unsigned int size) {
+        if (stateIs(State::SETTING_UP)) {
+            maximumPeakSize = size;
+
+            this->state = State::READY;
+        }
+    }
+
+    CHAIN(setBackground, const PixelType& color) {
+        if (stateIs(State::READY)) {
+            sourcePainter->fill(color);
+            expectedPainter->fill(color);
+        }
+    }
+
+    CHAIN(drawSquare, unsigned int x, unsigned int y, unsigned int size,
+            const PixelType& color) {
+        if (stateIs(State::READY)) {
+            auto area = size * size;
+
+            sourcePainter->drawSquare(x, y, size, color);
+
+            if (area >= maximumPeakSize)
+                expectedPainter->drawSquare(x, y, size, color);
+        }
+    }
+
+protected:
+    using SuperClass::cancelTestExecution;
+    using SuperClass::stateIs;
+    using SuperClass::tryToRunTest;
+
+    void finishSetUp() override {
+        cancelTestExecution();
+    }
+
+    ImplementationType instantiateImplementation(const ImageType& sourceImage,
+            ImageType& destinationImage) override {
+        return ImplementationType(maximumPeakSize, sourceImage,
+                destinationImage);
+    }
+};
+
+template <typename PixelType, typename ImageType = SimpleArrayImage<PixelType> >
+using TestData = AreaOpeningImplementationTestData<PixelType, ImageType>;
 
 #endif
