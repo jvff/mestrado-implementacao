475a9130f950cf2248cff4215bbfde1a297defd7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-03 11:09:23 -0300

    Test Coordinate value members

--- src/test/cpp/CoordinateTest.cpp
+++ src/test/cpp/CoordinateTest.cpp
@@ -7,3 +7,13 @@
 TEST(CoordinateTest, structExists) {
     AssertThat<Coordinate>::isClassOrStruct();
 }
+
+TEST(CoordinateTest, hasValueMembers) {
+    unsigned int x = 1;
+    unsigned int y = 2;
+
+    Coordinate coordinate = { x, y };
+
+    assertThat(coordinate.x).isEqualTo(x);
+    assertThat(coordinate.y).isEqualTo(y);
+}
