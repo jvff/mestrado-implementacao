36512d5c715bba66e138b59431137a1862109e99
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-05 16:31:23 -0300

    Test Pixel value constructor

--- src/test/cpp/PixelTest.cpp
+++ src/test/cpp/PixelTest.cpp
@@ -21,3 +21,15 @@ TEST(PixelTest, hasValueMembers) {
     assertThat(pixel.y).isEqualTo(y);
     assertThat(pixel.value).isEqualTo(value);
 }
+
+TEST(PixelTest, hasValueConstructor) {
+    unsigned int x = 12;
+    unsigned int y = 6;
+    DummyType value = { 1000 };
+
+    auto pixel = Pixel<DummyType>(x, y, value);
+
+    assertThat(pixel.x).isEqualTo(x);
+    assertThat(pixel.y).isEqualTo(y);
+    assertThat(pixel.value).isEqualTo(value);
+}
