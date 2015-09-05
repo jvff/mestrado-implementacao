9749007cb387c9623193d0a8fac9a3e92f7e8f8c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-05 16:27:59 -0300

    Test Pixel class template member values

--- src/test/cpp/PixelTest.cpp
+++ src/test/cpp/PixelTest.cpp
@@ -9,3 +9,15 @@
 TEST(PixelTest, structTemplateExists) {
     AssertThat<Pixel<DummyType> >::isClassOrStruct();
 }
+
+TEST(PixelTest, hasValueMembers) {
+    unsigned int x = 1;
+    unsigned int y = 2;
+    DummyType value = { 3 };
+
+    Pixel<DummyType> pixel = { x, y, value };
+
+    assertThat(pixel.x).isEqualTo(x);
+    assertThat(pixel.y).isEqualTo(y);
+    assertThat(pixel.value).isEqualTo(value);
+}
