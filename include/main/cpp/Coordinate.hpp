55d6cc4fa2dd226bdfb367db37ee50ff93bfe4bf
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-17 19:47:59 +0000

    Implement Coordinate::operator+

--- include/main/cpp/Coordinate.hpp
+++ include/main/cpp/Coordinate.hpp
@@ -10,6 +10,10 @@ struct Coordinate {
 
     Coordinate(unsigned int x, unsigned int y) : x(x), y(y) {
     }
+
+    Coordinate operator+(const Coordinate& displacement) const {
+        return Coordinate(x + displacement.x, y + displacement.y);
+    }
 };
 
 #endif
