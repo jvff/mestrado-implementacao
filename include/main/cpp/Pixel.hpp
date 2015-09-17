4afd2c615946965be9c6886b3e7aff364b2277ae
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-17 18:41:50 +0000

    Implement Pixel constructor with Coordinate param.

--- include/main/cpp/Pixel.hpp
+++ include/main/cpp/Pixel.hpp
@@ -1,6 +1,8 @@
 #ifndef PIXEL_HPP
 #define PIXEL_HPP
 
+#include "Coordinate.hpp"
+
 template <typename Type>
 struct Pixel {
     unsigned int x;
@@ -12,6 +14,10 @@ struct Pixel {
 
     Pixel(unsigned int x, unsigned int y, const Type& value) : x(x), y(y),
             value(value) {
+    }
+
+    Pixel(const Coordinate& coordinate, const Type& value) : x(coordinate.x),
+            y(coordinate.y), value(value) {
     }
 };
 
