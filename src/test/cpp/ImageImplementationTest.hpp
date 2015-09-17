b7f30e649c4464afce3e20d0280fd1e4adc9b904
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-17 12:44:29 +0000

    Refactor ImageImplementationTest
    - Move method implementation into the class

--- src/test/cpp/ImageImplementationTest.hpp
+++ src/test/cpp/ImageImplementationTest.hpp
@@ -8,71 +8,55 @@
 template <typename ImageType, typename PixelType>
 class ImageImplementationTest : public ::testing::Test {
 protected:
-    void testSinglePixel(PixelType value);
+    void testSinglePixel(PixelType value) {
+        testPixelsWithSameValue(1, 1, value);
+    }
+
     void testPixelsWithSameValue(unsigned int width, unsigned int height,
-            PixelType values);
+            PixelType value) {
+        ImageType image(width, height);
+
+        for (unsigned int x = 0; x < width; ++x) {
+            for (unsigned int y = 0; y < height; ++y)
+                image.setPixel(x, y, value);
+        }
+
+        for (unsigned int x = 0; x < width; ++x) {
+            for (unsigned int y = 0; y < height; ++y)
+                comparePixel(image.getPixel(x, y), value);
+        }
+    }
+
     void testPixels(unsigned int width, unsigned int height,
-            const PixelType values[]);
-    virtual void comparePixel(unsigned int x, unsigned int y, PixelType a,
-            PixelType b);
-    virtual bool comparePixel(PixelType a, PixelType b);
+            const PixelType values[]) {
+        ImageType image(width, height);
+        const PixelType* pixelIterator = &values[0];
+
+        for (unsigned int x = 0; x < width; ++x) {
+            for (unsigned int y = 0; y < height; ++y)
+                image.setPixel(x, y, *(pixelIterator++));
+        }
+
+        pixelIterator = &values[0];
+
+        for (unsigned int x = 0; x < width; ++x) {
+            for (unsigned int y = 0; y < height; ++y)
+                comparePixel(x, y, *(pixelIterator++), image.getPixel(x, y));
+        }
+    }
+
+    virtual void comparePixel(unsigned int x, unsigned int y,
+            PixelType expected, PixelType actual) {
+        if (comparePixel(expected, actual) == false) {
+            FAIL() << "Pixel at (" << x << "," << y << ")" << std::endl
+                    << "Expected: " << expected << std::endl
+                    << "  Actual: " << actual;
+        }
+    }
+
+    virtual bool comparePixel(PixelType expected, PixelType actual) {
+        return expected == actual;
+    }
 };
-
-template <typename ImageType, typename PixelType>
-void ImageImplementationTest<ImageType, PixelType>::testSinglePixel(
-        PixelType value) {
-    testPixelsWithSameValue(1, 1, value);
-}
-
-template <typename ImageType, typename PixelType>
-void ImageImplementationTest<ImageType, PixelType>::testPixelsWithSameValue(
-        unsigned int width, unsigned int height, PixelType value) {
-    ImageType image(width, height);
-
-    for (unsigned int x = 0; x < width; ++x) {
-        for (unsigned int y = 0; y < height; ++y)
-            image.setPixel(x, y, value);
-    }
-
-    for (unsigned int x = 0; x < width; ++x) {
-        for (unsigned int y = 0; y < height; ++y)
-            comparePixel(image.getPixel(x, y), value);
-    }
-}
-
-template <typename ImageType, typename PixelType>
-void ImageImplementationTest<ImageType, PixelType>::testPixels(
-        unsigned int width, unsigned int height, const PixelType values[]) {
-    ImageType image(width, height);
-    const PixelType* pixelIterator = &values[0];
-
-    for (unsigned int x = 0; x < width; ++x) {
-        for (unsigned int y = 0; y < height; ++y)
-            image.setPixel(x, y, *(pixelIterator++));
-    }
-
-    pixelIterator = &values[0];
-
-    for (unsigned int x = 0; x < width; ++x) {
-        for (unsigned int y = 0; y < height; ++y)
-            comparePixel(x, y, *(pixelIterator++), image.getPixel(x, y));
-    }
-}
-
-template <typename ImageType, typename PixelType>
-void ImageImplementationTest<ImageType, PixelType>::comparePixel(unsigned int x,
-        unsigned int y, PixelType expected, PixelType actual) {
-    if (comparePixel(expected, actual) == false) {
-        FAIL() << "Pixel at (" << x << "," << y << ")" << std::endl
-                << "Expected: " << expected << std::endl
-                << "  Actual: " << actual;
-    }
-}
-
-template <typename ImageType, typename PixelType>
-bool ImageImplementationTest<ImageType, PixelType>::comparePixel(
-        PixelType expected, PixelType actual) {
-    return expected == actual;
-}
 
 #endif
