b46baee9fe7bc237cdc89b209147dc59140ed01e
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-19 02:56:28 +0000

    Implement Coordinate::operator<

--- include/main/cpp/Coordinate.hpp
+++ include/main/cpp/Coordinate.hpp
@@ -14,6 +14,10 @@ struct Coordinate {
     Coordinate operator+(const Coordinate& displacement) const {
         return Coordinate(x + displacement.x, y + displacement.y);
     }
+
+    bool operator<(const Coordinate& other) const {
+        return x < other.x && y < other.y;
+    }
 };
 
 #endif
