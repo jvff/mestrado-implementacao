5feafeabd0cdeed997b5d33dd269ca651eea1efb
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-23 23:35:09 +0000

    Implement InversionImplementation
    - Without support for customized ranges

--- include/main/cpp/InversionImplementation.hpp
+++ include/main/cpp/InversionImplementation.hpp
@@ -1,6 +1,8 @@
 #ifndef INVERSION_IMPLEMENTATION_HPP
 #define INVERSION_IMPLEMENTATION_HPP
 
+#include <algorithm>
+
 #include "FilterImplementation.hpp"
 
 template <typename SourceImageType, typename DestinationImageType>
@@ -12,6 +14,11 @@ private:
             DestinationImageType>;
 
 private:
+    SourcePixelType minimumValue;
+    SourcePixelType maximumValue;
+
+    using SuperClass::width;
+    using SuperClass::height;
     using SuperClass::sourceImage;
     using SuperClass::destinationImage;
 
@@ -25,7 +32,41 @@ public:
     }
 
     void apply() override {
-        destinationImage = sourceImage;
+        discoverRange();
+        invert();
+    }
+
+    void discoverRange() {
+        initializeRangeValues();
+
+        for (unsigned int x = 0; x < width; ++x) {
+            for (unsigned int y = 0; y < height; ++y)
+                maybeUpdateRangeValues(sourceImage.getPixelValue(x, y));
+        }
+    }
+
+    void initializeRangeValues() {
+        minimumValue = sourceImage.getPixelValue(0, 0);
+        maximumValue = minimumValue;
+    }
+
+    void maybeUpdateRangeValues(const SourcePixelType& newValue) {
+        minimumValue = std::min(minimumValue, newValue);
+        maximumValue = std::max(maximumValue, newValue);
+    }
+
+    void invert() {
+        for (unsigned int x = 0; x < width; ++x) {
+            for (unsigned int y = 0; y < height; ++y)
+                invertPixel(x, y);
+        }
+    }
+
+    void invertPixel(unsigned int x, unsigned int y) {
+        auto originalValue = sourceImage.getPixelValue(x, y);
+        auto invertedValue = maximumValue - originalValue + minimumValue;
+
+        destinationImage.setPixel(x, y, invertedValue);
     }
 };
 
