19d8f8433b1e364c721b2587950cda6e8723cf1c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-23 18:35:54 +0000

    Subclass inversion implem. from filter implem.

--- include/main/cpp/InversionImplementation.hpp
+++ include/main/cpp/InversionImplementation.hpp
@@ -1,8 +1,11 @@
 #ifndef INVERSION_IMPLEMENTATION_HPP
 #define INVERSION_IMPLEMENTATION_HPP
 
+#include "FilterImplementation.hpp"
+
 template <typename SourceImageType, typename DestinationImageType>
-class InversionImplementation {
+class InversionImplementation
+        : public FilterImplementation<SourceImageType, DestinationImageType> {
 };
 
 #endif
