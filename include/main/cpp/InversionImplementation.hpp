1d6ffb6a3decde283f3861b2fb7a092e2d12000f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-23 16:27:26 +0000

    Create initial InversionImplementation class templ

---
+++ include/main/cpp/InversionImplementation.hpp
@@ -0,0 +1,8 @@
+#ifndef INVERSION_IMPLEMENTATION_HPP
+#define INVERSION_IMPLEMENTATION_HPP
+
+template <typename SourceImageType, typename DestinationImageType>
+class InversionImplementation {
+};
+
+#endif
