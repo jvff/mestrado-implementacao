e7824f0cadccb568a09e4c8836c47395121c5a2d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-09-24 14:46:02 -0300

    Test InversionImplementation with float image

--- src/test/cpp/InversionImplementationTest.cpp
+++ src/test/cpp/InversionImplementationTest.cpp
@@ -90,3 +90,14 @@ TEST_F(InversionImplementationTest, imageWithDifferentPixelsAndCustomRange) {
         .setPixel(1, 1, 200)
         .setPixel(1, 2, 111);
 }
+
+TEST_F(InversionImplementationTest, floatingPointImage) {
+    TestData<float>()
+        .setDimensions(3, 2)
+        .setPixel(0, 0, 0.008f)
+        .setPixel(0, 1, 12.75f)
+        .setPixel(1, 0, -100.2f)
+        .setPixel(1, 1, 1.1e4f)
+        .setPixel(2, 0, 1.1e7f)
+        .setPixel(2, 1, 9.2e7f);
+}
