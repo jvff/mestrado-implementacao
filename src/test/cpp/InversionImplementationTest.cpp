e0037ca996d35ddf9df39f52577cf89059a94309
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-10-26 20:47:54 -0200

    Refactor to create generic InversionTests
    - Use ComplexFilter test macros to create a separate set of
      InversionTests
    - Update InversionImplementationTest to instantiate the InversionTests

--- src/test/cpp/InversionImplementationTest.cpp
+++ src/test/cpp/InversionImplementationTest.cpp
@@ -1,4 +1,5 @@
 #include "InversionImplementationTest.hpp"
+#include "InversionTests.hpp"
 
 TEST_F(InversionImplementationTest, classTemplateExists) {
     AssertThat<DummyImplementation>::isClassOrStruct();
@@ -31,85 +32,5 @@ TEST_F(InversionImplementationTest, canBeConstructedWithOptionalParameters) {
             MaximumValueParameter>());
 }
 
-TEST_F(InversionImplementationTest, uniformImageIsntChanged) {
-    TestData<unsigned char>()
-        .setDimensions(2, 2)
-        .setPixel(0, 0, 100)
-        .setPixel(0, 1, 100)
-        .setPixel(1, 0, 100)
-        .setPixel(1, 1, 100);
-}
-
-TEST_F(InversionImplementationTest, pixelsAreSwapped) {
-    TestData<unsigned char>()
-        .setDimensions(2, 2)
-        .setPixel(0, 0, 12)
-        .setPixel(0, 1, 200)
-        .setPixel(1, 0, 200)
-        .setPixel(1, 1, 12);
-}
-
-TEST_F(InversionImplementationTest, pixelsAreSwappedAndMiddleValueIsKept) {
-    TestData<unsigned char>()
-        .setDimensions(2, 2)
-        .setPixel(0, 0, 12)
-        .setPixel(0, 1, 106)
-        .setPixel(1, 0, 106)
-        .setPixel(1, 1, 200);
-}
-
-TEST_F(InversionImplementationTest, imageWithDifferentPixels) {
-    TestData<unsigned char>()
-        .setDimensions(3, 2)
-        .setPixel(0, 0, 92)
-        .setPixel(0, 1, 100)
-        .setPixel(1, 0, 145)
-        .setPixel(1, 1, 140)
-        .setPixel(2, 0, 155)
-        .setPixel(2, 1, 200);
-}
-
-TEST_F(InversionImplementationTest, uniformImageIsDisplacedByCustomRange) {
-    TestData<unsigned char>()
-        .setDimensions(2, 2)
-        .setRange(36, 140)
-        .setPixel(0, 0, 37)
-        .setPixel(0, 1, 37)
-        .setPixel(1, 0, 37)
-        .setPixel(1, 1, 37);
-}
-
-TEST_F(InversionImplementationTest, imageWithDifferentPixelsAndCustomRange) {
-    TestData<unsigned char>()
-        .setDimensions(2, 3)
-        .setRange(22, 250)
-        .setPixel(0, 0, 40)
-        .setPixel(0, 1, 34)
-        .setPixel(0, 2, 249)
-        .setPixel(1, 0, 89)
-        .setPixel(1, 1, 200)
-        .setPixel(1, 2, 111);
-}
-
-TEST_F(InversionImplementationTest, floatingPointImage) {
-    TestData<float>()
-        .setDimensions(3, 2)
-        .setPixel(0, 0, 0.008f)
-        .setPixel(0, 1, 12.75f)
-        .setPixel(1, 0, -100.2f)
-        .setPixel(1, 1, 1.1e4f)
-        .setPixel(2, 0, 1.1e7f)
-        .setPixel(2, 1, 9.2e7f);
-}
-
-TEST_F(InversionImplementationTest, floatingPointImageWithCustomRange) {
-    TestData<float>()
-        .setDimensions(2, 3)
-        .setRange(-1.3e7f, 9.3e7f)
-        .setPixel(0, 0, 0.008f)
-        .setPixel(0, 1, 12.75f)
-        .setPixel(0, 2, -100.2f)
-        .setPixel(1, 0, 1.1e4f)
-        .setPixel(1, 1, 1.1e7f)
-        .setPixel(1, 2, 9.2e7f);
-}
+INSTANTIATE_COMPLEX_FILTER_TEST_CASE(InversionImplementationTest,
+        InversionTests, TestData);
---
+++ src/test/cpp/InversionTests.hpp
@@ -0,0 +1,97 @@
+#ifndef INVERSION_TESTS_HPP
+#define INVERSION_TESTS_HPP
+
+#include "ComplexFilterTestMacros.hpp"
+
+COMPLEX_FILTER_TEST_CASE(InversionTests);
+
+COMPLEX_FILTER_TEST(InversionTests, uniformImageIsntChanged) {
+    typename TestFixture::template TestData<unsigned char>()
+        .setDimensions(2, 2)
+        .setPixel(0, 0, 100)
+        .setPixel(0, 1, 100)
+        .setPixel(1, 0, 100)
+        .setPixel(1, 1, 100);
+}
+
+COMPLEX_FILTER_TEST(InversionTests, pixelsAreSwapped) {
+    typename TestFixture::template TestData<unsigned char>()
+        .setDimensions(2, 2)
+        .setPixel(0, 0, 12)
+        .setPixel(0, 1, 200)
+        .setPixel(1, 0, 200)
+        .setPixel(1, 1, 12);
+}
+
+COMPLEX_FILTER_TEST(InversionTests, pixelsAreSwappedAndMiddleValueIsKept) {
+    typename TestFixture::template TestData<unsigned char>()
+        .setDimensions(2, 2)
+        .setPixel(0, 0, 12)
+        .setPixel(0, 1, 106)
+        .setPixel(1, 0, 106)
+        .setPixel(1, 1, 200);
+}
+
+COMPLEX_FILTER_TEST(InversionTests, imageWithDifferentPixels) {
+    typename TestFixture::template TestData<unsigned char>()
+        .setDimensions(3, 2)
+        .setPixel(0, 0, 92)
+        .setPixel(0, 1, 100)
+        .setPixel(1, 0, 145)
+        .setPixel(1, 1, 140)
+        .setPixel(2, 0, 155)
+        .setPixel(2, 1, 200);
+}
+
+COMPLEX_FILTER_TEST(InversionTests, uniformImageIsDisplacedByCustomRange) {
+    typename TestFixture::template TestData<unsigned char>()
+        .setDimensions(2, 2)
+        .setRange(36, 140)
+        .setPixel(0, 0, 37)
+        .setPixel(0, 1, 37)
+        .setPixel(1, 0, 37)
+        .setPixel(1, 1, 37);
+}
+
+COMPLEX_FILTER_TEST(InversionTests, imageWithDifferentPixelsAndCustomRange) {
+    typename TestFixture::template TestData<unsigned char>()
+        .setDimensions(2, 3)
+        .setRange(22, 250)
+        .setPixel(0, 0, 40)
+        .setPixel(0, 1, 34)
+        .setPixel(0, 2, 249)
+        .setPixel(1, 0, 89)
+        .setPixel(1, 1, 200)
+        .setPixel(1, 2, 111);
+}
+
+COMPLEX_FILTER_TEST(InversionTests, floatingPointImage) {
+    typename TestFixture::template TestData<float>()
+        .setDimensions(3, 2)
+        .setPixel(0, 0, 0.008f)
+        .setPixel(0, 1, 12.75f)
+        .setPixel(1, 0, -100.2f)
+        .setPixel(1, 1, 1.1e4f)
+        .setPixel(2, 0, 1.1e7f)
+        .setPixel(2, 1, 9.2e7f);
+}
+
+COMPLEX_FILTER_TEST(InversionTests, floatingPointImageWithCustomRange) {
+    typename TestFixture::template TestData<float>()
+        .setDimensions(2, 3)
+        .setRange(-1.3e7f, 9.3e7f)
+        .setPixel(0, 0, 0.008f)
+        .setPixel(0, 1, 12.75f)
+        .setPixel(0, 2, -100.2f)
+        .setPixel(1, 0, 1.1e4f)
+        .setPixel(1, 1, 1.1e7f)
+        .setPixel(1, 2, 9.2e7f);
+}
+
+REGISTER_COMPLEX_FILTER_TEST_CASE(InversionTests, uniformImageIsntChanged,
+        pixelsAreSwapped, pixelsAreSwappedAndMiddleValueIsKept,
+        imageWithDifferentPixels, uniformImageIsDisplacedByCustomRange,
+        imageWithDifferentPixelsAndCustomRange, floatingPointImage,
+        floatingPointImageWithCustomRange);
+
+#endif
