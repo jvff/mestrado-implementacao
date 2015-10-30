adc92d4a475edfd0a27ece74841cff397082fce7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-10-30 18:21:52 -0200

    Test if InversionFilter is a ComplexFilter

--- src/test/cpp/InversionFilterTest.cpp
+++ src/test/cpp/InversionFilterTest.cpp
@@ -3,6 +3,7 @@
 #include "asserts.hpp"
 
 #include "InversionFilter.hpp"
+#include "InversionImplementation.hpp"
 
 #include "DummyTypes.hpp"
 #include "FakeImage.hpp"
@@ -12,4 +13,16 @@ TEST(InversionFilterTest, classTemplateExists) {
     using DummyFilter = InversionFilter<Image<DummyType>, ImageType>;
 
     AssertThat<DummyFilter>::isClassOrStruct();
+}
+
+TEST(InversionFilterTest, isComplexFilter) {
+    using SourceImageType = Image<DummyTypes<1> >;
+    using DestinationImageType = FakeImage<DummyTypes<2> >;
+    using ImplementationType = InversionImplementation<SourceImageType,
+            DestinationImageType>;
+    using FilterClass = InversionFilter<SourceImageType, DestinationImageType>;
+    using ParentFilterClass = ComplexFilter<SourceImageType,
+            DestinationImageType, ImplementationType>;
+
+    AssertThat<FilterClass>::isSubClass(Of<ParentFilterClass>());
 }
