f8b1915572cd509f82185db8968ab0e2f205c2b5
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-10-30 18:22:09 -0200

    Sub-class InversionFilter from ComplexFilter

--- include/main/cpp/InversionFilter.hpp
+++ include/main/cpp/InversionFilter.hpp
@@ -1,8 +1,14 @@
 #ifndef INVERSION_FILTER_HPP
 #define INVERSION_FILTER_HPP
 
+#include "ComplexFilter.hpp"
+#include "InversionImplementation.hpp"
+
 template <typename SourceImageType, typename DestinationImageType>
-class InversionFilter {
+class InversionFilter
+        : public ComplexFilter<SourceImageType, DestinationImageType,
+                InversionImplementation<SourceImageType,
+                        DestinationImageType> > {
 };
 
 #endif
