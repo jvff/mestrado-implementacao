31a6a739206081545c4daf1a9bfdb0cd6336a003
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-06 22:44:47 -0200

    Create empty AreaClosingImplementation class templ

---
+++ include/main/cpp/AreaClosingImplementation.hpp
@@ -0,0 +1,8 @@
+#ifndef AREA_CLOSING_IMPLEMENTATION_HPP
+#define AREA_CLOSING_IMPLEMENTATION_HPP
+
+template <typename SourceImageType, typename DestinationImageType>
+class AreaClosingImplementation {
+};
+
+#endif
