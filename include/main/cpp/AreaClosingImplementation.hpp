2b871c34eda2cc77ad5e5bff7adff626f7d03eb7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-09 17:21:26 -0200

    Sub-class area closing implem. from filter implem.

--- include/main/cpp/AreaClosingImplementation.hpp
+++ include/main/cpp/AreaClosingImplementation.hpp
@@ -1,8 +1,11 @@
 #ifndef AREA_CLOSING_IMPLEMENTATION_HPP
 #define AREA_CLOSING_IMPLEMENTATION_HPP
 
+#include "FilterImplementation.hpp"
+
 template <typename SourceImageType, typename DestinationImageType>
-class AreaClosingImplementation {
+class AreaClosingImplementation : public FilterImplementation<SourceImageType,
+        DestinationImageType> {
 };
 
 #endif
