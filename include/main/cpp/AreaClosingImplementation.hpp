771714659bc494e02c5d4efef946d007cd35892e
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-09 17:28:26 -0200

    Create AreaClosingImplementation constructor
    - Receives source image, destination image and maximum size of areas to
      be cleared
    - Override apply method to prevent class from being abstract

--- include/main/cpp/AreaClosingImplementation.hpp
+++ include/main/cpp/AreaClosingImplementation.hpp
@@ -6,6 +6,17 @@
 template <typename SourceImageType, typename DestinationImageType>
 class AreaClosingImplementation : public FilterImplementation<SourceImageType,
         DestinationImageType> {
+private:
+    using SuperClass = FilterImplementation<SourceImageType,
+            DestinationImageType>;
+
+public:
+    AreaClosingImplementation(unsigned int, const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage) {
+    }
+
+    void apply() override {
+    }
 };
 
 #endif
