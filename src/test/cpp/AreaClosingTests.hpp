d2f3ff84c07178628b325bc02cb212445f977cd4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-09 19:48:20 -0200

    Test if small hole is properly filled

--- src/test/cpp/AreaClosingTests.hpp
+++ src/test/cpp/AreaClosingTests.hpp
@@ -13,6 +13,15 @@ COMPLEX_FILTER_TEST(bigHoleIsntFilled) {
         .drawSquare(1, 1, 4, 100);
 }
 
-REGISTER_COMPLEX_FILTER_TEST_CASE(AreaClosingTests, bigHoleIsntFilled);
+COMPLEX_FILTER_TEST(smallHoleIsFilled) {
+    TestData<unsigned char>()
+        .setDimensions(6, 6)
+        .setMaximumExtremitySize(12)
+        .setBackground(199)
+        .drawSquare(1, 1, 2, 10);
+}
+
+REGISTER_COMPLEX_FILTER_TEST_CASE(AreaClosingTests, bigHoleIsntFilled,
+        smallHoleIsFilled);
 
 #endif
