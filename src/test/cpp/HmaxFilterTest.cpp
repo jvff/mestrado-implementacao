c4e490d50bc3d9627b2a9d4b3bf9ae766f272318
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-10 16:38:28 -0200

    Test if HmaxFilter is sub-class of Filter

--- src/test/cpp/HmaxFilterTest.cpp
+++ src/test/cpp/HmaxFilterTest.cpp
@@ -2,6 +2,7 @@
 
 #include "asserts.hpp"
 
+#include "Filter.hpp"
 #include "HmaxFilter.hpp"
 
 #include "DummyTypes.hpp"
@@ -12,4 +13,15 @@ TEST(HmaxFilterTest, classTemplateExists) {
     using DummyFilter = HmaxFilter<Image<DummyType>, DestinationImageType>;
 
     AssertThat<DummyFilter>::isClassOrStruct();
+}
+
+TEST(HmaxFilterTest, isFilter) {
+    using SourcePixelType = DummyTypes<1>;
+    using DestinationPixelType = DummyTypes<2>;
+    using SourceImageType = Image<SourcePixelType>;
+    using DestinationImageType = FakeImage<DestinationPixelType>;
+    using FilterClass = HmaxFilter<SourceImageType, DestinationImageType>;
+    using ParentFilter = Filter<SourceImageType, DestinationImageType>;
+
+    AssertThat<FilterClass>::isSubClass(Of<ParentFilter>());
 }
