0b4a1efe2a457b7740c21e9cbe9ef30c7de2a32f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-10 19:11:39 +0000

    Implement HmaxFilter
    - Create the marker image using a displaced source image
    - Apply a morphological reconstruction filter

--- include/main/cpp/HmaxFilter.hpp
+++ include/main/cpp/HmaxFilter.hpp
@@ -1,18 +1,42 @@
 #ifndef HMAX_FILTER_HPP
 #define HMAX_FILTER_HPP
 
+#include "MorphologicalReconstructionFilter.hpp"
 #include "Filter.hpp"
 
 template <typename SourceImageType, typename DestinationImageType>
 class HmaxFilter : public Filter<SourceImageType, DestinationImageType> {
 private:
+    using DestinationPixelType = typename DestinationImageType::PixelType;
+    using MorphologicalReconstructionFilterType =
+            MorphologicalReconstructionFilter<SourceImageType, DestinationImageType>;
     using SourcePixelType = typename SourceImageType::PixelType;
+    using SuperClass = Filter<SourceImageType, DestinationImageType>;
+
+    MorphologicalReconstructionFilterType morphologicalReconstructionFilter;
+    SourcePixelType featureHeight;
 
 public:
-    HmaxFilter(const SourcePixelType&) {
+    HmaxFilter(const SourcePixelType& featureHeight)
+            : featureHeight(featureHeight) {
     }
 
-    void apply(const SourceImageType&, DestinationImageType&) override {
+    void apply(const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage) override {
+        setUpMarkerImage(sourceImage, destinationImage);
+
+        morphologicalReconstructionFilter.apply(sourceImage, destinationImage);
+    }
+
+    using SuperClass::apply;
+
+private:
+    void setUpMarkerImage(const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage) {
+        destinationImage = [&] (unsigned int x, unsigned int y)
+                -> DestinationPixelType {
+            return sourceImage.getPixelValue(x, y) - featureHeight;
+        };
     }
 };
 
