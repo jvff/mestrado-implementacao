8f4290fb15d8a9b33b283cb35b2cd65899b8ba52
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-10 19:52:55 +0000

    Make HminImplem. sub-class FilterImplementation

--- include/main/cpp/HminImplementation.hpp
+++ include/main/cpp/HminImplementation.hpp
@@ -1,8 +1,11 @@
 #ifndef HMIN_IMPLEMENTATION_HPP
 #define HMIN_IMPLEMENTATION_HPP
 
+#include "FilterImplementation.hpp"
+
 template <typename SourceImageType, typename DestinationImageType>
-class HminImplementation {
+class HminImplementation : public FilterImplementation<SourceImageType,
+        DestinationImageType> {
 };
 
 #endif
