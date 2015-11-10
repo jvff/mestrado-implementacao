a3f26f97609687e2a01da8f86754ff18e48f23e8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-10 18:54:05 +0000

    Test if HminFilter removes shallow hole
    - Create HminFilterTestData class to help with the test set up

--- src/test/cpp/HmaxFilterTest.cpp
+++ src/test/cpp/HmaxFilterTest.cpp
@@ -16,3 +16,12 @@ TEST_F(HmaxFilterTest, isConstructibleWithParameter) {
 
     AssertThat<DummyFilter>::isConstructible(With<FeatureHeightParameter>());
 }
+
+TEST_F(HmaxFilterTest, removesShallowHole) {
+    TestData<unsigned char>()
+        .setDimensions(7, 7)
+        .setFeatureHeight(4)
+        .setBackground(124)
+        .drawSquare(1, 1, 5, 121)
+        .setExpectedBackground(120);
+}
--- src/test/cpp/HmaxFilterTest.hpp
+++ src/test/cpp/HmaxFilterTest.hpp
@@ -7,9 +7,13 @@
 
 #include "Filter.hpp"
 #include "HmaxFilter.hpp"
+#include "SimpleArrayImage.hpp"
 
+#include "AbstractFilterTestData.hpp"
+#include "ChainableMethodMacros.hpp"
 #include "DummyTypes.hpp"
 #include "FakeImage.hpp"
+#include "RectanglePainter.hpp"
 
 class HmaxFilterTest : public ::testing::Test {
 protected:
@@ -19,5 +23,68 @@ protected:
     using DestinationImageType = FakeImage<DestinationPixelType>;
     using DummyFilter = HmaxFilter<SourceImageType, DestinationImageType>;
 };
+
+template <typename PixelType, typename ImageType = SimpleArrayImage<PixelType> >
+class HmaxFilterTestData : public AbstractFilterTestData<
+        HmaxFilter<ImageType, ImageType>, ImageType, ImageType> {
+private:
+    using FilterType = HmaxFilter<ImageType, ImageType>;
+    using PainterType = RectanglePainter<ImageType>;
+    using State = AbstractTestData::State;
+    using SuperClass = AbstractFilterTestData<FilterType, ImageType, ImageType>;
+    using ThisType = HmaxFilterTestData<PixelType, ImageType>;
+
+    std::unique_ptr<PainterType> sourcePainter;
+    std::unique_ptr<PainterType> expectedPainter;
+
+public:
+    virtual ~HmaxFilterTestData() {
+        this->tryToRunTest();
+    }
+
+    CHAIN(setDimensions, unsigned int width, unsigned int height) {
+        if (SuperClass::setDimensions(width, height)) {
+            sourcePainter.reset(new PainterType(*this->sourceImage));
+            expectedPainter.reset(new PainterType(*this->expectedImage));
+
+            this->state = State::SETTING_UP;
+        }
+    }
+
+    CHAIN(setFeatureHeight, const PixelType& featureHeight) {
+        if (stateIs(State::SETTING_UP)) {
+            this->initializeFilter(featureHeight);
+
+            this->state = State::READY;
+        }
+    }
+
+    CHAIN(setBackground, const PixelType& color) {
+        if (stateIs(State::READY))
+            sourcePainter->fill(color);
+    }
+
+    CHAIN(setExpectedBackground, const PixelType& color) {
+        if (stateIs(State::READY))
+            expectedPainter->fill(color);
+    }
+
+    CHAIN(drawSquare, unsigned int x, unsigned int y, unsigned int size,
+            const PixelType& color) {
+        if (stateIs(State::READY))
+            sourcePainter->drawSquare(x, y, size, color);
+    }
+
+protected:
+    using SuperClass::cancelTestExecution;
+    using SuperClass::stateIs;
+
+    void finishSetUp() override {
+        cancelTestExecution();
+    }
+};
+
+template <typename PixelType, typename ImageType = SimpleArrayImage<PixelType> >
+using TestData = HmaxFilterTestData<PixelType, ImageType>;
 
 #endif
