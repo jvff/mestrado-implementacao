b0b4da676c06607e31f6696fc6bce1bea5a57412
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-10 19:51:46 +0000

    Test for HminImplementation's parent class

--- src/test/cpp/HminImplementationTest.cpp
+++ src/test/cpp/HminImplementationTest.cpp
@@ -2,6 +2,7 @@
 
 #include "asserts.hpp"
 
+#include "FilterImplementation.hpp"
 #include "HminImplementation.hpp"
 
 #include "DummyTypes.hpp"
@@ -16,4 +17,17 @@ TEST(HminImplementationTest, classTemplateExists) {
             DestinationImageType>;
 
     AssertThat<DummyImplementation>::isClassOrStruct();
+}
+
+TEST(HminImplementationTest, isFilterImplementation) {
+    using SourcePixelType = DummyTypes<1>;
+    using DestinationPixelType = DummyTypes<2>;
+    using SourceImageType = Image<SourcePixelType>;
+    using DestinationImageType = FakeImage<DestinationPixelType>;
+    using ImplementationClass = HminImplementation<SourceImageType,
+            DestinationImageType>;
+    using ParentImplementation = FilterImplementation<SourceImageType,
+            DestinationImageType>;
+
+    AssertThat<ImplementationClass>::isSubClass(Of<ParentImplementation>());
 }
