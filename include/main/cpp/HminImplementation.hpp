efa54814b2f3c6e0002037563255accc0aa0281c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-11 18:33:25 +0000

    Create HminImplementation parameter constructor
    - Also override apply() method so that the class can be constructed

--- include/main/cpp/HminImplementation.hpp
+++ include/main/cpp/HminImplementation.hpp
@@ -6,6 +6,16 @@
 template <typename SourceImageType, typename DestinationImageType>
 class HminImplementation : public FilterImplementation<SourceImageType,
         DestinationImageType> {
+private:
+    using SourcePixelType = typename SourceImageType::PixelType;
+
+public:
+    HminImplementation(const SourcePixelType&, const SourceImageType&,
+            DestinationImageType&) {
+    }
+
+    void apply() override {
+    }
 };
 
 #endif
