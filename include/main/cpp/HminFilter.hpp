75e979d69f53ef60834d0f340a1bfa0da65201b4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-11 21:02:15 -0200

    Sub-class HminFilter from ComplexFilter

--- include/main/cpp/HminFilter.hpp
+++ include/main/cpp/HminFilter.hpp
@@ -1,8 +1,12 @@
 #ifndef HMIN_FILTER_HPP
 #define HMIN_FILTER_HPP
 
+#include "ComplexFilter.hpp"
+#include "HminImplementation.hpp"
+
 template <typename SourceImageType, typename DestinationImageType>
-class HminFilter {
+class HminFilter : public ComplexFilter<SourceImageType, DestinationImageType,
+        HminImplementation<SourceImageType, DestinationImageType> > {
 };
 
 #endif
