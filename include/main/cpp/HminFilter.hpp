97104f25b7919bc8a6fe2c82ba7df021dc55df1c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-11 22:01:34 -0200

    Implement HminFilter
    - Create constructor with feature height parameter
    - Implement initializeFilter method to initialize the HminImplementation

--- include/main/cpp/HminFilter.hpp
+++ include/main/cpp/HminFilter.hpp
@@ -7,6 +7,24 @@
 template <typename SourceImageType, typename DestinationImageType>
 class HminFilter : public ComplexFilter<SourceImageType, DestinationImageType,
         HminImplementation<SourceImageType, DestinationImageType> > {
+private:
+    using ImplementationType = HminImplementation<SourceImageType,
+            DestinationImageType>;
+    using SourcePixelType = typename SourceImageType::PixelType;
+
+    SourcePixelType featureHeight;
+
+public:
+    HminFilter(const SourcePixelType& featureHeight)
+            : featureHeight(featureHeight) {
+    }
+
+protected:
+    ImplementationType instantiateImplementation(
+            const SourceImageType& sourceImage,
+            DestinationImageType& destinationImage) {
+        return ImplementationType(featureHeight, sourceImage, destinationImage);
+    }
 };
 
 #endif
