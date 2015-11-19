b1f9670581a7eed98d5d4fa3a8e0df3fa0fccfb5
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-19 18:48:55 -0200

    Refactor RgbImage into empty class template

--- include/main/cpp/RgbImage.hpp
+++ include/main/cpp/RgbImage.hpp
@@ -1,12 +1,8 @@
 #ifndef RGB_IMAGE_HPP
 #define RGB_IMAGE_HPP
 
-#include "Image.hpp"
-
-class RgbImage : public Image<int> {
-public:
-    RgbImage(int width, int height) : Image(width, height) {
-    }
+template <typename InternalImageType>
+class RgbImage {
 };
 
 #endif
