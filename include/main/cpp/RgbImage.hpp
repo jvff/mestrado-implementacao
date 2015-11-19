a4761a5f9ad24ffb4ee796dab0245957bd6f62e1
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-19 20:03:22 -0200

    Sub-class RgbImage from Image
    - Retrieve PixelType from InternalImageType

--- include/main/cpp/RgbImage.hpp
+++ include/main/cpp/RgbImage.hpp
@@ -1,8 +1,10 @@
 #ifndef RGB_IMAGE_HPP
 #define RGB_IMAGE_HPP
 
+#include "Image.hpp"
+
 template <typename InternalImageType>
-class RgbImage {
+class RgbImage : public Image<typename InternalImageType::PixelType> {
 };
 
 #endif
