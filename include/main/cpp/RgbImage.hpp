b7c776ff9ae9ba011b08d9a5eca7523e9d49217b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-20 11:40:04 -0200

    Refactor RgbImage to support only integral types

--- include/main/cpp/RgbImage.hpp
+++ include/main/cpp/RgbImage.hpp
@@ -1,12 +1,21 @@
 #ifndef RGB_IMAGE_HPP
 #define RGB_IMAGE_HPP
 
+#include <type_traits>
+
 #include "Image.hpp"
 
 template <typename InternalImageType>
 class RgbImage : public Image<typename InternalImageType::PixelType> {
 private:
     using PixelType = typename InternalImageType::PixelType;
+
+    static constexpr bool pixelTypeIsSupported() {
+        return std::is_integral<PixelType>::value
+            && !std::is_same<PixelType, bool>::value;
+    }
+
+    static_assert(pixelTypeIsSupported(), "Unsupported pixel type");
 
 public:
     RgbImage(InternalImageType&) {
