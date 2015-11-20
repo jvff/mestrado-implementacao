9944085a1a4d6565af775d56f9e775165dbb9cee
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-20 11:25:05 -0200

    Create initial RgbImage constructor with parameter
    - Parameter is the internal image to use
    - Override getPixelValue and setPixel methods to make the class
      constructible

--- include/main/cpp/RgbImage.hpp
+++ include/main/cpp/RgbImage.hpp
@@ -5,6 +5,19 @@
 
 template <typename InternalImageType>
 class RgbImage : public Image<typename InternalImageType::PixelType> {
+private:
+    using PixelType = typename InternalImageType::PixelType;
+
+public:
+    RgbImage(InternalImageType&) {
+    }
+
+    void setPixel(unsigned int x, unsigned int y, PixelType value) override {
+    }
+
+    PixelType getPixelValue(unsigned int x, unsigned int y) const override {
+        return 0;
+    }
 };
 
 #endif
