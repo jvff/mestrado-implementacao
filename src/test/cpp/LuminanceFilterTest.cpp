818f929867fe7508d9fb2b70983ee86785feb675
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-20 19:30:00 -0200

    Refactor to create LuminanceFilterTest fixture
    - Centralize common aliases

--- src/test/cpp/LuminanceFilterTest.cpp
+++ src/test/cpp/LuminanceFilterTest.cpp
@@ -1,31 +1,11 @@
-#include <gtest/gtest.h>
+#include "LuminanceFilterTest.hpp"
 
-#include "asserts.hpp"
-
-#include "Filter.hpp"
-#include "LuminanceFilter.hpp"
-
-#include "DummyTypes.hpp"
-#include "FakeImage.hpp"
-
-TEST(LuminanceFilter, classTemplateExists) {
-    using SourcePixelType = DummyTypes<1>;
-    using DestinationPixelType = DummyTypes<2>;
-    using SourceImageType = Image<SourcePixelType>;
-    using DestinationImageType = FakeImage<DestinationPixelType>;
-    using DummyFilter = LuminanceFilter<SourceImageType, DestinationImageType>;
-
+TEST_F(LuminanceFilterTest, classTemplateExists) {
     AssertThat<DummyFilter>::isClassOrStruct();
 }
 
-TEST(LuminanceFilter, isSubClassOfFilter) {
-    using SourcePixelType = DummyTypes<1>;
-    using DestinationPixelType = DummyTypes<2>;
-    using SourceImageType = Image<SourcePixelType>;
-    using DestinationImageType = FakeImage<DestinationPixelType>;
-    using DummyFilterClass = LuminanceFilter<SourceImageType,
-            DestinationImageType>;
-    using ParentFilterClass = Filter<SourceImageType, DestinationImageType>;
+TEST_F(LuminanceFilterTest, isSubClassOfFilter) {
+    using ParentFilter = Filter<SourceImageType, DestinationImageType>;
 
-    AssertThat<DummyFilterClass>::isSubClass(Of<ParentFilterClass>());
+    AssertThat<DummyFilter>::isSubClass(Of<ParentFilter>());
 }
---
+++ src/test/cpp/LuminanceFilterTest.hpp
@@ -0,0 +1,23 @@
+#ifndef LUMINANCE_FILTER_TEST_HPP
+#define LUMINANCE_FILTER_TEST_HPP
+
+#include <gtest/gtest.h>
+
+#include "asserts.hpp"
+
+#include "Filter.hpp"
+#include "LuminanceFilter.hpp"
+
+#include "DummyTypes.hpp"
+#include "FakeImage.hpp"
+
+class LuminanceFilterTest : public ::testing::Test {
+protected:
+    using SourcePixelType = DummyTypes<1>;
+    using DestinationPixelType = DummyTypes<2>;
+    using SourceImageType = Image<SourcePixelType>;
+    using DestinationImageType = FakeImage<DestinationPixelType>;
+    using DummyFilter = LuminanceFilter<SourceImageType, DestinationImageType>;
+};
+
+#endif
