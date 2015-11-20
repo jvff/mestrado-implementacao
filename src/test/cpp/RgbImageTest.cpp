00f49a1f17685de53b925b712744873dc39481ae
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-20 12:02:03 -0200

    Test if RgbImage reads from internal image
    - Test image dimensions and image pixels

--- src/test/cpp/RgbImageTest.cpp
+++ src/test/cpp/RgbImageTest.cpp
@@ -1,11 +1,15 @@
 #include <gtest/gtest.h>
 
+#include "fakeit.hpp"
+
 #include "asserts.hpp"
 
 #include "RgbImage.hpp"
 
 #include "DummyTypes.hpp"
 #include "FakeImage.hpp"
+
+using namespace fakeit;
 
 TEST(RgbImageTest, classTemplateExists) {
     using InternalImageType = FakeImage<unsigned int>;
@@ -30,4 +34,37 @@ TEST(RgbImageTest, isConstructibleWithUnsignedIntImage) {
     using IntImageParameter = InternalImageType&;
 
     AssertThat<IntRgbImageClass>::isConstructible(With<IntImageParameter>());
+}
+
+TEST(RgbImageTest, usesInternalImage) {
+    using PixelType = unsigned int;
+    using InternalImageType = FakeImage<PixelType>;
+
+    unsigned int width = 8;
+    unsigned int height = 5;
+
+    Mock<InternalImageType> mockImage;
+    InternalImageType& internalImage = mockImage.get();
+
+    auto returnPixel = [width] (unsigned int x, unsigned int y) -> PixelType {
+        return y * width + x;
+    };
+
+    When(Method(mockImage, getWidth)).AlwaysReturn(width);
+    When(Method(mockImage, getHeight)).AlwaysReturn(height);
+    When(Method(mockImage, getPixelValue)).AlwaysDo(returnPixel);
+
+    RgbImage<InternalImageType> rgbImage(internalImage);
+
+    assertThat(rgbImage.getWidth()).isEqualTo(width);
+    assertThat(rgbImage.getHeight()).isEqualTo(height);
+
+    for (unsigned int x = 0; x < width; ++x) {
+        for (unsigned int y = 0; y < height; ++y) {
+            auto pixelValue = rgbImage.getPixelValue(x, y);
+            auto expectedValue = internalImage.getPixelValue(x, y);
+
+            assertThat(pixelValue).isEqualTo(expectedValue);
+        }
+    }
 }
