09f928b1437834105ca8e511c40fff1de77cb430
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-20 16:08:14 -0200

    Implement color channel extraction methods

--- include/main/cpp/RgbImage.hpp
+++ include/main/cpp/RgbImage.hpp
@@ -21,10 +21,35 @@ private:
 private:
     InternalImageType& internalImage;
 
+    unsigned int redChannelShiftAmount;
+    unsigned int greenChannelShiftAmount;
+    unsigned int blueChannelShiftAmount;
+
+    unsigned int redChannelMask;
+    unsigned int greenChannelMask;
+    unsigned int blueChannelMask;
+
 public:
     RgbImage(InternalImageType& internalImage)
             : SuperClass(internalImage.getWidth(), internalImage.getHeight()),
             internalImage(internalImage) {
+        unsigned int availableBits = 8 * sizeof(PixelType);
+        unsigned int numberOfChannels = 3;
+
+        auto bitsPerChannel = availableBits / numberOfChannels;
+        auto remainingBits = availableBits - numberOfChannels * bitsPerChannel;
+
+        auto redChannelBits = bitsPerChannel;
+        auto greenChannelBits = bitsPerChannel + remainingBits;
+        auto blueChannelBits = bitsPerChannel;
+
+        redChannelShiftAmount = greenChannelBits + blueChannelBits;
+        greenChannelShiftAmount = blueChannelBits;
+        blueChannelShiftAmount = 0;
+
+        redChannelMask = (1 << redChannelBits) - 1;
+        greenChannelMask = (1 << greenChannelBits) - 1;
+        blueChannelMask = (1 << blueChannelBits) - 1;
     }
 
     void setPixel(unsigned int x, unsigned int y, PixelType value) override {
@@ -35,7 +60,28 @@ public:
         return internalImage.getPixelValue(x, y);
     }
 
+    PixelType getRedComponent(unsigned int x, unsigned int y) const {
+        return getColorComponent(x, y, redChannelShiftAmount, redChannelMask);
+    }
+
+    PixelType getGreenComponent(unsigned int x, unsigned int y) const {
+        return getColorComponent(x, y, greenChannelShiftAmount,
+                greenChannelMask);
+    }
+
+    PixelType getBlueComponent(unsigned int x, unsigned int y) const {
+        return getColorComponent(x, y, blueChannelShiftAmount, blueChannelMask);
+    }
+
     using SuperClass::operator=;
+
+private:
+    PixelType getColorComponent(unsigned int x, unsigned int y,
+            unsigned int channelShiftAmount, PixelType channelMask) const {
+        PixelType value = internalImage.getPixelValue(x, y);
+
+        return (value >> channelShiftAmount) & channelMask;
+    }
 };
 
 #endif
