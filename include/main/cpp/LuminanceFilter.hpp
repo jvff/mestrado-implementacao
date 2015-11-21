74ffbc0d1dd007135b6a24b96cb65b313c6dd442
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-21 14:35:45 -0200

    Implement LuminanceFilter<RgbImage> specialization

--- include/main/cpp/LuminanceFilter.hpp
+++ include/main/cpp/LuminanceFilter.hpp
@@ -1,7 +1,11 @@
 #ifndef LUMINANCE_FILTER_HPP
 #define LUMINANCE_FILTER_HPP
 
+#include <cmath>
+
 #include "Filter.hpp"
+#include "RgbImage.hpp"
+#include "SimpleFilter.hpp"
 
 template <typename SourceImageType, typename DestinationImageType>
 class LuminanceFilter : public Filter<SourceImageType, DestinationImageType> {
@@ -15,6 +19,44 @@ public:
     }
 
     using SuperClass::apply;
+};
+
+template <typename InternalImageType, typename DestinationImageType>
+class LuminanceFilter<RgbImage<InternalImageType>, DestinationImageType>
+        : public SimpleFilter<RgbImage<InternalImageType>,
+                DestinationImageType> {
+private:
+    using DestinationPixelType = typename DestinationImageType::PixelType;
+    using SourceImageType = RgbImage<InternalImageType>;
+    using SuperClass = SimpleFilter<SourceImageType, DestinationImageType>;
+
+public:
+    using SuperClass::apply;
+
+protected:
+    DestinationPixelType apply(unsigned int x, unsigned int y,
+            const SourceImageType& sourceImage) {
+        float redComponent = sourceImage.getRelativeRedComponent(x, y);
+        float greenComponent = sourceImage.getRelativeGreenComponent(x, y);
+        float blueComponent = sourceImage.getRelativeBlueComponent(x, y);
+
+        float redContribution = 0.2126 * redComponent;
+        float greenContribution = 0.7152 * greenComponent;
+        float blueContribution = 0.0722 * blueComponent;
+
+        float luminance = 0.f;
+        float conversionFactor = getMaximumLuminanceValue();
+
+        luminance += redContribution;
+        luminance += greenContribution;
+        luminance += blueContribution;
+
+        return (DestinationPixelType)std::round(luminance * conversionFactor);
+    }
+
+    float getMaximumLuminanceValue() {
+        return std::numeric_limits<DestinationPixelType>::max();
+    }
 };
 
 #endif
