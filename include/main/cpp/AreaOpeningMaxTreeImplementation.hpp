e732e8b45061d944a5b5392555426ec4a7acfb02
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-23 21:32:23 -0200

    Sub-class from FilterImplementation

--- include/main/cpp/AreaOpeningMaxTreeImplementation.hpp
+++ include/main/cpp/AreaOpeningMaxTreeImplementation.hpp
@@ -1,8 +1,11 @@
 #ifndef AREA_OPENING_MAX_TREE_IMPLEMENTATION_HPP
 #define AREA_OPENING_MAX_TREE_IMPLEMENTATION_HPP
 
+#include "FilterImplementation.hpp"
+
 template <typename SourceImageType, typename DestinationImageType>
-class AreaOpeningMaxTreeImplementation {
+class AreaOpeningMaxTreeImplementation
+        : public FilterImplementation<SourceImageType, DestinationImageType> {
 };
 
 #endif
