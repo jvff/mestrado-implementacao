b7e10be8d3080efe0470be2421fda6ba8d9b0849
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-23 21:30:07 -0200

    Create empty AreaOpeningMaxTreeImplementation

---
+++ include/main/cpp/AreaOpeningMaxTreeImplementation.hpp
@@ -0,0 +1,8 @@
+#ifndef AREA_OPENING_MAX_TREE_IMPLEMENTATION_HPP
+#define AREA_OPENING_MAX_TREE_IMPLEMENTATION_HPP
+
+template <typename SourceImageType, typename DestinationImageType>
+class AreaOpeningMaxTreeImplementation {
+};
+
+#endif
