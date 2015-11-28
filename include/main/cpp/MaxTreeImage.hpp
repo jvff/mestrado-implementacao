4d3582c5a7e710779ab5848ff8aea200333532ca
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-28 12:59:46 -0200

    Sub-class MaxTreeImage from Image
    - Using the InternalImageType's PixelType

--- include/main/cpp/MaxTreeImage.hpp
+++ include/main/cpp/MaxTreeImage.hpp
@@ -1,8 +1,10 @@
 #ifndef MAX_TREE_IMAGE_HPP
 #define MAX_TREE_IMAGE_HPP
 
+#include "Image.hpp"
+
 template <typename InternalImageType>
-class MaxTreeImage {
+class MaxTreeImage : public Image<typename InternalImageType::PixelType> {
 };
 
 #endif
