83214f0bebfa04bb2cf95c7a2f11d96d655f9ec1
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-28 16:20:44 -0200

    Add members to MaxTreeNode

--- include/main/cpp/MaxTreeNode.hpp
+++ include/main/cpp/MaxTreeNode.hpp
@@ -1,8 +1,13 @@
 #ifndef MAX_TREE_NODE_HPP
 #define MAX_TREE_NODE_HPP
 
+#include <memory>
+
 template <typename PixelType>
 struct MaxTreeNode {
+    std::shared_ptr<MaxTreeNode<PixelType> > parent;
+    PixelType level;
+    unsigned int id;
 };
 
 #endif
