955a76ca14fac865cf007f00129699a757577947
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-28 16:19:29 -0200

    Test if MaxTreeNode struct has expected members

--- src/test/cpp/MaxTreeNodeTest.cpp
+++ src/test/cpp/MaxTreeNodeTest.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+
 #include <gtest/gtest.h>
 
 #include "asserts.hpp"
@@ -8,4 +10,16 @@
 
 TEST(MaxTreeNodeTest, structTemplateExists) {
     AssertThat<MaxTreeNode<DummyType> >::isClassOrStruct();
+}
+
+TEST(MaxTreeNodeTest, hasValueMembers) {
+    std::shared_ptr<MaxTreeNode<DummyType> > parent;
+    DummyType level = { 7 };
+    unsigned int id = 10;
+
+    MaxTreeNode<DummyType> node = { parent, level, id };
+
+    assertThat(node.id).isEqualTo(id);
+    assertThat(node.level).isEqualTo(level);
+    assertThat((bool)node.parent).isEqualTo(false);
 }
