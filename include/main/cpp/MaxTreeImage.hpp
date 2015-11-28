269c664331bcd0e5c5b6e15d4132791bd4e15581
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-28 19:07:08 -0200

    Implement initial MaxTree construction operation
    - For now, assignPixelToLatest node is the only construction operation
    - Creates a level for the pixel node if necessary
    - Store levels in a map between the level and the list of nodes in that
      level
    - Assigns the pixel to the latest node in the pixel level
    - Use an image of unsigned ints to store the node ID of every pixel
    - Use a noParent pointer to indicate that the node is the root node
    - Properly implement getNodeOfPixel method

--- include/main/cpp/MaxTreeImage.hpp
+++ include/main/cpp/MaxTreeImage.hpp
@@ -1,21 +1,33 @@
 #ifndef MAX_TREE_IMAGE_HPP
 #define MAX_TREE_IMAGE_HPP
 
+#include <map>
+#include <vector>
+
 #include "Image.hpp"
 #include "MaxTreeNode.hpp"
+#include "SimpleArrayImage.hpp"
 
 template <typename InternalImageType>
 class MaxTreeImage : public Image<typename InternalImageType::PixelType> {
 private:
     using PixelType = typename InternalImageType::PixelType;
     using NodeType = MaxTreeNode<PixelType>;
+    using NodePointerType = std::shared_ptr<NodeType>;
+    using NodeList = std::vector<NodePointerType>;
     using SuperClass = Image<PixelType>;
 
     InternalImageType internalImage;
+    SimpleArrayImage<unsigned int> nodeIdImage;
+
+    std::map<PixelType, NodeList> treeLevels;
+
+    NodePointerType noParent;
 
 public:
     MaxTreeImage(unsigned int width, unsigned int height)
-            : SuperClass(width, height), internalImage(width, height) {
+            : SuperClass(width, height), internalImage(width, height),
+            nodeIdImage(width, height) {
     }
 
     void setPixel(unsigned int x, unsigned int y, PixelType value) override {
@@ -26,16 +38,87 @@ public:
         return internalImage.getPixelValue(x, y);
     }
 
-    void assignPixelToLatestNode(unsigned int, unsigned int) {
+    void assignPixelToLatestNode(unsigned int x, unsigned int y) {
+        auto level = getPixelValue(x, y);
+        auto& latestNode = getLatestNodeAtLevel(level);
+
+        nodeIdImage.setPixel(x, y, latestNode->id);
     }
 
-    NodeType getNodeOfPixel(unsigned int x, unsigned int y) {
-        NodeType node;
+    const NodeType& getNodeOfPixel(unsigned int x, unsigned int y) {
+        auto level = internalImage.getPixelValue(x, y);
+        auto nodeId = nodeIdImage.getPixelValue(x, y);
+        auto& nodePointer = getNode(level, nodeId);
 
-        node.level = getPixelValue(x, y);
-        node.id = 0;
+        return *nodePointer;
+    }
 
-        return node;
+private:
+    NodePointerType& getNode(PixelType level, unsigned int nodeId) {
+        auto& nodesAtLevel = getNodesAtLevel(level);
+
+        return nodesAtLevel[nodeId];
+    }
+
+    NodePointerType& getLatestNodeAtLevel(PixelType level) {
+        auto& nodesAtLevel = getNodesAtLevel(level);
+
+        return nodesAtLevel.back();
+    }
+
+    NodeList& getNodesAtLevel(PixelType level) {
+        if (treeDoesNotHaveLevel(level))
+            createNewLevel(level);
+
+        return treeLevels[level];
+    }
+
+    bool treeDoesNotHaveLevel(PixelType level) {
+        auto notFound = treeLevels.end();
+
+        return treeLevels.find(level) == notFound;
+    }
+
+    void createNewLevel(PixelType level) {
+        NodeList nodesAtLevel;
+        NodePointerType newNode(new NodeType);
+        auto& parent = getLatestNodeAtLevelBefore(level);
+
+        newNode->level = level;
+        newNode->id = 0;
+        newNode->parent = parent;
+
+        nodesAtLevel.push_back(newNode);
+
+        treeLevels[level] = nodesAtLevel;
+    }
+
+    NodePointerType& getLatestNodeAtLevelBefore(PixelType level) {
+        if (treeLevels.empty() || level == getFirstLevel())
+            return noParent;
+
+        auto previousLevel = getLevelBefore(level);
+
+        return getLatestNodeAtLevel(previousLevel);
+    }
+
+    PixelType getFirstLevel() {
+        auto firstLevelPosition = treeLevels.begin();
+        auto firstLevelKeyValuePair = *firstLevelPosition;
+        auto firstLevel = std::get<0>(firstLevelKeyValuePair);
+
+        return firstLevel;
+    }
+
+    PixelType getLevelBefore(PixelType level) {
+        auto position = treeLevels.find(level);
+
+        --position;
+
+        auto keyValuePair = *position;
+        auto previousLevel = std::get<0>(keyValuePair);
+
+        return previousLevel;
     }
 };
 
