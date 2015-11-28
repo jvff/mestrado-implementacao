c382de91671adfc20ce19b0a3170fab2aa4e6dbb
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-28 13:12:03 -0200

    Make MaxTreeImage constructible
    - Receive width and height parameters in constructor
    - Construct parent class with the same parameters
    - Stub setPixel method to make it concrete
    - Implement getPixelValue method, getting the respective pixel from an
      internal image
    - Initialize internal image with correct dimensions

--- include/main/cpp/MaxTreeImage.hpp
+++ include/main/cpp/MaxTreeImage.hpp
@@ -5,6 +5,23 @@
 
 template <typename InternalImageType>
 class MaxTreeImage : public Image<typename InternalImageType::PixelType> {
+private:
+    using PixelType = typename InternalImageType::PixelType;
+    using SuperClass = Image<PixelType>;
+
+    InternalImageType internalImage;
+
+public:
+    MaxTreeImage(unsigned int width, unsigned int height)
+            : SuperClass(width, height), internalImage(width, height) {
+    }
+
+    void setPixel(unsigned int x, unsigned int y, PixelType value) override {
+    }
+
+    PixelType getPixelValue(unsigned int x, unsigned int y) const override {
+        return internalImage.getPixelValue(x, y);
+    }
 };
 
 #endif
