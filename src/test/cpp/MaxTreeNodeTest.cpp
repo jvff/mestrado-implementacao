58d8de8835665f463ed019b1b16bcc07cc6a00c4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-28 16:27:37 -0200

    Test MaxTreeNode parent values

--- src/test/cpp/MaxTreeNodeTest.cpp
+++ src/test/cpp/MaxTreeNodeTest.cpp
@@ -23,3 +23,26 @@ TEST(MaxTreeNodeTest, hasValueMembers) {
     assertThat(node.level).isEqualTo(level);
     assertThat((bool)node.parent).isEqualTo(false);
 }
+
+TEST(MaxTreeNodeTest, hasParentMember) {
+    std::shared_ptr<MaxTreeNode<DummyType> > noParent;
+    std::shared_ptr<MaxTreeNode<DummyType> > rootNode;
+
+    DummyType parentLevel = { 90 };
+    unsigned int parentId = 3;
+    MaxTreeNode<DummyType> parent = { noParent, parentLevel, parentId };
+
+    rootNode.reset(new MaxTreeNode<DummyType>(parent));
+
+    DummyType childLevel = { 91 };
+    unsigned int childId = 10;
+    MaxTreeNode<DummyType> child = { rootNode, childLevel, childId };
+
+    assertThat(child.level).isEqualTo(childLevel);
+    assertThat(child.id).isEqualTo(childId);
+
+    assertThat((bool)child.parent).isEqualTo(true);
+    assertThat(child.parent->level).isEqualTo(parentLevel);
+    assertThat(child.parent->id).isEqualTo(parentId);
+    assertThat((bool)child.parent->parent).isEqualTo(false);
+}
