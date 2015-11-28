989afa85d4fd293b7a560bd61be43a3561668e59
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-28 21:58:13 -0200

    Create initial MaxTreeImplementation class templ.

---
+++ include/main/cpp/MaxTreeImplementation.hpp
@@ -0,0 +1,8 @@
+#ifndef MAX_TREE_IMPLEMENTATION_HPP
+#define MAX_TREE_IMPLEMENTATION_HPP
+
+template <typename SourceImageType, typename InternalImageType>
+class MaxTreeImplementation {
+};
+
+#endif
