95e10ec31bceb4644abacb2130551133c9b1e2cd
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-29 11:06:05 -0200

    Test if all nodes are assigned to root node
    - Use a source image filled with a single color

--- src/test/cpp/MaxTreeImplementationTest.cpp
+++ src/test/cpp/MaxTreeImplementationTest.cpp
@@ -18,3 +18,36 @@ TEST_F(MaxTreeImplementationTest, isConstructibleWithParameters) {
     AssertThat<DummyImplementationType>::isConstructible(With<
             SourceImageParameter, DestinationImageParameter>());
 }
+
+TEST_F(MaxTreeImplementationTest, allNodesAreAssignedToRootNode) {
+    using PixelType = unsigned char;
+    using SourceImageType = SimpleArrayImage<PixelType>;
+    using DestinationImageType = MaxTreeImage<SourceImageType>;
+    using ImplementationType = MaxTreeImplementation<SourceImageType,
+            SourceImageType>;
+
+    unsigned int width = 4;
+    unsigned int height = 2;
+    PixelType imageColor = 8;
+
+    SourceImageType sourceImage(width, height);
+    DestinationImageType destinationImage(width, height);
+    ImplementationType implementation(sourceImage, destinationImage);
+
+    sourceImage = [imageColor] (unsigned int, unsigned int) -> PixelType {
+        return imageColor;
+    };
+
+    implementation.apply();
+
+    for (unsigned int x = 0; x < width; ++x) {
+        for (unsigned int y = 0; y < height; ++y) {
+            auto node = destinationImage.getNodeOfPixel(x, y);
+            unsigned int expectedId = 0;
+
+            assertThat(node.level).isEqualTo(imageColor);
+            assertThat(node.id).isEqualTo(expectedId);
+            assertThat((bool)node.parent).isEqualTo(false);
+        }
+    }
+}
