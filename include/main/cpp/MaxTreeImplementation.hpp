5f40d8f4507f4b338f7e7ae4b8c103e5e803d469
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-28 22:26:48 -0200

    Sub-class MaxTreeImplementation from FilterImplem.
    - Destination image type is a MaxTreeImage with the specified internal
      image type

--- include/main/cpp/MaxTreeImplementation.hpp
+++ include/main/cpp/MaxTreeImplementation.hpp
@@ -1,8 +1,12 @@
 #ifndef MAX_TREE_IMPLEMENTATION_HPP
 #define MAX_TREE_IMPLEMENTATION_HPP
 
+#include "FilterImplementation.hpp"
+#include "MaxTreeImage.hpp"
+
 template <typename SourceImageType, typename InternalImageType>
-class MaxTreeImplementation {
+class MaxTreeImplementation : public FilterImplementation<SourceImageType,
+        MaxTreeImage<InternalImageType> > {
 };
 
 #endif
