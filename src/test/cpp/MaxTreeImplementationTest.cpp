05a4d9036b23ea5af123fc7656e7a8c5356f6026
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-11-28 22:26:00 -0200

    Test if MaxTreeImplementation is a FilterImplem.
    - Destination image type is a MaxTreeImage with the specified internal
      image type

--- src/test/cpp/MaxTreeImplementationTest.cpp
+++ src/test/cpp/MaxTreeImplementationTest.cpp
@@ -2,6 +2,8 @@
 
 #include "asserts.hpp"
 
+#include "FilterImplementation.hpp"
+#include "MaxTreeImage.hpp"
 #include "MaxTreeImplementation.hpp"
 
 #include "DummyTypes.hpp"
@@ -16,4 +18,18 @@ TEST(MaxTreeImplementationTest, classTemplateExists) {
             InternalImageType>;
 
     AssertThat<DummyImplementationType>::isClassOrStruct();
+}
+
+TEST(MaxTreeImplementationTest, isFilterImplementation) {
+    using SourcePixelType = DummyTypes<1>;
+    using DestinationPixelType = DummyTypes<2>;
+    using SourceImageType = Image<SourcePixelType>;
+    using InternalImageType = FakeImage<DestinationPixelType>;
+    using DestinationImageType = MaxTreeImage<InternalImageType>;
+    using DummyImplementationType = MaxTreeImplementation<SourceImageType,
+            InternalImageType>;
+    using ParentClass = FilterImplementation<SourceImageType,
+            DestinationImageType>;
+
+    AssertThat<DummyImplementationType>::isSubClass(Of<ParentClass>());
 }
