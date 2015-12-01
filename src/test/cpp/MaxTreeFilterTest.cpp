d565601b802e8f3ac456513a9de4fccb52190b46
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-01 19:32:36 -0200

    Test if MaxTreeFilter is a ComplexFilter

--- src/test/cpp/MaxTreeFilterTest.cpp
+++ src/test/cpp/MaxTreeFilterTest.cpp
@@ -2,7 +2,10 @@
 
 #include "asserts.hpp"
 
+#include "ComplexFilter.hpp"
 #include "MaxTreeFilter.hpp"
+#include "MaxTreeImage.hpp"
+#include "MaxTreeImplementation.hpp"
 
 #include "DummyTypes.hpp"
 #include "FakeImage.hpp"
@@ -16,4 +19,20 @@ TEST(MaxTreeFilterTest, classTemplateExists) {
             InternalImageType>;
 
     AssertThat<DummyMaxTreeFilter>::isClassOrStruct();
+}
+
+TEST(MaxTreeFilterTest, isComplexFilter) {
+    using SourcePixelType = DummyTypes<1>;
+    using DestinationPixelType = DummyTypes<2>;
+    using SourceImageType = Image<SourcePixelType>;
+    using InternalImageType = FakeImage<DestinationPixelType>;
+    using DestinationImageType = MaxTreeImage<InternalImageType>;
+    using DummyMaxTreeFilter = MaxTreeFilter<SourceImageType,
+            InternalImageType>;
+    using ImplementationType = MaxTreeImplementation<SourceImageType,
+            InternalImageType>;
+    using ParentFilter = ComplexFilter<SourceImageType, DestinationImageType,
+            ImplementationType>;
+
+    AssertThat<DummyMaxTreeFilter>::isSubClass(Of<ParentFilter>());
 }
