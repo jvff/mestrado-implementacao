78dcee1f647d5b7578521e350b116c001ca976dc
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-01 19:33:08 -0200

    Sub-class MaxTreeFilter from ComplexFilter

--- include/main/cpp/MaxTreeFilter.hpp
+++ include/main/cpp/MaxTreeFilter.hpp
@@ -1,8 +1,14 @@
 #ifndef MAX_TREE_FILTER_HPP
 #define MAX_TREE_FILTER_HPP
 
+#include "ComplexFilter.hpp"
+#include "MaxTreeImage.hpp"
+#include "MaxTreeImplementation.hpp"
+
 template <typename SourceImageType, typename InternalImageType>
-class MaxTreeFilter {
+class MaxTreeFilter : public ComplexFilter<SourceImageType,
+        MaxTreeImage<InternalImageType>, MaxTreeImplementation<SourceImageType,
+                InternalImageType> > {
 };
 
 #endif
