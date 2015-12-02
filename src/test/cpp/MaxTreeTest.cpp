b55f22ab32848ee83a0272ad43b57be5b876024b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-02 20:10:10 -0200

    Refactor to create MaxTreeTest fixture
    - Centralize subject-under-test

--- src/test/cpp/MaxTreeTest.cpp
+++ src/test/cpp/MaxTreeTest.cpp
@@ -1,48 +1,30 @@
-#include <gtest/gtest.h>
+#include "MaxTreeTest.hpp"
 
-#include "asserts.hpp"
-
-#include "MaxTree.hpp"
-
-#include "DummyTypes.hpp"
-
-TEST(MaxTreeTest, classTemplateExists) {
-    using DummyMaxTreeType = MaxTree<DummyType>;
-
+TEST_F(MaxTreeTest, classTemplateExists) {
     AssertThat<DummyMaxTreeType>::isClassOrStruct();
 }
 
-TEST(MaxTreeTest, isInitiallyEmpty) {
-    MaxTree<DummyType> tree;
-
+TEST_F(MaxTreeTest, isInitiallyEmpty) {
     assertThat(tree.isEmpty()).isEqualTo(true);
 }
 
-TEST(MaxTreeTest, initiallyHasZeroLevels) {
-    MaxTree<DummyType> tree;
-
+TEST_F(MaxTreeTest, initiallyHasZeroLevels) {
     assertThat(tree.numberOfLevels()).isEqualTo(0u);
 }
 
-TEST(MaxTreeTest, isNotEmptyAfterInsertingNode) {
-    MaxTree<DummyType> tree;
-
+TEST_F(MaxTreeTest, isNotEmptyAfterInsertingNode) {
     tree.addNode(DummyType{ 0 });
 
     assertThat(tree.isEmpty()).isEqualTo(false);
 }
 
-TEST(MaxTreeTest, hasOneLevelAfterInsertingNode) {
-    MaxTree<DummyType> tree;
-
+TEST_F(MaxTreeTest, hasOneLevelAfterInsertingNode) {
     tree.addNode(DummyType{ 0 });
 
     assertThat(tree.numberOfLevels()).isEqualTo(1u);
 }
 
-TEST(MaxTreeTest, hasOneLevelAfterInsertingNodesAtSameLevel) {
-    MaxTree<DummyType> tree;
-
+TEST_F(MaxTreeTest, hasOneLevelAfterInsertingNodesAtSameLevel) {
     tree.addNode(DummyType{ 70 });
     tree.addNode(DummyType{ 70 });
     tree.addNode(DummyType{ 70 });
@@ -50,18 +32,14 @@ TEST(MaxTreeTest, hasOneLevelAfterInsertingNodesAtSameLevel) {
     assertThat(tree.numberOfLevels()).isEqualTo(1u);
 }
 
-TEST(MaxTreeTest, isEmptyAfterInsertingThenRemovingNode) {
-    MaxTree<DummyType> tree;
-
+TEST_F(MaxTreeTest, isEmptyAfterInsertingThenRemovingNode) {
     tree.addNode(DummyType{ 0 });
     tree.removeNode(DummyType{ 0 }, 0);
 
     assertThat(tree.isEmpty()).isEqualTo(true);
 }
 
-TEST(MaxTreeTest, isNotEmptyAfterInsertingTwoNodesThenRemovingOneNode) {
-    MaxTree<DummyType> tree;
-
+TEST_F(MaxTreeTest, isNotEmptyAfterInsertingTwoNodesThenRemovingOneNode) {
     tree.addNode(DummyType{ 0 });
     tree.addNode(DummyType{ 0 });
     tree.removeNode(DummyType{ 0 }, 0);
---
+++ src/test/cpp/MaxTreeTest.hpp
@@ -0,0 +1,19 @@
+#ifndef MAX_TREE_TEST_HPP
+#define MAX_TREE_TEST_HPP
+
+#include <gtest/gtest.h>
+
+#include "asserts.hpp"
+
+#include "MaxTree.hpp"
+
+#include "DummyTypes.hpp"
+
+class MaxTreeTest : public ::testing::Test {
+protected:
+    using DummyMaxTreeType = MaxTree<DummyType>;
+
+    DummyMaxTreeType tree;
+};
+
+#endif
