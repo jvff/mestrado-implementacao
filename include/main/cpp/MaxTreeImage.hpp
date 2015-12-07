e530daf44e74fe4ab00c205d615ec866faa0d5e4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-07 15:00:21 -0200

    Refactor MaxTreeImage to use a MaxTree instance
    - Add a MaxTree instance member
    - Remove custom tree implementation and manipulation methods
    - Refactor assignPixelToLatestNode() and assignPixelToNewNode() methods
      to use a helper assignPixelToNode() method, because MaxTree doesn't
      automatically create a node for a level if the level is empty

--- include/main/cpp/MaxTreeImage.hpp
+++ include/main/cpp/MaxTreeImage.hpp
@@ -1,10 +1,8 @@
 #ifndef MAX_TREE_IMAGE_HPP
 #define MAX_TREE_IMAGE_HPP
 
-#include <map>
-#include <vector>
-
 #include "Image.hpp"
+#include "MaxTree.hpp"
 #include "MaxTreeNode.hpp"
 #include "SimpleArrayImage.hpp"
 
@@ -15,16 +13,12 @@ public:
 
 private:
     using NodeType = MaxTreeNode<PixelType>;
-    using NodePointerType = std::shared_ptr<NodeType>;
-    using NodeList = std::vector<NodePointerType>;
     using SuperClass = Image<PixelType>;
 
     InternalImageType internalImage;
     SimpleArrayImage<unsigned int> nodeIdImage;
 
-    std::map<PixelType, NodeList> treeLevels;
-
-    NodePointerType noParent;
+    MaxTree<PixelType> maxTree;
 
 public:
     MaxTreeImage(unsigned int width, unsigned int height)
@@ -44,145 +38,42 @@ public:
 
     void assignPixelToLatestNode(unsigned int x, unsigned int y) {
         auto level = getPixelValue(x, y);
-        auto& latestNode = getLatestNodeOrCreateFirstNodeAtLevel(level);
 
-        nodeIdImage.setPixel(x, y, latestNode->getId());
+        if (maxTree.hasLevel(level))
+            assignPixelToNode(x, y, maxTree.getLatestNodeOnLevel(level));
+        else
+            assignPixelToNode(x, y, maxTree.addNode(level));
     }
 
     void assignPixelToNewNode(unsigned int x, unsigned int y) {
         auto level = getPixelValue(x, y);
-        auto& newNode = createNodeAtLevel(level);
 
-        nodeIdImage.setPixel(x, y, newNode->getId());
+        assignPixelToNode(x, y, maxTree.addNode(level));
     }
 
     void connectPixels(unsigned int firstX, unsigned int firstY,
             unsigned int secondX, unsigned int secondY) {
-        auto& firstNode = getPixelNode(firstX, firstY);
-        auto& secondNode = getPixelNode(secondX, secondY);
+        auto& firstNode = getNodeOfPixel(firstX, firstY);
+        auto& secondNode = getNodeOfPixel(secondX, secondY);
 
-        if (secondNode->getLevel() > firstNode->getParent().getLevel())
-            firstNode->setParent(secondNode);
+        if (secondNode.getLevel() > firstNode.getParent().getLevel())
+            maxTree.setNodeParent(firstNode, secondNode);
     }
 
     const NodeType& getNodeOfPixel(unsigned int x, unsigned int y) const {
-        auto& nodePointer = getPixelNode(x, y);
+        auto level = internalImage.getPixelValue(x, y);
+        auto id = nodeIdImage.getPixelValue(x, y);
 
-        return *nodePointer;
+        return maxTree.getNode(level, id);
     }
 
     using SuperClass::getPixelValue;
     using SuperClass::setPixel;
 
 private:
-    const NodePointerType& getPixelNode(unsigned int x, unsigned int y) const {
-        auto level = internalImage.getPixelValue(x, y);
-        auto nodeId = nodeIdImage.getPixelValue(x, y);
-        auto& nodePointer = getNode(level, nodeId);
-
-        return nodePointer;
-    }
-
-    const NodePointerType& getNode(PixelType level, unsigned int nodeId) const {
-        auto& nodesAtLevel = getNodesAtLevel(level);
-
-        return nodesAtLevel[nodeId];
-    }
-
-    NodePointerType& getLatestNodeOrCreateFirstNodeAtLevel(PixelType level) {
-        auto& nodesAtLevel = getNodesAtLevelOrCreateLevel(level);
-
-        return nodesAtLevel.back();
-    }
-
-    NodePointerType& createNodeAtLevel(PixelType level) {
-        bool levelWasCreated = treeDoesNotHaveLevel(level);
-        auto& nodesAtLevel = getNodesAtLevelOrCreateLevel(level);
-
-        if (levelWasCreated)
-            return nodesAtLevel.back();
-        else
-            return createNodeIn(level, nodesAtLevel);
-    }
-
-    const NodeList& getNodesAtLevel(PixelType level) const {
-        if (treeDoesNotHaveLevel(level))
-            throw std::string("Unavailable tree level");
-
-        return treeLevels.at(level);
-    }
-
-    NodeList& getNodesAtLevelOrCreateLevel(PixelType level) {
-        if (treeDoesNotHaveLevel(level))
-            createNewLevel(level);
-
-        return treeLevels[level];
-    }
-
-    bool treeDoesNotHaveLevel(PixelType level) const {
-        auto notFound = treeLevels.end();
-
-        return treeLevels.find(level) == notFound;
-    }
-
-    void createNewLevel(PixelType level) {
-        NodeList nodesAtLevel;
-        NodePointerType newNode(new NodeType);
-        auto& parent = getLatestNodeAtLevelBefore(level);
-
-        newNode->setLevel(level);
-        newNode->setId(0);
-        newNode->setParent(parent);
-
-        nodesAtLevel.push_back(newNode);
-
-        treeLevels[level] = nodesAtLevel;
-    }
-
-    NodePointerType& createNodeIn(PixelType level, NodeList& nodesAtLevel) {
-        NodePointerType newNode(new NodeType);
-        auto& parent = getLatestNodeAtLevelBefore(level);
-        auto id = nodesAtLevel.size();
-
-        newNode->setLevel(level);
-        newNode->setId(id);
-        newNode->setParent(parent);
-
-        nodesAtLevel.push_back(newNode);
-
-        return nodesAtLevel[id];
-    }
-
-    NodePointerType& getLatestNodeAtLevelBefore(PixelType level) {
-        if (treeLevels.empty() || level == getFirstLevel())
-            return noParent;
-
-        auto previousLevel = getLevelBefore(level);
-
-        return getLatestNodeOrCreateFirstNodeAtLevel(previousLevel);
-    }
-
-    PixelType getFirstLevel() {
-        auto firstLevelPosition = treeLevels.begin();
-        auto firstLevelKeyValuePair = *firstLevelPosition;
-        auto firstLevel = std::get<0>(firstLevelKeyValuePair);
-
-        return firstLevel;
-    }
-
-    PixelType getLevelBefore(PixelType level) {
-        auto position = treeLevels.begin();
-        auto end = treeLevels.end();
-
-        while (position->first < level && position != end)
-            ++position;
-
-        --position;
-
-        auto keyValuePair = *position;
-        auto previousLevel = std::get<0>(keyValuePair);
-
-        return previousLevel;
+    void assignPixelToNode(unsigned int x, unsigned int y,
+            const MaxTreeNode<PixelType>& node) {
+        nodeIdImage.setPixel(x, y, node.getId());
     }
 };
 
