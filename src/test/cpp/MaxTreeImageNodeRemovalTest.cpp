c4a63392f893ce610c19a0009391f0a991c71c7f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-07 18:47:54 -0200

    Test if only pixels assigned to node are updated

--- src/test/cpp/MaxTreeImageNodeRemovalTest.cpp
+++ src/test/cpp/MaxTreeImageNodeRemovalTest.cpp
@@ -31,3 +31,29 @@ TEST(MaxTreeImageNodeRemovalTest, pixelsAreUpdatedWhenNodeIsRemoved) {
     assertThat(image.getPixelValue(0, 1)).isEqualTo(lowerPixelColor);
     assertThat(image.getPixelValue(1, 1)).isEqualTo(lowerPixelColor);
 }
+
+TEST(MaxTreeImageNodeRemovalTest, onlyPixelsAssignedToNodeAreUpdated) {
+    MaxTreeImage<SimpleArrayImage<DummyType> > image(2, 2);
+
+    auto lowerPixelColor = DummyType{ 3018 };
+    auto higherPixelColor = DummyType{ 91314 };
+
+    image.setPixel(0, 0, lowerPixelColor);
+    image.setPixel(1, 0, higherPixelColor);
+    image.setPixel(0, 1, higherPixelColor);
+    image.setPixel(1, 1, higherPixelColor);
+
+    image.assignPixelToLatestNode(0, 0);
+    image.assignPixelToLatestNode(1, 0);
+    image.assignPixelToNewNode(0, 1);
+    image.assignPixelToLatestNode(1, 1);
+
+    auto& higherNode = image.getPixelNode(1, 1);
+
+    image.removeNode(higherNode);
+
+    assertThat(image.getPixelValue(0, 0)).isEqualTo(lowerPixelColor);
+    assertThat(image.getPixelValue(1, 0)).isEqualTo(higherPixelColor);
+    assertThat(image.getPixelValue(0, 1)).isEqualTo(lowerPixelColor);
+    assertThat(image.getPixelValue(1, 1)).isEqualTo(lowerPixelColor);
+}
