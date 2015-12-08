7cf6adcba3d23448272ca32e0f9f95c5e665f43b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-08 16:38:11 -0200

    Refactor to separate node removal tests
    - Create new MaxTreeLevelNodeRemovalTest set
    - Create fixture based on MaxTreeLevelTest fixture
    - Move node removal tests into the new test set

---
+++ src/test/cpp/MaxTreeLevelNodeRemovalTest.cpp
@@ -0,0 +1,76 @@
+#include "MaxTreeLevelNodeRemovalTest.hpp"
+
+TEST_F(MaxTreeLevelNodeRemovalTest, testRemovalOfLastNode) {
+    level.addNode();
+    auto secondNode = level.addNode();
+    level.addNode();
+
+    level.removeNode(2u);
+
+    auto latestNode = level.getLatestNode();
+
+    verifyNode(latestNode, 1u);
+
+    assertThat(latestNode).isEqualTo(secondNode);
+}
+
+TEST_F(MaxTreeLevelNodeRemovalTest, testRemovalOfFirstNode) {
+    level.addNode();
+    level.addNode();
+    auto lastNode = level.addNode();
+
+    level.removeNode(0u);
+
+    auto firstNode = level.getNode(0u);
+    auto secondNode = level.getNode(1u);
+    auto latestNode = level.getLatestNode();
+
+    verifyNode(firstNode, 0u);
+    verifyNode(secondNode, 1u);
+    verifyNode(latestNode, 1u);
+
+    assertThat(secondNode).isEqualTo(lastNode);
+    assertThat(latestNode).isEqualTo(lastNode);
+}
+
+TEST_F(MaxTreeLevelNodeRemovalTest, testRemovalOfMiddleNode) {
+    level.addNode();
+    level.addNode();
+    auto lastNode = level.addNode();
+
+    level.removeNode(1u);
+
+    auto firstNode = level.getNode(0u);
+    auto secondNode = level.getNode(1u);
+    auto latestNode = level.getLatestNode();
+
+    verifyNode(firstNode, 0u);
+    verifyNode(secondNode, 1u);
+    verifyNode(latestNode, 1u);
+
+    assertThat(secondNode).isEqualTo(lastNode);
+    assertThat(latestNode).isEqualTo(lastNode);
+}
+
+TEST_F(MaxTreeLevelNodeRemovalTest, testRemovalOfAllNodes) {
+    level.addNode();
+    level.addNode();
+
+    level.removeNode(1u);
+    level.removeNode(0u);
+
+    assertThat(level.isEmpty()).isEqualTo(true);
+}
+
+TEST_F(MaxTreeLevelNodeRemovalTest, removingThenAddingNodeRecreatesIt) {
+    auto initialNode = level.addNode();
+
+    level.removeNode(0u);
+
+    auto newNode = level.addNode();
+
+    verifyNode(initialNode, 0u);
+    verifyNode(newNode, 0u);
+
+    assertThat(newNode).isNotEqualTo(initialNode);
+}
---
+++ src/test/cpp/MaxTreeLevelNodeRemovalTest.hpp
@@ -0,0 +1,15 @@
+#ifndef MAX_TREE_LEVEL_NODE_REMOVAL_TEST_HPP
+#define MAX_TREE_LEVEL_NODE_REMOVAL_TEST_HPP
+
+#include <gtest/gtest.h>
+
+#include "asserts.hpp"
+
+#include "MaxTreeLevel.hpp"
+
+#include "MaxTreeLevelTest.hpp"
+
+class MaxTreeLevelNodeRemovalTest : public MaxTreeLevelTest {
+};
+
+#endif
