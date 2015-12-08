e7c72636f0b996321e6c9739dda8b0eac9673d5a
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-08 11:18:08 -0200

    Return a pointer to a new node in addNode()
    - Always create the same node for now

--- include/main/cpp/MaxTreeLevel.hpp
+++ include/main/cpp/MaxTreeLevel.hpp
@@ -1,21 +1,31 @@
 #ifndef MAX_TREE_LEVEL_HPP
 #define MAX_TREE_LEVEL_HPP
 
+#include <memory>
+
+#include "MaxTreeNode.hpp"
+
 template <typename T>
 class MaxTreeLevel {
 private:
+    using NodeType = MaxTreeNode<T>;
+    using NodePointer = std::shared_ptr<NodeType>;
+
     bool empty = true;
+    T level;
 
 public:
-    MaxTreeLevel(const T&) {
+    MaxTreeLevel(const T& level) : level(level) {
     }
 
     bool isEmpty() {
         return empty;
     }
 
-    void addNode() {
+    NodePointer addNode() {
         empty = false;
+
+        return std::make_shared<NodeType>(level, 0u);
     }
 };
 
