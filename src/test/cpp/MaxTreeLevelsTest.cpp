2cb918ba4bc22aa356071243919aa4aa394d5008
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-14 14:42:15 -0200

    Test if it initially has no levels

--- src/test/cpp/MaxTreeLevelsTest.cpp
+++ src/test/cpp/MaxTreeLevelsTest.cpp
@@ -11,3 +11,20 @@ TEST_F(MaxTreeLevelsTest, isInitiallyEmpty) {
 TEST_F(MaxTreeLevelsTest, initiallyHasZeroLevels) {
     assertThat(constLevels.numberOfLevels()).isEqualTo(0u);
 }
+
+TEST_F(MaxTreeLevelsTest, initiallHasNoLevels) {
+    auto minimumLevel = DummyType{ std::numeric_limits<int>::min() };
+    auto maximumLevel = DummyType{ std::numeric_limits<int>::max() };
+
+    assertThat(constLevels.hasLevel(minimumLevel)).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ -345 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ -3 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ -2 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ -1 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 0 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 1 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 2 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 3 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 345 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(maximumLevel)).isEqualTo(false);
+}
