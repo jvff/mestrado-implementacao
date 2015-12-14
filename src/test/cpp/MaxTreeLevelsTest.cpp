1545b042dc532da31e9d9675a9f3e89c844c08a6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-14 15:27:49 -0200

    Test if MaxTreeLevels has newly created level
    - Also make sure it doesn't have the added level when the list is
      initialized

--- src/test/cpp/MaxTreeLevelsTest.cpp
+++ src/test/cpp/MaxTreeLevelsTest.cpp
@@ -26,6 +26,7 @@ TEST_F(MaxTreeLevelsTest, initiallHasNoLevels) {
     assertThat(constLevels.hasLevel(DummyType{ 2 })).isEqualTo(false);
     assertThat(constLevels.hasLevel(DummyType{ 3 })).isEqualTo(false);
     assertThat(constLevels.hasLevel(DummyType{ 345 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 75042 })).isEqualTo(false);
     assertThat(constLevels.hasLevel(maximumLevel)).isEqualTo(false);
 }
 
@@ -50,4 +51,36 @@ TEST_F(MaxTreeLevelsTest, hasOneLevelAfterCreatingLevel) {
     levels.getOrCreateLevel(levelHeight);
 
     assertThat(constLevels.numberOfLevels()).isEqualTo(1u);
+}
+
+TEST_F(MaxTreeLevelsTest, hasCreatedLevel) {
+    auto levelHeight = DummyType{ 75042 };
+    auto levelHeightAbove = DummyType{ 75043 };
+    auto levelHeightBelow = DummyType{ 75040 };
+    auto negativeLevelHeight = DummyType{ -75042 };
+    auto negativeLevelHeightBelow = DummyType{ -75043 };
+    auto negativeLevelHeightAbove = DummyType{ -75040 };
+    auto minimumLevel = DummyType{ std::numeric_limits<int>::min() };
+    auto maximumLevel = DummyType{ std::numeric_limits<int>::max() };
+
+    levels.getOrCreateLevel(levelHeight);
+
+    assertThat(constLevels.hasLevel(levelHeight)).isEqualTo(true);
+
+    assertThat(constLevels.hasLevel(minimumLevel)).isEqualTo(false);
+    assertThat(constLevels.hasLevel(negativeLevelHeightBelow)).isEqualTo(false);
+    assertThat(constLevels.hasLevel(negativeLevelHeight)).isEqualTo(false);
+    assertThat(constLevels.hasLevel(negativeLevelHeightAbove)).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ -345 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ -3 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ -2 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ -1 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 0 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 1 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 2 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 3 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(DummyType{ 345 })).isEqualTo(false);
+    assertThat(constLevels.hasLevel(levelHeightBelow)).isEqualTo(false);
+    assertThat(constLevels.hasLevel(levelHeightAbove)).isEqualTo(false);
+    assertThat(constLevels.hasLevel(maximumLevel)).isEqualTo(false);
 }
