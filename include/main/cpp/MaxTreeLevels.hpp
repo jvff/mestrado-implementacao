0ffa42e846328fadaeba62971d22fe2f80507143
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-14 15:01:10 -0200

    Create initial getOrCreateLevel() method

--- include/main/cpp/MaxTreeLevels.hpp
+++ include/main/cpp/MaxTreeLevels.hpp
@@ -1,8 +1,17 @@
 #ifndef MAX_TREE_LEVELS_HPP
 #define MAX_TREE_LEVELS_HPP
 
+#include <memory>
+
+#include "MaxTreeLevel.hpp"
+
 template <typename T>
 class MaxTreeLevels {
+private:
+    using NodeLevel = MaxTreeLevel<T>;
+
+    std::shared_ptr<NodeLevel> level;
+
 public:
     bool isEmpty() const {
         return true;
@@ -14,6 +23,12 @@ public:
 
     bool hasLevel(const T&) const {
         return false;
+    }
+
+    NodeLevel& getOrCreateLevel(const T& levelHeight) {
+        level = std::make_shared<NodeLevel>(levelHeight);
+
+        return *level;
     }
 };
 
