8beaa8fdb90a2a5fa40569b5dfc757df8a815321
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-12-14 11:59:35 -0200

    Test if MaxTreeLevels is initially empty

--- src/test/cpp/MaxTreeLevelsTest.cpp
+++ src/test/cpp/MaxTreeLevelsTest.cpp
@@ -3,3 +3,9 @@
 TEST_F(MaxTreeLevelsTest, classTemplateExists) {
     AssertThat<DummyMaxTreeLevels>::isClassOrStruct();
 }
+
+TEST_F(MaxTreeLevelsTest, isInitiallyEmpty) {
+    const DummyMaxTreeLevels levels;
+
+    assertThat(levels.isEmpty()).isEqualTo(true);
+}
