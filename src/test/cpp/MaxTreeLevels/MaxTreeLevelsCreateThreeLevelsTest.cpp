17a67a0365fb4c8370bc97703ae68e956dfb37ab
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-01-02 23:53:42 -0200

    Test if third created level is empty

--- src/test/cpp/MaxTreeLevels/MaxTreeLevelsCreateThreeLevelsTest.cpp
+++ src/test/cpp/MaxTreeLevels/MaxTreeLevelsCreateThreeLevelsTest.cpp
@@ -9,3 +9,13 @@ TEST_F(MaxTreeLevelsCreateThreeLevelsTest, thirdCreatedLevelHasCorrectHeight) {
 
     assertThat(thirdLevel.getLevel()).isEqualTo(thirdLevelHeight);
 }
+
+TEST_F(MaxTreeLevelsCreateThreeLevelsTest, thirdCreatedLevelIsEmpty) {
+    levels.getOrCreateLevel(DummyType{ 14931 });
+    levels.getOrCreateLevel(DummyType{ 20398 });
+
+    auto thirdLevelHeight = DummyType{ 24971 };
+    auto& thirdLevel = levels.getOrCreateLevel(thirdLevelHeight);
+
+    assertThat(thirdLevel.isEmpty()).isEqualTo(true);
+}
