5d50e51fdda885f15812c48ac473939aa2540d57
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-01-03 23:06:41 -0200

    Test if second level replaces first on its removal

--- src/test/cpp/MaxTreeLevels/MaxTreeLevelsRemoveLevelsTest.cpp
+++ src/test/cpp/MaxTreeLevels/MaxTreeLevelsRemoveLevelsTest.cpp
@@ -7,3 +7,14 @@ TEST_F(MaxTreeLevelsRemoveLevelsTest, removedLevelIsNotPresent) {
 
     assertThat(levels.hasLevel(height)).isEqualTo(false);
 }
+
+TEST_F(MaxTreeLevelsRemoveLevelsTest,
+        secondLevelBecomesFirstWhenFirstIsRemoved) {
+    auto secondLevelHeight = DummyType{ 2 };
+    auto& secondLevel = levels.getLevel(secondLevelHeight);
+
+    levels.removeLevel(DummyType{ 1 });
+
+    assertThat(levels.getFirstLevelHeight()).isEqualTo(secondLevelHeight);
+    assertThat(levels.getFirstLevel()).isAtSameAddressAs(secondLevel);
+}
