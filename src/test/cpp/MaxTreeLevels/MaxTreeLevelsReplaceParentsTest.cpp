3da6609e443bc8c4896093b444e0d42a89515b0a
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-01-14 18:21:47 -0200

    Test if parents are replaced on different levels

--- src/test/cpp/MaxTreeLevels/MaxTreeLevelsReplaceParentsTest.cpp
+++ src/test/cpp/MaxTreeLevels/MaxTreeLevelsReplaceParentsTest.cpp
@@ -78,3 +78,74 @@ TEST_F(MaxTreeLevelsReplaceParentsTest, replacesThreeParents) {
                 2, 1,
                 2, 1);
 }
+
+TEST_F(MaxTreeLevelsReplaceParentsTest, replacesParentsOnDifferentLevels) {
+    createRootLevel(1);
+    createLevelWithParents(2,
+            1, 0,
+            1, 0,
+            1, 0);
+    createLevelWithParents(5,
+            2, 0,
+            1, 0,
+            2, 1,
+            2, 0,
+            2, 2);
+    createLevelWithParents(6,
+            2, 2,
+            5, 1,
+            5, 4,
+            2, 0,
+            5, 2);
+    createLevelWithParents(10,
+            6, 0,
+            6, 3,
+            5, 4,
+            2, 2,
+            2, 0,
+            5, 0);
+    createLevelWithParents(11,
+            10, 0,
+            5, 1,
+            10, 3,
+            5, 1,
+            10, 2,
+            6, 3);
+
+    auto oldParent = getNode(DummyType{ 5 }, 1u);
+    auto newParent = getNode(DummyType{ 2 }, 2u);
+
+    levels.replaceParents(oldParent, newParent);
+
+    verifyTree(DummyType{ 1 },
+            DummyType{ 2 },
+                1, 0,
+                1, 0,
+                1, 0,
+            DummyType{ 5 },
+                2, 0,
+                1, 0,
+                2, 1,
+                2, 0,
+                2, 2,
+            DummyType{ 6 },
+                2, 2,
+                2, 2,
+                5, 4,
+                2, 0,
+                5, 2,
+            DummyType{ 10 },
+                6, 0,
+                6, 3,
+                5, 4,
+                2, 2,
+                2, 0,
+                5, 0,
+            DummyType{ 11 },
+                10, 0,
+                2, 2,
+                10, 3,
+                2, 2,
+                10, 2,
+                6, 3);
+}
