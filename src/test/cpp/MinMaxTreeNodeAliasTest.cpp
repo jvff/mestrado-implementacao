4af64f64652b030ade9d1a14bad3e4b649abbc71
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-02 13:28:29 -0200

    Test if MinNodeTree type alias exists

--- src/test/cpp/MinMaxTreeNodeAliasTest.cpp
+++ src/test/cpp/MinMaxTreeNodeAliasTest.cpp
@@ -5,6 +5,7 @@
 #include "asserts.hpp"
 
 #include "MaxTreeNode.hpp"
+#include "MinTreeNode.hpp"
 
 #include "DummyTypes.hpp"
 
@@ -13,4 +14,11 @@ TEST(MinMaxTreeNodeAliasTest, aliasForMaxTreeNodeExists) {
     using ExpectedType = MinMaxTreeNode<DummyType, std::less<DummyType> >;
 
     AssertThat<MaxTreeNodeAlias>::isTheSame(As<ExpectedType>());
+}
+
+TEST(MinMaxTreeNodeAliasTest, aliasForMinTreeNodeExists) {
+    using MinTreeNodeAlias = MinTreeNode<DummyType>;
+    using ExpectedType = MinMaxTreeNode<DummyType, std::greater<DummyType> >;
+
+    AssertThat<MinTreeNodeAlias>::isTheSame(As<ExpectedType>());
 }
