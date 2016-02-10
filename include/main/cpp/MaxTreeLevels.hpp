fe45a2c75670b049266cdfece08675e47ffdcfe9
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-10 18:39:50 -0200

    Refactor MinMaxTreeLevels into separate file
    - Create a separate header for MinMaxTreeLevels class template
    - Leave only the MaxTreeLevels alias in the old header
    - Include missing <functional> header
    
    MASTIMP-7 #time 3m

--- include/main/cpp/MaxTreeLevels.hpp
+++ include/main/cpp/MaxTreeLevels.hpp
@@ -1,94 +1,9 @@
 #ifndef MAX_TREE_LEVELS_HPP
 #define MAX_TREE_LEVELS_HPP
 
-#include <map>
-#include <memory>
+#include <functional>
 
-#include "MaxTreeLevel.hpp"
-
-template <typename T, typename LevelOrderComparator>
-class MinMaxTreeLevels {
-private:
-    using NodeLevel = MaxTreeLevel<T>;
-    using NodePointer = std::shared_ptr<MaxTreeNode<T> >;
-
-    std::map<T, NodeLevel> levels;
-
-public:
-    bool isEmpty() const {
-        return levels.empty();
-    }
-
-    unsigned int numberOfLevels() const {
-        return levels.size();
-    }
-
-    T getFirstLevelHeight() const {
-        auto firstPosition = levels.begin();
-        auto firstLevelHeight = firstPosition->first;
-
-        return firstLevelHeight;
-    }
-
-    NodeLevel& getFirstLevel() {
-        auto firstPosition = levels.begin();
-        auto& firstLevel = firstPosition->second;
-
-        return firstLevel;
-    }
-
-    bool hasLevel(const T& levelHeight) const {
-        auto notFound = levels.end();
-        auto searchResult = levels.find(levelHeight);
-
-        return searchResult != notFound;
-    }
-
-    NodeLevel& getOrCreateLevel(const T& levelHeight) {
-        if (!hasLevel(levelHeight))
-            createLevel(levelHeight);
-
-        return getLevel(levelHeight);
-    }
-
-    NodeLevel& getLevel(const T& levelHeight) {
-        return levels.at(levelHeight);
-    }
-
-    const NodeLevel& getLevel(const T& levelHeight) const {
-        return levels.at(levelHeight);
-    }
-
-    NodeLevel& findLevelBefore(const T& height) {
-        auto firstPositionAfterOrAtHeight = levels.lower_bound(height);
-        auto& positionBeforeHeight = firstPositionAfterOrAtHeight;
-
-        --positionBeforeHeight;
-
-        return positionBeforeHeight->second;;
-    }
-
-    void removeLevel(const T& height) {
-        levels.erase(height);
-    }
-
-    void replaceParents(NodePointer oldParent, NodePointer newParent) {
-        auto startLevelHeight = oldParent->getLevel();
-        auto startLevel = levels.find(startLevelHeight);
-        auto endLevel = levels.end();
-
-        for (auto position = startLevel; position != endLevel; ++position) {
-            auto& level = position->second;
-
-            level.replaceParents(oldParent, newParent);
-        }
-    }
-
-private:
-    void createLevel(const T& levelHeight) {
-        levels.emplace(levelHeight, NodeLevel(levelHeight));
-    }
-};
+#include "MinMaxTreeLevels.hpp"
 
 template <typename T>
 using MaxTreeLevels = MinMaxTreeLevels<T, std::less<T> >;
