85e607c88f34be32489a6fd1fdaea99c16e7b7a8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-18 17:19:15 -0200

    Refactor MaxTreeImplementationTest into typed test
    - Prepare for testing both MaxTreeImplementation and
      MinTreeImplementation
    - Create a wrapper struct for a template template parameter
    - Use the wrapper struct as the type parameter for the test fixture
    - Deduce from the type parameter the necessary level order comparator
      and the tree type comparator necessary for MinMaxTreeImage and an
      updated MinMaxTreeImplementation class template, respectively
    
    MASTIMP-19 #time 17m

--- src/test/cpp/MaxTreeImplementationTest.cpp
+++ src/test/cpp/MaxTreeImplementationTest.cpp
@@ -1,18 +1,22 @@
 #include "MaxTreeImplementationTest.hpp"
 #include "MaxTreeTests.hpp"
 
-TEST_F(MaxTreeImplementationTest, classTemplateExists) {
+using TreeTypeComparators = ::testing::Types<ComparatorWrapper<std::less> >;
+
+TYPED_TEST_CASE(MaxTreeImplementationTest, TreeTypeComparators);
+
+TEST_C(classTemplateExists) {
     AssertThat<DummyImplementationType>::isClassOrStruct();
 }
 
-TEST_F(MaxTreeImplementationTest, isFilterImplementation) {
+TEST_C(isFilterImplementation) {
     using ParentClass = FilterImplementation<SourceImageType,
             DestinationImageType>;
 
     AssertThat<DummyImplementationType>::isSubClass(Of<ParentClass>());
 }
 
-TEST_F(MaxTreeImplementationTest, isConstructibleWithParameters) {
+TEST_C(isConstructibleWithParameters) {
     using SourceImageParameter = const SourceImageType&;
     using DestinationImageParameter = DestinationImageType&;
 
--- src/test/cpp/MaxTreeImplementationTest.hpp
+++ src/test/cpp/MaxTreeImplementationTest.hpp
@@ -1,6 +1,8 @@
 #ifndef MAX_TREE_IMPLEMENTATION_TEST_HPP
 #define MAX_TREE_IMPLEMENTATION_TEST_HPP
 
+#include <functional>
+
 #include <gtest/gtest.h>
 
 #include "asserts.hpp"
@@ -11,20 +13,54 @@
 #include "SimpleArrayImage.hpp"
 
 #include "AbstractFilterImplementationTestData.hpp"
+#include "CustomTypedTestMacros.hpp"
 #include "DummyTypes.hpp"
 #include "FakeImage.hpp"
 #include "MaxTreeTestData.hpp"
 
+template <typename TypeParameter>
 class MaxTreeImplementationTest : public ::testing::Test {
 protected:
+    template <typename T>
+    using TreeTypeComparator =
+            typename TypeParameter::template ComparatorFor<T>;
+
     using SourcePixelType = DummyTypes<1>;
     using DestinationPixelType = DummyTypes<2>;
     using SourceImageType = Image<SourcePixelType>;
     using InternalImageType = FakeImage<DestinationPixelType>;
-    using DestinationImageType = MaxTreeImage<InternalImageType>;
-    using DummyImplementationType = MaxTreeImplementation<SourceImageType,
-            InternalImageType>;
+    using LevelOrderComparator = TreeTypeComparator<DestinationPixelType>;
+    using DestinationImageType = MinMaxTreeImage<InternalImageType,
+            LevelOrderComparator>;
+    using DummyImplementationType = MinMaxTreeImplementation<SourceImageType,
+            InternalImageType, TreeTypeComparator>;
 };
+
+template <template <typename> class Comparator>
+struct ComparatorWrapper {
+    template <typename T>
+    using ComparatorFor = Comparator<T>;
+};
+
+#define TEST_C(testName) \
+    CREATE_MAX_TREE_IMPLEMENTATION_TEST_CLASS(testName); \
+    REGISTER_CUSTOM_TYPED_TEST(MaxTreeImplementationTest, testName); \
+    START_CUSTOM_TYPED_TEST_BODY(MaxTreeImplementationTest, testName)
+
+#define CREATE_MAX_TREE_IMPLEMENTATION_TEST_CLASS(testName) \
+template <typename TypeParameter> \
+class GTEST_TEST_CLASS_NAME_(MaxTreeImplementationTest, testName) \
+        : public MaxTreeImplementationTest<TypeParameter> { \
+private: \
+    using SuperClass = MaxTreeImplementationTest<TypeParameter>; \
+    using SourceImageType = typename SuperClass::SourceImageType; \
+    using DestinationImageType = typename SuperClass::DestinationImageType; \
+    using DummyImplementationType = \
+            typename SuperClass::DummyImplementationType; \
+\
+private: \
+    virtual void TestBody(); \
+}
 
 template <typename PixelType, typename ImageType = SimpleArrayImage<PixelType> >
 using TestData = MaxTreeTestData<AbstractFilterImplementationTestData<
