0ba9e5a15b664b6b9e663941c140bc4eb0acc823
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-19 00:34:37 -0200

    Create MaxTreeFilter alias
    - Rename original class to MinMaxTreeFilter
    - Add extra template parameter to determine the type of the tree
    - Create alias with the extra parameter set
    
    MASTIMP-4 #time 2m

--- include/main/cpp/MaxTreeFilter.hpp
+++ include/main/cpp/MaxTreeFilter.hpp
@@ -1,14 +1,21 @@
 #ifndef MAX_TREE_FILTER_HPP
 #define MAX_TREE_FILTER_HPP
 
+#include <functional>
+
 #include "ComplexFilter.hpp"
 #include "MaxTreeImage.hpp"
 #include "MaxTreeImplementation.hpp"
 
-template <typename SourceImageType, typename InternalImageType>
-class MaxTreeFilter : public ComplexFilter<SourceImageType,
+template <typename SourceImageType, typename InternalImageType,
+        template <typename> class TreeTypeComparator>
+class MinMaxTreeFilter : public ComplexFilter<SourceImageType,
         MaxTreeImage<InternalImageType>, MaxTreeImplementation<SourceImageType,
                 InternalImageType> > {
 };
+
+template <typename SourceImageType, typename InternalImageType>
+using MaxTreeFilter = MinMaxTreeFilter<SourceImageType, InternalImageType,
+        std::less>;
 
 #endif
