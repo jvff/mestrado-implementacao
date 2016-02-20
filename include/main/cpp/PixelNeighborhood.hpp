919bb394e5a5ca8d5324475f6baa0b31b5ec0282
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-20 17:47:22 -0200

    Create initial hasAvailableNeighbor() method
    MASTIMP-22 #time 2m

--- include/main/cpp/PixelNeighborhood.hpp
+++ include/main/cpp/PixelNeighborhood.hpp
@@ -1,8 +1,14 @@
 #ifndef PIXEL_NEIGHBORHOOD_HPP
 #define PIXEL_NEIGHBORHOOD_HPP
 
+#include "Image.hpp"
+
 template <typename PixelType>
 class PixelNeighborhood {
+public:
+    bool hasAvailableNeighbor(const Image<bool>&, unsigned int, unsigned int) {
+        return true;
+    }
 };
 
 #endif
