25a0e5f2cd0c2f389dcb468ea9a89cb973e6cb06
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-20 17:53:59 -0200

    Test hasAvailableNeighbor with marked north pixel
    MASTIMP-22 #time 7m

--- src/test/cpp/PixelNeighborhoodTest.cpp
+++ src/test/cpp/PixelNeighborhoodTest.cpp
@@ -13,3 +13,11 @@ TEST_F(PixelNeighborhoodTest, canTestMaskImageIfThereIsAnAvailableNeighbor) {
 
     assertThat(result).isEqualTo(true);
 }
+
+TEST_F(PixelNeighborhoodTest, hasNeighborAvailableIfOnlyNorthWasMarked) {
+    maskImage.setPixel(1, 0, true);
+
+    auto result = neighborhood.hasAvailableNeighbor(maskImageReference, 1, 1);
+
+    assertThat(result).isEqualTo(true);
+}
