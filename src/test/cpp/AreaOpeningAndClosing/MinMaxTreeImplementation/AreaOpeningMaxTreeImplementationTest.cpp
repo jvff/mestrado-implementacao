aa17cec512812186b83c0acf46412fa478e8cd0b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-24 18:17:56 -0300

    Remove AreaOpeningMaxTreeImplementationTest header
    - Move its contents to the source file
    
    MASTIMP-6 #time 4m

--- src/test/cpp/AreaOpeningAndClosing/MinMaxTreeImplementation/AreaOpeningMaxTreeImplementationTest.cpp
+++ src/test/cpp/AreaOpeningAndClosing/MinMaxTreeImplementation/AreaOpeningMaxTreeImplementationTest.cpp
@@ -1,6 +1,38 @@
-#include "AreaOpeningMaxTreeImplementationTest.hpp"
+#include <gtest/gtest.h>
 
+#include "asserts.hpp"
+
+#include "AreaOpeningMaxTreeImplementation.hpp"
+#include "SimpleArrayImage.hpp"
+
+#include "../../AbstractFilterImplementationTestData.hpp"
+
+#include "../AreaOpeningAndClosingTestData.hpp"
 #include "../AreaOpeningTests.hpp"
+
+template <typename PixelType, typename ImageType = SimpleArrayImage<PixelType> >
+class AbstractAreaOpeningMaxTreeImplementationTestData
+        : public AbstractFilterImplementationTestData<
+                AreaOpeningMaxTreeImplementation<ImageType, ImageType>,
+                ImageType, ImageType> {
+private:
+    using ImplementationType = AreaOpeningMaxTreeImplementation<ImageType,
+            ImageType>;
+
+protected:
+    ImplementationType instantiateImplementation(const ImageType& sourceImage,
+            ImageType& destinationImage) override {
+        return ImplementationType(getMaximumExtremitySize(), sourceImage,
+                destinationImage);
+    }
+
+    virtual unsigned int getMaximumExtremitySize() const = 0;
+};
+
+template <typename PixelType, typename ImageType = SimpleArrayImage<PixelType> >
+using TestData = AreaOpeningAndClosingTestData<
+        AbstractAreaOpeningMaxTreeImplementationTestData<PixelType, ImageType>,
+        PixelType, ImageType>;
 
 INSTANTIATE_COMPLEX_FILTER_TEST_CASE(AreaOpeningMaxTreeImplementationTest,
         AreaOpeningTests, TestData);
