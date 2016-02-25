9c25b9e8852ccd0128a33fa75c32511084b6c470
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-25 19:54:55 -0300

    Test if levels with a different level differ
    MASTIMP-26 #time 1m

--- src/test/cpp/MinMaxTreeLevels/ComparisonTests.cpp
+++ src/test/cpp/MinMaxTreeLevels/ComparisonTests.cpp
@@ -43,3 +43,25 @@ TEST_C(ComparisonTests, levelsWithSameEmptyLevelsAreEqual) {
     assertThat(constOtherLevels).isEqualTo(constLevels);
     assertThat(constLevels).isEqualTo(constOtherLevels);
 }
+
+TEST_C(ComparisonTests, levelsWithDifferentLevelDiffer) {
+    DummyMinMaxTreeLevels otherLevels;
+    const auto& constOtherLevels = otherLevels;
+
+    auto firstLevel = DummyType{ 22 };
+    auto secondLevel = DummyType{ 12 };
+    auto thirdLevel = DummyType{ 96 };
+
+    levels.getOrCreateLevel(firstLevel);
+    levels.getOrCreateLevel(secondLevel);
+    levels.getOrCreateLevel(thirdLevel);
+
+    otherLevels.getOrCreateLevel(firstLevel);
+    otherLevels.getOrCreateLevel(secondLevel);
+    otherLevels.getOrCreateLevel(thirdLevel);
+
+    otherLevels.getOrCreateLevel(DummyType{ 2038 });
+
+    assertThat(constOtherLevels).isNotEqualTo(constLevels);
+    assertThat(constLevels).isNotEqualTo(constOtherLevels);
+}
