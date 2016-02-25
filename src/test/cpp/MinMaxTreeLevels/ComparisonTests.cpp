7d370359081aa7babf49bb9234cfabc855a78a97
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-25 18:53:51 -0300

    Test if empty levels are equal
    MASTIMP-26 #time 5m

--- src/test/cpp/MinMaxTreeLevels/ComparisonTests.cpp
+++ src/test/cpp/MinMaxTreeLevels/ComparisonTests.cpp
@@ -5,3 +5,11 @@ SUB_TEST(ComparisonTests);
 TEST_C(ComparisonTests, isComparable) {
     assertThat(constLevels).isEqualTo(constLevels);
 }
+
+TEST_C(ComparisonTests, emptyLevelsAreTheSame) {
+    DummyMinMaxTreeLevels emptyLevels;
+    const auto& constEmptyLevels = emptyLevels;
+
+    assertThat(constEmptyLevels).isEqualTo(constLevels);
+    assertThat(constLevels).isEqualTo(constEmptyLevels);
+}
