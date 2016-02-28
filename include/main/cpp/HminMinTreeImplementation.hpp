d76003ee613e0c160da911b5be414f74b2685c5b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-28 19:12:35 -0300

    Add parent class to HminMinTreeImplementation
    MASTIMP-5 #time 1m

--- include/main/cpp/HminMinTreeImplementation.hpp
+++ include/main/cpp/HminMinTreeImplementation.hpp
@@ -1,8 +1,12 @@
 #ifndef HMIN_MIN_TREE_IMPLEMENTATION_HPP
 #define HMIN_MIN_TREE_IMPLEMENTATION_HPP
 
+#include "FilterImplementation.hpp"
+#include "MinTreeImage.hpp"
+
 template <typename InternalImageType>
-class HminMinTreeImplementation {
+class HminMinTreeImplementation : public FilterImplementation<
+        MinTreeImage<InternalImageType>, MinTreeImage<InternalImageType> > {
 };
 
 #endif
