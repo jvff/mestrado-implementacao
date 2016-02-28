218c10e4c90366b5ff09e68646123104ae473c8c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-27 22:34:24 -0300

    Test if empty trees are equal
    MASTIMP-25 #time 1m

--- src/test/cpp/MinMaxTree/ComparisonTests.cpp
+++ src/test/cpp/MinMaxTree/ComparisonTests.cpp
@@ -7,3 +7,11 @@ SUB_TEST();
 TEST_C(isComparable) {
     assertThat(constTree).isEqualTo(constTree);
 }
+
+TEST_C(emptyTreesAreEqual) {
+    auto emptyTree = DummyMinMaxTreeType();
+    const auto& constEmptyTree = emptyTree;
+
+    assertThat(constTree).isEqualTo(constEmptyTree);
+    assertThat(constEmptyTree).isEqualTo(constTree);
+}
