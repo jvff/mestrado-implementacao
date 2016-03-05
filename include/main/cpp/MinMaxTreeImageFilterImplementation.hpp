62972d008aa75febc444c5d9a98f9857eb44e59e
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-05 19:52:22 -0300

    Create initial MinMaxTreeImageFilterImplementation
    MASTIMP-36 #time 3m

---
+++ include/main/cpp/MinMaxTreeImageFilterImplementation.hpp
@@ -0,0 +1,8 @@
+#ifndef MIN_MAX_TREE_IMAGE_FILTER_IMPLEMENTATION_HPP
+#define MIN_MAX_TREE_IMAGE_FILTER_IMPLEMENTATION_HPP
+
+template <typename InternalImageType, typename LevelOrderComparator>
+class MinMaxTreeImageFilterImplementation {
+};
+
+#endif
