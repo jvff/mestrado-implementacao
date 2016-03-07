639a22eac7c228df619659f6565711b2d939d7cf
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-07 19:30:26 -0300

    Create initial collectPixelNodes() method
    - For now only return a set with the node of the first pixel
    
    MASTIMP-36 #time 12m

--- include/main/cpp/MinMaxTreeImageFilterImplementation.hpp
+++ include/main/cpp/MinMaxTreeImageFilterImplementation.hpp
@@ -1,13 +1,35 @@
 #ifndef MIN_MAX_TREE_IMAGE_FILTER_IMPLEMENTATION_HPP
 #define MIN_MAX_TREE_IMAGE_FILTER_IMPLEMENTATION_HPP
 
+#include <set>
+
 #include "FilterImplementation.hpp"
 #include "MinMaxTreeImage.hpp"
+#include "MinMaxTreeNode.hpp"
 
 template <typename InternalImageType, typename LevelOrderComparator>
 class MinMaxTreeImageFilterImplementation : public FilterImplementation<
         MinMaxTreeImage<InternalImageType, LevelOrderComparator>,
         MinMaxTreeImage<InternalImageType, LevelOrderComparator> > {
+private:
+    using PixelType = typename InternalImageType::PixelType;
+    using NodeType = MinMaxTreeNode<PixelType, LevelOrderComparator>;
+    using ImageType = MinMaxTreeImage<InternalImageType, LevelOrderComparator>;
+    using SuperClass = FilterImplementation<ImageType, ImageType>;
+
+    using SuperClass::sourceImage;
+
+public:
+    using SuperClass::SuperClass;
+
+protected:
+    std::set<NodeType> collectPixelNodes() {
+        std::set<NodeType> pixelNodes;
+
+        pixelNodes.insert(sourceImage.getPixelNode(0, 0));
+
+        return pixelNodes;
+    }
 };
 
 #endif
