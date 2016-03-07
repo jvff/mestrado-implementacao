521e96b6a25c213d5edf0a340faf4e58ff91d3e9
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-06 22:01:14 -0300

    Sub-class from FilterImplementation
    MASTIMP-36 #time 2m

--- include/main/cpp/MinMaxTreeImageFilterImplementation.hpp
+++ include/main/cpp/MinMaxTreeImageFilterImplementation.hpp
@@ -1,8 +1,13 @@
 #ifndef MIN_MAX_TREE_IMAGE_FILTER_IMPLEMENTATION_HPP
 #define MIN_MAX_TREE_IMAGE_FILTER_IMPLEMENTATION_HPP
 
+#include "FilterImplementation.hpp"
+#include "MinMaxTreeImage.hpp"
+
 template <typename InternalImageType, typename LevelOrderComparator>
-class MinMaxTreeImageFilterImplementation {
+class MinMaxTreeImageFilterImplementation : public FilterImplementation<
+        MinMaxTreeImage<InternalImageType, LevelOrderComparator>,
+        MinMaxTreeImage<InternalImageType, LevelOrderComparator> > {
 };
 
 #endif
