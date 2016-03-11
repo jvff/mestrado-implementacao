e76e9cae8da1c3995f8d9d8667c3a84051472c28
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-11 10:08:00 -0300

    Make OpenCLImage a concrete class
    - Add constructor to receive image dimensions and the OpenCL context and
      command queue
    - Stub the setPixel and getPixelValue methods to make the class concrete
    
    MASTIMP-38 #time 7m

--- include/main/cpp/OpenCLImage.hpp
+++ include/main/cpp/OpenCLImage.hpp
@@ -1,10 +1,26 @@
 #ifndef OPEN_C_L_IMAGE_HPP
 #define OPEN_C_L_IMAGE_HPP
 
+#include <CL/cl.hpp>
+
 #include "Image.hpp"
 
 template <typename PixelType>
 class OpenCLImage : public Image<PixelType> {
+private:
+    using SuperClass = Image<PixelType>;
+
+public:
+    OpenCLImage(unsigned int width, unsigned int height, cl::Context&,
+            cl::CommandQueue&) : SuperClass(width, height) {
+    }
+
+    void setPixel(unsigned int, unsigned int, PixelType) override {
+    }
+
+    PixelType getPixelValue(unsigned int, unsigned int) const override {
+        return 0;
+    }
 };
 
 #endif
