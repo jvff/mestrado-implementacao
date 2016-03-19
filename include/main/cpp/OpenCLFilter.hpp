972138b331c71d5eb3e96796d287c2f80c22ae7b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-19 00:08:27 -0300

    Sub-class OpenCLFilter from Filter
    MASTIMP-39 #time 2m

--- include/main/cpp/OpenCLFilter.hpp
+++ include/main/cpp/OpenCLFilter.hpp
@@ -1,8 +1,12 @@
 #ifndef OPEN_C_L_FILTER_HPP
 #define OPEN_C_L_FILTER_HPP
 
+#include "AbstractFilter.hpp"
+#include "OpenCLImage.hpp"
+
 template <typename PixelType, typename... KernelParameterTypes>
-class OpenCLFilter {
+class OpenCLFilter : public AbstractFilter<OpenCLImage<PixelType>,
+        OpenCLImage<PixelType> > {
 };
 
 #endif
