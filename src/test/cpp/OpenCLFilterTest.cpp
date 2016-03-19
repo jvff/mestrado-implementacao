207584415e5c64e809bd9f239724ee7c88c4b39f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-18 23:53:55 -0300

    Test if OpenCLFilter has variadic template args.
    MASTIMP-39 #time 2m

--- src/test/cpp/OpenCLFilterTest.cpp
+++ src/test/cpp/OpenCLFilterTest.cpp
@@ -3,3 +3,13 @@
 TEST_F(OpenCLFilterTest, classTemplateExists) {
     AssertThat<FilterType>::isClassOrStruct();
 }
+
+TEST_F(OpenCLFilterTest, classTemplateHasVariadicTemplateArguments) {
+    using FirstExtraParameter = unsigned int;
+    using SecondExtraParameter = char;
+    using ThirdExtraParameter = float;
+    using FilterTypeWithExtraParameters = OpenCLFilter<PixelType,
+            FirstExtraParameter, SecondExtraParameter, ThirdExtraParameter>;
+
+    AssertThat<FilterTypeWithExtraParameters>::isClassOrStruct();
+}
