2c163fbc3a086f3ac9ca811fe0b2ca2a449d43a7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-19 00:03:23 -0300

    Create initial OpenCLFilter constructor
    - Also override apply method and createDestinationImage methods so that
      OpenCLFilter becomes a concrete class
    - Implement createDestinationImage to create an OpenCLImage using the
      same context and command queue as the source image
    
    MASTIMP-39 #time 5m

--- include/main/cpp/OpenCLFilter.hpp
+++ include/main/cpp/OpenCLFilter.hpp
@@ -7,6 +7,25 @@
 template <typename PixelType, typename... KernelParameterTypes>
 class OpenCLFilter : public AbstractFilter<OpenCLImage<PixelType>,
         OpenCLImage<PixelType> > {
+private:
+    using ImageType = OpenCLImage<PixelType>;
+
+public:
+    OpenCLFilter(const std::string&, const std::string&) {
+    }
+
+    void apply(const ImageType&, ImageType&) override {
+    }
+
+protected:
+    ImageType createDestinationImage(const ImageType& sourceImage) override {
+        auto width = sourceImage.getWidth();
+        auto height = sourceImage.getHeight();
+        auto context = sourceImage.getContext();
+        auto commandQueue = sourceImage.getCommandQueue();
+
+        return ImageType(width, height, context, commandQueue);
+    }
 };
 
 #endif
