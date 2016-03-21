39754248725d20225526e6e3910acd6c870199fe
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-21 20:25:55 -0300

    Create initial apply method
    - Applies the filter kernel, using a global work size of one task per
      destination image pixel
    
    MASTIMP-39 #time 47m

--- include/main/cpp/OpenCLFilter.hpp
+++ include/main/cpp/OpenCLFilter.hpp
@@ -1,6 +1,10 @@
 #ifndef OPEN_C_L_FILTER_HPP
 #define OPEN_C_L_FILTER_HPP
 
+#include <tuple>
+
+#include <CL/cl.hpp>
+
 #include "AbstractFilter.hpp"
 #include "OpenCLImage.hpp"
 
@@ -10,12 +14,45 @@ class OpenCLFilter : public AbstractFilter<OpenCLImage<PixelType>,
 private:
     using ImageType = OpenCLImage<PixelType>;
 
+private:
+    const std::string kernelSourceCode;
+    const std::string kernelFunctionName;
+
 public:
-    OpenCLFilter(const std::string&, const std::string&,
-            KernelParameterTypes...) {
+    OpenCLFilter(const std::string& kernelSourceCode,
+            const std::string& kernelFunctionName, KernelParameterTypes...)
+            : kernelSourceCode(kernelSourceCode),
+            kernelFunctionName(kernelFunctionName) {
     }
 
-    void apply(const ImageType&, ImageType&) override {
+    void apply(const ImageType& sourceImage, ImageType& destinationImage)
+            override {
+        auto sourceCodeString = kernelSourceCode.c_str();
+        auto sourceCodeSize = kernelSourceCode.length();
+        auto sourcePair = std::make_pair(sourceCodeString, sourceCodeSize);
+        auto sources = cl::Program::Sources(1, sourcePair);
+
+        auto context = destinationImage.getContext();
+        auto commandQueue = destinationImage.getCommandQueue();
+        auto sourceImageBuffer = sourceImage.getImageBuffer();
+        auto destinationImageBuffer = destinationImage.getImageBuffer();
+
+        cl::Program program(context, sources);
+
+        program.build();
+
+        cl::Kernel kernel(program, kernelFunctionName.c_str());
+
+        kernel.setArg(0, sourceImageBuffer);
+        kernel.setArg(1, destinationImageBuffer);
+
+        auto width = destinationImage.getWidth();
+        auto height = destinationImage.getHeight();
+        auto globalOffset = cl::NullRange;
+        auto globalWorkSize = cl::NDRange(width, height);
+
+        commandQueue.enqueueNDRangeKernel(kernel, globalOffset, globalWorkSize);
+        commandQueue.finish();
     }
 
 protected:
