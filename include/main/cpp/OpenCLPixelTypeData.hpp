daae4604863504cdc42b1b95f759525f554a1577
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-21 17:42:02 -0300

    Create OpenCLPixelTypeData specialization for int
    MASTIMP-60 #time 1m #resolve

--- include/main/cpp/OpenCLPixelTypeData.hpp
+++ include/main/cpp/OpenCLPixelTypeData.hpp
@@ -15,4 +15,9 @@ struct OpenCLPixelTypeData<unsigned int> {
     static constexpr auto CL_PIXEL_TYPE = CL_UNSIGNED_INT32;
 };
 
+template <>
+struct OpenCLPixelTypeData<int> {
+    static constexpr auto CL_PIXEL_TYPE = CL_SIGNED_INT32;
+};
+
 #endif
