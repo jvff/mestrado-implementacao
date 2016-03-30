bebdcc74081c4c547445c1d766479863ff357d55
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-03-30 17:28:38 -0300

    Remove obsoleted TestImage class
    MASTIMP-42 #time 1m

--- src/test/cpp/BinarizationFilter/BinarizationFilterTest.hpp
+++ src/test/cpp/BinarizationFilter/BinarizationFilterTest.hpp
@@ -24,36 +24,4 @@ public:
     }
 };
 
-template <typename SourcePixel,
-        template <typename> class ComparatorTemplate = std::greater>
-class TestImage : public Image<SourcePixel> {
-public:
-    using Comparator = ComparatorTemplate<SourcePixel>;
-
-    SimpleArrayImage<SourcePixel> sourceImage;
-    SimpleArrayImage<bool> expectedImage;
-    SourcePixel filterThreshold;
-    Comparator comparator;
-
-    TestImage(unsigned int width, unsigned int height,
-            const SourcePixel& threshold)
-            : Image<SourcePixel>(width, height), sourceImage(width, height),
-            expectedImage(width, height), filterThreshold(threshold) {
-        *this = [] (unsigned int x, unsigned int y) {
-            return (int)x - (int)y;
-        };
-    }
-
-    using Image<SourcePixel>::operator=;
-
-    SourcePixel getPixelValue(unsigned int x, unsigned int y) const override {
-        return sourceImage.getPixelValue(x, y);
-    }
-
-    void setPixel(unsigned int x, unsigned int y, SourcePixel value) override {
-        sourceImage.setPixel(x, y, value);
-        expectedImage.setPixel(x, y, comparator(value, filterThreshold));
-    }
-};
-
 #endif
