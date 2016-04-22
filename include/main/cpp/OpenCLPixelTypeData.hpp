9c4983838179b26e3e335f5f67089969fe8006f5
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-04-22 18:44:48 -0300

    Refactor OpenCLPixelTypeData to use macros
    - Reduce repeated code
    
    MASTIMP-68 #time 3m

--- include/main/cpp/OpenCLPixelTypeData.hpp
+++ include/main/cpp/OpenCLPixelTypeData.hpp
@@ -9,25 +9,18 @@ template <typename PixelType>
 struct OpenCLPixelTypeData {
 };
 
-template <>
-struct OpenCLPixelTypeData<unsigned char> {
-    static constexpr auto CL_PIXEL_TYPE = CL_UNSIGNED_INT8;
+#define OPENCL_PIXEL_TYPE_DATA_FOR(Type, OpenCLPixelType) \
+template <> \
+struct OpenCLPixelTypeData<Type> { \
+    static constexpr auto CL_PIXEL_TYPE = OpenCLPixelType; \
+    \
+    static const std::string setPixelKernel; \
+}
 
-    static const std::string setPixelKernel;
-};
+OPENCL_PIXEL_TYPE_DATA_FOR(unsigned char, CL_UNSIGNED_INT8);
+OPENCL_PIXEL_TYPE_DATA_FOR(unsigned int, CL_UNSIGNED_INT32);
+OPENCL_PIXEL_TYPE_DATA_FOR(int, CL_SIGNED_INT32);
 
-template <>
-struct OpenCLPixelTypeData<unsigned int> {
-    static constexpr auto CL_PIXEL_TYPE = CL_UNSIGNED_INT32;
-
-    static const std::string setPixelKernel;
-};
-
-template <>
-struct OpenCLPixelTypeData<int> {
-    static constexpr auto CL_PIXEL_TYPE = CL_SIGNED_INT32;
-
-    static const std::string setPixelKernel;
-};
+#undef OPENCL_PIXEL_TYPE_DATA_FOR
 
 #endif
