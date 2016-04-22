3a9ef507ea1c1b7877ebc2db4536e422cac99ac0
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-04-22 18:41:05 -0300

    Refactor OpenCLPixelTypeDataTest to use macros
    - Reduce repeated code
    
    MASTIMP-68 #time 6m

--- src/test/cpp/OpenCLPixelTypeDataTest.cpp
+++ src/test/cpp/OpenCLPixelTypeDataTest.cpp
@@ -6,38 +6,19 @@
 
 #include "OpenCLPixelTypeData.hpp"
 
-TEST(OpenCLPixelTypeDataTest, hasSetPixelKernelForUnsignedChar) {
-    auto setPixelKernel = OpenCLPixelTypeData<unsigned char>::setPixelKernel;
+#define TEST_C(TypeName, Type, OpenCLPixelType, KernelSuffix) \
+    TEST(OpenCLPixelTypeDataTest, hasPixelTypeFor##TypeName) { \
+        auto pixelType = OpenCLPixelTypeData<Type>::CL_PIXEL_TYPE; \
+        \
+        assertThat(pixelType).isEqualTo(OpenCLPixelType); \
+    } \
+    \
+    TEST(OpenCLPixelTypeDataTest, hasSetPixelKernelFor##TypeName) { \
+        auto setPixelKernel = OpenCLPixelTypeData<Type>::setPixelKernel; \
+        \
+        assertThat(setPixelKernel).isEqualTo("setPixel" KernelSuffix); \
+    }
 
-    assertThat(setPixelKernel).isEqualTo("setPixelUsingOneChannel");
-}
-
-TEST(OpenCLPixelTypeDataTest, hasPixelTypeForUnsignedChar) {
-    auto pixelType = OpenCLPixelTypeData<unsigned char>::CL_PIXEL_TYPE;
-
-    assertThat(pixelType).isEqualTo(CL_UNSIGNED_INT8);
-}
-
-TEST(OpenCLPixelTypeDataTest, hasSetPixelKernelForUnsignedInt) {
-    auto setPixelKernel = OpenCLPixelTypeData<unsigned int>::setPixelKernel;
-
-    assertThat(setPixelKernel).isEqualTo("setPixelUsingOneChannel");
-}
-
-TEST(OpenCLPixelTypeDataTest, hasPixelTypeForUnsignedInt) {
-    auto pixelType = OpenCLPixelTypeData<unsigned int>::CL_PIXEL_TYPE;
-
-    assertThat(pixelType).isEqualTo(CL_UNSIGNED_INT32);
-}
-
-TEST(OpenCLPixelTypeDataTest, hasSetPixelKernelForInt) {
-    auto setPixelKernel = OpenCLPixelTypeData<int>::setPixelKernel;
-
-    assertThat(setPixelKernel).isEqualTo("setPixelUsingOneChannel");
-}
-
-TEST(OpenCLPixelTypeDataTest, hasPixelTypeForInt) {
-    auto pixelType = OpenCLPixelTypeData<int>::CL_PIXEL_TYPE;
-
-    assertThat(pixelType).isEqualTo(CL_SIGNED_INT32);
-}
+TEST_C(UnsignedChar, unsigned char, CL_UNSIGNED_INT8, "UsingOneChannel")
+TEST_C(UnsignedInt, unsigned int, CL_UNSIGNED_INT32, "UsingOneChannel")
+TEST_C(Int, int, CL_SIGNED_INT32, "UsingOneChannel")
