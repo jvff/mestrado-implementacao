76678a6b7edab852af3a9c73c17f9d94d1d75cb2
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-04-27 17:20:44 -0300

    Add pixel channels to OpenCLPixelTypeData
    - For now only one channel is used
    
    MASTIMP-68 #time 10m

--- include/main/cpp/OpenCLPixelTypeData.hpp
+++ include/main/cpp/OpenCLPixelTypeData.hpp
@@ -9,18 +9,19 @@ template <typename PixelType>
 struct OpenCLPixelTypeData {
 };
 
-#define OPENCL_PIXEL_TYPE_DATA_FOR(Type, OpenCLPixelType) \
+#define OPENCL_PIXEL_TYPE_DATA_FOR(Type, OpenCLPixelType, OpenCLPixelChannels) \
 template <> \
 struct OpenCLPixelTypeData<Type> { \
     static constexpr auto CL_PIXEL_TYPE = OpenCLPixelType; \
+    static constexpr auto CL_PIXEL_CHANNELS = OpenCLPixelChannels; \
     \
     static const std::string getPixelKernel; \
     static const std::string setPixelKernel; \
 }
 
-OPENCL_PIXEL_TYPE_DATA_FOR(unsigned char, CL_UNSIGNED_INT8);
-OPENCL_PIXEL_TYPE_DATA_FOR(unsigned int, CL_UNSIGNED_INT32);
-OPENCL_PIXEL_TYPE_DATA_FOR(int, CL_SIGNED_INT32);
+OPENCL_PIXEL_TYPE_DATA_FOR(unsigned char, CL_UNSIGNED_INT8, CL_R);
+OPENCL_PIXEL_TYPE_DATA_FOR(unsigned int, CL_UNSIGNED_INT32, CL_R);
+OPENCL_PIXEL_TYPE_DATA_FOR(int, CL_SIGNED_INT32, CL_R);
 
 #undef OPENCL_PIXEL_TYPE_DATA_FOR
 
