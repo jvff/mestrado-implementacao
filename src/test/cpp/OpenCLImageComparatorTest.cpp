27f0f9e67f3ce681d964b7d2a3cf95a290fd6f2c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-05-09 18:10:10 -0300

    Test if OpenCLImageComparator is an OpenCLFilter
    - Should have an extra kernel parameter for the result value
    
    MASTIMP-69 #time 18m

--- src/test/cpp/OpenCLImageComparatorTest.cpp
+++ src/test/cpp/OpenCLImageComparatorTest.cpp
@@ -1,7 +1,10 @@
+#include <CL/cl.hpp>
+
 #include <gtest/gtest.h>
 
 #include "asserts.hpp"
 
+#include "OpenCLFilter.hpp"
 #include "OpenCLImageComparator.hpp"
 
 #include "DummyTypes.hpp"
@@ -10,4 +13,11 @@ TEST(OpenCLImageComparatorTest, classTemplateExists) {
     using ComparatorType = OpenCLImageComparator<DummyType>;
 
     AssertThat<ComparatorType>::isClassOrStruct();
+}
+
+TEST(OpenCLImageComparatorTest, isOpenCLFilter) {
+    using ComparatorType = OpenCLImageComparator<DummyType>;
+    using ParentClass = OpenCLFilter<DummyType, unsigned char*>;
+
+    AssertThat<ComparatorType>::isSubClass(Of<ParentClass>());
 }
