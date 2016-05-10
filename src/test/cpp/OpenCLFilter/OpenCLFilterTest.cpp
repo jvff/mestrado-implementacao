f9ae7d79ef920908f18a2d93daf0d90f0e86e8a9
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-05-10 17:15:41 -0300

    Refactor to separate basic OpenCLFilter tests
    MASTIMP-71 #time 4m

--- src/test/cpp/OpenCLFilter/OpenCLFilterTest.cpp
+++ src/test/cpp/OpenCLFilter/OpenCLFilterTest.cpp
@@ -1,46 +1,5 @@
 #include "OpenCLFilterTest.hpp"
 
-TEST_F(OpenCLFilterTest, classTemplateExists) {
-    AssertThat<FilterType>::isClassOrStruct();
-}
-
-TEST_F(OpenCLFilterTest, classTemplateHasVariadicTemplateArguments) {
-    using FirstExtraParameter = unsigned int;
-    using SecondExtraParameter = char;
-    using ThirdExtraParameter = float;
-    using FilterTypeWithExtraParameters = OpenCLFilter<PixelType,
-            FirstExtraParameter, SecondExtraParameter, ThirdExtraParameter>;
-
-    AssertThat<FilterTypeWithExtraParameters>::isClassOrStruct();
-}
-
-TEST_F(OpenCLFilterTest, isFilter) {
-    using ParentFilter = AbstractFilter<ImageType, ImageType>;
-
-    AssertThat<FilterType>::isSubClass(Of<ParentFilter>());
-}
-
-TEST_F(OpenCLFilterTest, isConstructibleWithKernelSourceCode) {
-    using KernelSourceCodeParameter = const std::string&;
-    using KernelFunctionNameParameter = const std::string&;
-
-    AssertThat<FilterType>::isConstructible(
-            With<KernelSourceCodeParameter, KernelFunctionNameParameter>());
-}
-
-TEST_F(OpenCLFilterTest, isConstructibleWithKernelSourceCodeAndParameters) {
-    using KernelSourceCodeParameter = const std::string&;
-    using KernelFunctionNameParameter = const std::string&;
-    using FirstKernelParameter = unsigned int;
-    using SecondKernelParameter = float;
-    using FilterTypeWithExtraParameters = OpenCLFilter<PixelType,
-            FirstKernelParameter, SecondKernelParameter>;
-
-    AssertThat<FilterTypeWithExtraParameters>::isConstructible(
-            With<KernelSourceCodeParameter, KernelFunctionNameParameter,
-                    FirstKernelParameter, SecondKernelParameter>());
-}
-
 TEST_F(OpenCLFilterTest, appliesKernelToImages) {
     auto width = 4u;
     auto height = 5u;
