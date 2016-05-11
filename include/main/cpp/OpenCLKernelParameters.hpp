71717f10440c07996dc3253e80f33c9a58253ccc
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-05-11 18:24:46 -0300

    Create initial value constructor
    MASTIMP-71 #time 6m

--- include/main/cpp/OpenCLKernelParameters.hpp
+++ include/main/cpp/OpenCLKernelParameters.hpp
@@ -1,8 +1,13 @@
 #ifndef OPEN_C_L_KERNEL_PARAMETERS_HPP
 #define OPEN_C_L_KERNEL_PARAMETERS_HPP
 
+#include <CL/cl.hpp>
+
 template <typename... Parameters>
 class OpenCLKernelParameters {
+public:
+    OpenCLKernelParameters(const cl::Context&, Parameters...) {
+    }
 };
 
 #endif
