8e63082163bb34f1989ccb3f38aebc0bf8edcec7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-05-12 18:31:41 -0300

    Handle pointer types differently
    - Add extra template parameter to method templates in order to allow the
      buffer type to be switched between:
      - A FakeBuffer type used for testing
      - cl::Buffer when used in production
    
    MASTIMP-71 #time 40m

--- include/main/cpp/OpenCLKernelParameters.hpp
+++ include/main/cpp/OpenCLKernelParameters.hpp
@@ -21,32 +21,65 @@ private:
     using EnableForInvalid =
             typename std::enable_if<parameterIndex >= numberOfParameters>::type;
 
+    template <int parameterIndex>
+    using ParameterType =
+            typename std::tuple_element<parameterIndex, TupleType>::type;
+
+    template <int parameterIndex>
+    using ParameterIsPointer = std::is_pointer<ParameterType<parameterIndex> >;
+
+    template <int parameterIndex, typename BufferType>
+    using EnableForPointerParameter =
+            typename std::enable_if<ParameterIsPointer<parameterIndex>::value,
+                    BufferType>::type;
+
+    template <int parameterIndex>
+    using EnableForNonPointerParameter =
+            typename std::enable_if<!ParameterIsPointer<parameterIndex>::value,
+                    ParameterType<parameterIndex> >::type;
+
 private:
+    const cl::Context& context;
     TupleType parameters;
 
 public:
-    OpenCLKernelParameters(const cl::Context&, Parameters... parameters)
-            : parameters(parameters...) {
+    OpenCLKernelParameters(const cl::Context& context, Parameters... parameters)
+            : context(context), parameters(parameters...) {
     }
 
-    template <typename KernelType>
+    template <typename KernelType, typename BufferType = cl::Buffer>
     void configureKernel(KernelType& kernel) {
-        configureParameters<0>(kernel);
+        configureParameters<0, KernelType, BufferType>(kernel);
     }
 
 private:
-    template <int parameterIndex, typename KernelType>
+    template <int parameterIndex, typename KernelType, typename BufferType>
     EnableForValid<parameterIndex> configureParameters(KernelType& kernel) {
         const auto nextParameterIndex = parameterIndex + 1;
-        auto parameterValue = std::get<parameterIndex>(parameters);
+        auto parameterValue = getParameter<parameterIndex, BufferType>();
 
         kernel.setArg(parameterIndex, parameterValue);
 
-        configureParameters<nextParameterIndex>(kernel);
+        configureParameters<nextParameterIndex, KernelType, BufferType>(kernel);
     }
 
-    template <int parameterIndex, typename KernelType>
+    template <int parameterIndex, typename KernelType, typename BufferType>
     EnableForInvalid<parameterIndex> configureParameters(KernelType&) {
+    }
+
+    template <int parameterIndex, typename BufferType>
+    EnableForPointerParameter<parameterIndex, BufferType> getParameter() {
+        using PointerType = ParameterType<parameterIndex>;
+        using ValueType = typename std::remove_pointer<PointerType>::type;
+
+        auto size = sizeof(ValueType);
+
+        return BufferType(context, CL_MEM_READ_WRITE, size);
+    }
+
+    template <int parameterIndex, typename BufferType>
+    EnableForNonPointerParameter<parameterIndex> getParameter() {
+        return std::get<parameterIndex>(parameters);
     }
 };
 
