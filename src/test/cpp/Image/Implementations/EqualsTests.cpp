256dac3426c179d7384f70fabb4798768cf71f37
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-06-01 18:20:21 -0300

    Test if an image is equal to itself
    MASTIMP-70 #time 4m

--- src/test/cpp/Image/Implementations/EqualsTests.cpp
+++ src/test/cpp/Image/Implementations/EqualsTests.cpp
@@ -1,6 +1,18 @@
 #include "AllPixelTypes.hpp"
 #include "EqualsTests.hpp"
 
+TEST_C(imageIsEqualToItself) {
+    auto width = 12u;
+    auto height = 10u;
+    auto image = createImage(width, height);
+
+    image = [&] (unsigned int x, unsigned int y) -> PixelType {
+        return pattern(width, height, x, y);
+    };
+
+    assertThat(image).isEqualTo(image);
+}
+
 TEST_C(isComparable) {
     auto width = 89u;
     auto height = 78u;
