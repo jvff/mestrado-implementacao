f9e1a150a1aed622b35b884e9e75964e092d0a65
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-07-01 19:42:15 -0300

    Test if work size is different for bool pixel type
    MASTIMP-86 #time 3m

--- src/test/cpp/OpenCLImageComparator/WorkSizeTests.cpp
+++ src/test/cpp/OpenCLImageComparator/WorkSizeTests.cpp
@@ -16,3 +16,21 @@ TEST_C(defaultWorkSizeIsImageDimensions) {
 
     assertThat(workSize).isEqualTo(expectedWorkSize);
 }
+
+TEST_C(workSizeIsDifferentForBoolPixelType) {
+    using PixelType = bool;
+
+    auto pixelGroupsPerLine = 450;
+    auto width = pixelGroupsPerLine * 8;
+    auto height = 1829;
+    auto expectedWorkSize = cl::NDRange(pixelGroupsPerLine, height);
+
+    auto mockImage = createMockImage<PixelType>(width, height);
+    auto& image = mockImage.get();
+
+    auto comparator = FakeOpenCLImageComparator<PixelType>();
+
+    auto workSize = comparator.getGlobalWorkSize(image, image);
+
+    assertThat(workSize).isEqualTo(expectedWorkSize);
+}
