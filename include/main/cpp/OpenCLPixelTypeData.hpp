b008e46837b680cfa5dac43f5300fa0beb511eb8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-07-06 18:15:22 -0300

    Add pixel buffer size to OpenCLPixelTypeData
    MASTIMP-87 #time 7m #resolve

--- include/main/cpp/OpenCLPixelTypeData.hpp
+++ include/main/cpp/OpenCLPixelTypeData.hpp
@@ -10,22 +10,25 @@ template <typename PixelType>
 struct OpenCLPixelTypeData {
 };
 
-#define OPENCL_PIXEL_TYPE_DATA_FOR(Type, OpenCLPixelType, OpenCLPixelChannels) \
+#define OPENCL_PIXEL_TYPE_DATA_FOR(Type, OpenCLPixelType, OpenCLPixelChannels, \
+        OpenCLPixelBufferType) \
 template <> \
 struct OpenCLPixelTypeData<Type> { \
     static constexpr auto CL_PIXEL_TYPE = OpenCLPixelType; \
     static constexpr auto CL_PIXEL_CHANNELS = OpenCLPixelChannels; \
+    static constexpr auto CL_PIXEL_BUFFER_SIZE = \
+            sizeof(OpenCLPixelBufferType); \
     \
     static const std::string getPixelKernel; \
     static const std::string setPixelKernel; \
     static const std::string kernelPixelType; \
 }
 
-OPENCL_PIXEL_TYPE_DATA_FOR(bool, CL_UNSIGNED_INT8, CL_R);
-OPENCL_PIXEL_TYPE_DATA_FOR(unsigned char, CL_UNSIGNED_INT8, CL_R);
-OPENCL_PIXEL_TYPE_DATA_FOR(unsigned int, CL_UNSIGNED_INT32, CL_R);
-OPENCL_PIXEL_TYPE_DATA_FOR(std::uint64_t, CL_UNSIGNED_INT32, CL_RG);
-OPENCL_PIXEL_TYPE_DATA_FOR(int, CL_SIGNED_INT32, CL_R);
+OPENCL_PIXEL_TYPE_DATA_FOR(bool, CL_UNSIGNED_INT8, CL_R, cl_uchar);
+OPENCL_PIXEL_TYPE_DATA_FOR(unsigned char, CL_UNSIGNED_INT8, CL_R, cl_uint);
+OPENCL_PIXEL_TYPE_DATA_FOR(unsigned int, CL_UNSIGNED_INT32, CL_R, cl_uint);
+OPENCL_PIXEL_TYPE_DATA_FOR(std::uint64_t, CL_UNSIGNED_INT32, CL_RG, cl_ulong);
+OPENCL_PIXEL_TYPE_DATA_FOR(int, CL_SIGNED_INT32, CL_R, cl_uint);
 
 #undef OPENCL_PIXEL_TYPE_DATA_FOR
 
